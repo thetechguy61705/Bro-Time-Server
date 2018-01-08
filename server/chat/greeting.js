ae09756a7aa17fcbd4945e334fa17f6b3da09af3
Colton Loewen <loewencjay@gmail.com>
2018-01-07 23:12:31 -0700

    Added several more greetings

--- server/chat/greeting.js
+++ server/chat/greeting.js
@@ -17,6 +17,15 @@ RESPONSES[newGreeting("greetings")] = () => "Hey";
 RESPONSES[newGreeting("greeting")] = () => "Hey";
 RESPONSES[newGreeting("ahoy")] = () => "Greetings";
 RESPONSES[newGreeting("hai")] = () => "Oh hi";
+RESPONSES[newGreeting("su+p")] = matches => "Y" + "o".repeat(matches[0].length - 2);
+RESPONSES[newGreeting("yo")] = matches => "S" + "u".repeat(matches[0].length - 1) + "p";
+RESPONSES[newGreeting("e+y+")] = matches => matches[0];
+RESPONSES[newGreeting("a+y+")] = matches => matches[0];
+RESPONSES[newGreeting("howdy")] = () => "howdy";
+RESPONSES[newGreeting("was+up")] = matches => "Wa" + "s".repeat(matches[0].length - 3) + "up";
+RESPONSES[newGreeting("waz+up")] = matches => "Wa" + "z".repeat(matches[0].length - 3) + "up";
+RESPONSES[newGreeting("salutation")] = () => "greeting";
+RESPONSES[newGreeting("salutations")] = () => "greetings";
 
 function getGreeting(message) {
 	var content = message.content;
