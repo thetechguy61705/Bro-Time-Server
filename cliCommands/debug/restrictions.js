cf2690ba2b13dcf7661eb0a5c783f01a0c9bc50e
Colton Loewen <loewencjay@gmail.com>
2018-02-17 01:02:31 -0700

    Added basic restrictions command

---
+++ cliCommands/debug/restrictions.js
@@ -0,0 +1,8 @@
+exports.command = "restrictions <id> ";
+
+exports.describe = "View restrictions, optionally scanning who can access it by id.";
+
+exports.handler = () => {
+	// View restrictions.
+	// If scan, check access.
+};
