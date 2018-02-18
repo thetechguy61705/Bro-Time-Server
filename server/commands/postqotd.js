cf765b74914309681d92f400190b7691032ef99b
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-18 12:05:42 -0500

    Create postqotd.js

---
+++ server/commands/postqotd.js
@@ -0,0 +1,12 @@
+module.exports = {
+	id: "postqotd",
+	load: () => {},
+	execute: (call) => {
+  if(message.member.roles.has("392041430610214912")) {
+  		let qotd = call.params.readParameter(" ");
+      let qotdrole = message.guild.roles.get("387375439745908747");
+      qotdrole.setMentionable(true);
+      call.message.channel.send(`<@&387375439745908747>:${qotd}`);
+  
+  }
+};
