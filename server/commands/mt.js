59ae517c7b7850a3c97d31506d10bf1493c52a91
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-18 11:01:49 -0500

    Update mt.js

--- server/commands/mt.js
+++ server/commands/mt.js
@@ -4,7 +4,7 @@ module.exports = {
 	execute: (call) => {
 		let rolename = call.params.readParameter(" ").toLowerCase();
 		const prefixes = ["", "-g- ", "[f] ", "[c] "];
-		if(call.message.member.roles.some(r=>["&414568002181398552", "&414605900792201216"].includes(r.id)) ) {
+		if(call.message.member.roles.some(r=>["414568002181398552", "414605900792201216"].includes(r.id)) ) {
 			for (const prefix of prefixes) {
 				let role = call.message.guild.roles.find(r=> r.name.toLowerCase() === prefix+rolename);
 				if(role) role.setMentionable(!role.mentionable);
