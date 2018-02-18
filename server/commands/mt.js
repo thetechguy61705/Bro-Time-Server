87ae7e35847520ae9f57eb47fd1b6b6a8c15fe28
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-17 19:11:24 -0500

    Update mt.js

--- server/commands/mt.js
+++ server/commands/mt.js
@@ -2,13 +2,13 @@ module.exports = {
 	id: "mt",
 	load: () => {},
 	execute: (call) => { 
-	let rolename = args.join(" ").toLowerCase();
-  const prefixes = ["", "-g- ", "[f] ", "[c] "];
+		let rolename = call.args.join(" ").toLowerCase();
+		const prefixes = ["", "-g- ", "[f] ", "[c] "];
 
-  for (const prefix of prefixes) {
-    let role = call.message.guild.roles.find(r=> r.name.toLowerCase() === prefix+rolename);
-    if(role) role.setMentionable(!role.mentionable);
-  }
-  call.message.delete();
+		for (const prefix of prefixes) {
+			let role = call.message.guild.roles.find(r=> r.name.toLowerCase() === prefix+rolename);
+			if(role) role.setMentionable(!role.mentionable);
+		}
+		call.message.delete();
 	}
 };
