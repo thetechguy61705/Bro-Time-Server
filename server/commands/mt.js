7f981263693c12aa63d13f0cc10155854c0a3833
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-17 22:11:58 -0500

    Update mt.js

--- server/commands/mt.js
+++ server/commands/mt.js
@@ -4,8 +4,8 @@ module.exports = {
 	execute: (call) => {
 		let rolename = call.params.readParameter(" ").toLowerCase();
 		const prefixes = ["", "-g- ", "[f] ", "[c] "];
-		if(call.message.member.roles.array()
-			.some(r => {return ["414603140399366145", "414605974205235201"]
+		if(["414603140399366145", "414605974205235201"]
+			.some(r => {return call.message.member.roles.array()
 				.indexOf(r) !== -1;})) {
 			for (const prefix of prefixes) {
 				let role = call.message.guild.roles.find(r=> r.name.toLowerCase() === prefix+rolename);
