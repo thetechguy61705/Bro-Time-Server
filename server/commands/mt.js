7b871a94f53816a3dcfa7483dfa8fb76eefaa18e
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-17 20:17:23 -0500

    Update mt.js

--- server/commands/mt.js
+++ server/commands/mt.js
@@ -4,10 +4,11 @@ module.exports = {
 	execute: (call) => {
 		let rolename = call.params.readParameter(" ").toLowerCase();
 		const prefixes = ["", "-g- ", "[f] ", "[c] "];
+		const allowed = ["414590269997187073", "414590282143891457", "414590302184275978", "414590310795182110"];
 
-		for (const prefix of prefixes) {
+		for ((const prefix of prefixes) && (const id of allowed)) {
 			let role = call.message.guild.roles.find(r=> r.name.toLowerCase() === prefix+rolename);
-			if((role) && (call.message.member.roles.has("414587931043430410"))) {
+			if((role) && (call.message.member.roles.has(id))) {
 				role.setMentionable(!role.mentionable);
 			} else if (role) {
 				call.message.channel.send(`${call.message.author}, you do not have permission to use this command!`);
