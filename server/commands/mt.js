38729dd5accfc10bfaa2a3b589665c4c0eb3c9e5
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-17 20:09:05 -0500

    Update mt.js

--- server/commands/mt.js
+++ server/commands/mt.js
@@ -7,7 +7,11 @@ module.exports = {
 
 		for (const prefix of prefixes) {
 			let role = call.message.guild.roles.find(r=> r.name.toLowerCase() === prefix+rolename);
-			if(role) role.setMentionable(!role.mentionable);
+			if((role) && (call.message.member.roles.has("414587931043430410"))) {
+				role.setMentionable(!role.mentionable);
+			} else if (role) {
+				call.message.channel.send(`${call.message.author}, you do not have permission to use this command!`);
+			}
 		}
 		call.message.delete();
 	}
