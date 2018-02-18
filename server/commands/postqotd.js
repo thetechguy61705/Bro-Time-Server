02d0313ef88fa5861ea41e9624007e20ceb6d892
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-18 18:04:59 -0500

    Update postqotd.js

--- server/commands/postqotd.js
+++ server/commands/postqotd.js
@@ -3,7 +3,7 @@ module.exports = {
 	load: () => {},
 	execute: (call) => {
 		if(call.message.member.roles.has("392041430610214912")) {
-			let qotd = call.params.readParameter("");
+			let qotd = call.params.readRaw(" ");
 			let qotdrole = call.message.guild.roles.get("387375439745908747");
 			qotdrole.setMentionable(true);
 			call.message.channel.send(`<@&387375439745908747>: ${qotd}`);
