70a623b1a48dac2eb2b4ecdfae51541cf704f71f
Colton Loewen <loewencjay@gmail.com>
2018-02-23 18:01:05 -0700

    Added remaining parameters functions
    These functions are currently untested. A test driver command will be
    created later to aid in testing them.

--- server/chat/utility/paramaters.js
+++ server/chat/utility/paramaters.js
@@ -2,9 +2,11 @@ var SPACE = "\\s,";
 
 class Paramaters {
 	constructor(message) {
-		// Store the guild for role access.
 		this.sep = new RegExp(`[${SPACE}]+`, "y");
 		this.raw = message.content;
+		// Store the client for user access.
+		this.client = message.client;
+		// Store the guild for role access.
 		this.guild = message.guild;
 		this.index = 0;
 	}
@@ -56,17 +58,42 @@ class Paramaters {
 		}
 	}
 
-	readUser() {
-		// client
-		// Get the user from client.users that matches the most recent parameter.
+	readObject(objects, name = "name", mentions = /(.+)/, allowPartial = true) {
+		var param = this.readParameter().toLowerCase();
+		var mention = param.match(mentions);
+		if (mention !== null) {
+			param = mention[1];
+		}
+		// Check for objects by name.
+		var object = objects.find((candidate) => {
+			return candidate[name].toLowerCase() == param;
+		});
+		if (object === null) {
+			// Check for objects by id.
+			var id = parseFloat(param);
+			if (!isNaN(id)) {
+				object = objects.get(id);
+			}
+		}
+		if (object === null && allowPartial) {
+			// Check for objects by partial name.
+			object = objects.find((candidate) => {
+				return candidate[name].toLowerCase().includes(param);
+			});
+		}
+		return object;
 	}
 
-	readRole() {
-		// Get the role from a guild that matches the most recent parameter.
+	readUser(allowPartial = true) {
+		return this.readObject(this.client.users, "username", /<@!?(\d+)>/, allowPartial);
 	}
 
-	readChannel() {
-		// Get the channel from a guild that matches the most recent parameter.
+	readRole(allowPartial = true) {
+		return this.readObject(this.guild.roles, "name", /<@&(\d+)>/, allowPartial);
+	}
+
+	readChannel(allowPartial = true) {
+		return this.readObject(this.guild.channels, "name", /<#(\d+)>/, allowPartial);
 	}
 }
 
