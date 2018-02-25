24cf67a2c962dbd09dd728e4fab52ea87b21f1c7
Yikey <gtc122004@gmail.com>
2018-02-24 19:43:48 -0500

    shortened gamerole.js
    travis ci build will fail due to trailing space in parameters.js that is fixed in another pull

--- server/commands/gamerole.js
+++ server/commands/gamerole.js
@@ -2,55 +2,26 @@ var games = ["roblox", "minecraft", "cuphead", "fortnite", "undertale", "unturne
 	"pubg", "fnaf", "clash of clans", "clash royale", "sims", "terraria", "subnautica", "rocket league",
 	"portal", "hat in time", "csgo", "splatoon", "mario", "starbound", "garry's mod", "overwatch",
 	"call of duty", "destiny"];
-var ucfgames = ["roblox", "minecraft", "fortnite", "undertale", "unturned", "sims", "terraria",
-	"subnautica", "portal", "splatoon", "mario", "starbound", "overwatch", "destiny"];
-var wgames = ["vrchat", "clash of clans", "clash royale", "rocket league", "hat in time",
-	"garry's mod", "call of duty"];
-var acgames = ["pubg", "fnaf", "csgo"];
 
 module.exports = {
 	id: "gamerole",
 	load: () => {},
 	execute: (call) => {
-		var game = call.params.readRaw().toLowerCase();
-		var gamerole;
-		if (ucfgames.includes(game)) {
-			gamerole = game.charAt(0).toUpperCase() + game.slice(1);
-		} else if (acgames.includes(game)) {
-			gamerole = call.params.readRaw().toUpperCase();
-		} else if (wgames.includes(game)) {
-			if (game == "vrchat") {
-				gamerole = "VRChat";
-			} else if (game == "clash of clans") {
-				gamerole = "Clash of Clans";
-			} else if (game == "clash royale") {
-				gamerole = "Clash Royale";
-			} else if (game == "rocket league") {
-				gamerole = "Rocket League";
-			} else if (game == "hat in time") {
-				gamerole = "Hat in Time";
-			} else if (game == "garry's mod"||game == "garry’s mod"||game == "garrys mod") {
-				gamerole = "Garry's Mod";
-			} else if (game == "call of duty") {
-				gamerole = "Call of Duty";
-			}
-		}
-		if (game !== null) {
-			if (games.includes(game)) {
-				if (call.message.member.roles.find("name", gamerole)) {
-					call.message.member.removeRole(call.message.guild.roles.find("name", gamerole));
-					call.message.channel
-						.send(`Since you already had the \`${game}\` game role, it has been removed from you!`);
-				} else {
-					call.message.member.addRole(call.message.guild.roles.find("name", gamerole));
-					call.message.channel.send(`Successfully given you the \`${game}\` game role!`);
-				}
-			} else {
+		let game = call.params.readRaw().toLowerCase();
+		if (games.includes(game)) {
+			var role = call.params.readRole();
+			var roleid = role.toString().substr(3).slice(0, -1);
+			if(call.message.member.roles.has(roleid)) {
+				call.message.member.removeRole(role);
 				call.message.channel
-					.send(`\`${game}\` is not a valid game option.`);
+					.send(`Since you already had the \`${game}\` game role, it has been removed from you!`);
+			} else {
+				call.message.member.addRole(role);
+				call.message.channel.send(`Successfully given you the \`${game}\` game role!`);
 			}
 		} else {
-			call.message.channel.send("You didn't ask for any game role. \n Usage: `/gamerole (gamname)`");
+				call.message.channel
+				.send(`\`${game} \` is not a valid game option.`);
 		}
 	}
 };
