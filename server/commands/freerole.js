9dbb6ae7e02611f31b126a07426c1e5c3ef18c1b
Colton Loewen <loewencjay@gmail.com>
2018-02-24 20:03:57 -0700

    Fixed formating
    Also removed some complexities.

--- server/commands/freerole.js
+++ server/commands/freerole.js
@@ -1,29 +1,26 @@
-var joinableroles = ["[F] QOTD", "[F] ANN", "[F] GW"];
-
 module.exports = {
 	id: "freerole",
 	load: () => {},
 	execute: (call) => {
+		var finput = call.params.readRaw();
 		var role = call.params.readRole(true, (candidate) => { return candidate.name.startsWith("[F] "); });
 		if (role !== null) {
-			var finupt = role.name
-			if (joinableroles.includes(finput)) {
-				if(call.message.member.roles.has(role.id)) {
-					call.message.member.removeRole(role).then(() => {
-						call.message.channel.send(`Successfully removed the \`${finput}\` free role from you.`);
-					}).catch(() => {
-						call.message.channel.send(`Unable to remove the \`${finput}\` free role from you!`);
-					});
-				} else {
-					call.message.member.addRole(role).then(() => {
-						call.message.channel.send(`Successfully added the \`${finput}\` free role to you.`);
-					}).catch(() => {
-						call.message.channel.send(`Unable to add the \`${finput}\` free role to you!`);
-					});
-				}
+			finput = role.name;
+			if(call.message.member.roles.has(role.id)) {
+				call.message.member.removeRole(role).then(() => {
+					call.message.channel.send(`Successfully removed the \`${finput}\` free role from you.`);
+				}).catch(() => {
+					call.message.channel.send(`Unable to remove the \`${finput}\` free role from you!`);
+				});
 			} else {
-				call.message.channel.send(`\`${call.params.readRaw()} \` is not a valid freerole option`);
+				call.message.member.addRole(role).then(() => {
+					call.message.channel.send(`Successfully added the \`${finput}\` free role to you.`);
+				}).catch(() => {
+					call.message.channel.send(`Unable to add the \`${finput}\` free role to you!`);
+				});
 			}
+		} else {
+			call.message.channel.send(`\`${finput} \` is not a valid freerole option`);
 		}
 	}
 };
