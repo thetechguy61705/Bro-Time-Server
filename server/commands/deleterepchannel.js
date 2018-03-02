2fb5e58fe3a079facaf8d6efb515a51cd5fa7ca4
Yikey <gtc122004@gmail.com>
2018-03-02 15:53:21 -0500

    Create deleterepchannel.js

---
+++ server/commands/deleterepchannel.js
@@ -0,0 +1,8 @@
+var Discord = require("discord.js");
+var bot = new Discord.Client();
+// deletes  messages from #deleterepchannel if user is not Mantaro. This is for an event!!
+bot.on("message", message => {
+  if (message.channel.id == 419221619550978058 && message.author.id != 213466096718708737) {
+    message.delete();
+  }
+});
