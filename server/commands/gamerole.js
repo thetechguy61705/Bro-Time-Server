bfac7494239205621a6fe6d05068176926b5785d
Yikey <gtc122004@gmail.com>
2018-03-01 20:37:04 -0500

    Update gamerole.js

--- server/commands/gamerole.js
+++ server/commands/gamerole.js
@@ -7,24 +7,24 @@ module.exports = {
 	id: "gamerole",
 	load: () => {},
 	execute: (call) => {
-		var prms = call.params
-		var role = prms.readRole()
+		var prms = call.params;
+		var role = prms.readRole();
 		var rawinput = call.message.content.substr(10);
 		if (games.includes(rawinput.toLowerCase())) {
-				if (call.message.member.roles.has(role.id)) {
-					call.message.member.removeRole(role).then(() => {
-						call.message.channel
-							.send(`Since you already had the \`${rawinput}\` game role, it has been removed from you!`);
-					}).catch(() => {
-						call.message.channel.send(`Unable to remove the \`${rawinput}\` game role!`);
-					});
-				} else {
-					call.message.member.addRole(role).then(() => {
-						call.message.channel.send(`Successfully given you the \`${rawinput}\` game role!`);
-					}).catch(() => {
-						call.message.channel.send(`Unable to give you the \`${rawinput}\` game role!`);
-					});
-				}
+			if (call.message.member.roles.has(role.id)) {
+				call.message.member.removeRole(role).then(() => {
+					call.message.channel
+						.send(`Since you already had the \`${rawinput}\` game role, it has been removed from you!`);
+				}).catch(() => {
+					call.message.channel.send(`Unable to remove the \`${rawinput}\` game role!`);
+				});
+			} else {
+				call.message.member.addRole(role).then(() => {
+					call.message.channel.send(`Successfully given you the \`${rawinput}\` game role!`);
+				}).catch(() => {
+					call.message.channel.send(`Unable to give you the \`${rawinput}\` game role!`);
+				});
+			}
 		} else {
 			call.message.channel
 				.send(`\`${rawinput} \` is not a valid game option.`);
