a41b38d2bd47605c1284a0e8708034735e0eb849
Yikey <gtc122004@gmail.com>
2018-03-07 13:17:40 -0500

    should fix !postqotd not pinging role

--- server/commands/postqotd.js
+++ server/commands/postqotd.js
@@ -6,9 +6,12 @@ module.exports = {
 			let ann = call.message.guild.channels.get("330920609435353090");
 			let qotd = call.params.readRaw(" ");
 			let qotdrole = call.message.guild.roles.get("387375439745908747");
-			qotdrole.setMentionable(true);
-			ann.send(`<@&387375439745908747>: **${qotd}**\n*Posted by ${call.message.author}*`);
-			qotdrole.setMentionable(false);
+			qotdrole.setMentionable(true).then(() => {
+				ann.send(`<@&387375439745908747>: **${qotd}**\n*Posted by ${call.message.author}*`);
+				qotdrole.setMentionable(false);
+			}).catch(() => {
+				call.message.channel.send("Something went wrong and I couldn't send the QoTD);
+			});
 
 		}
 	}
