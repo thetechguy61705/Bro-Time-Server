43de991a962d5500b05664562f6670c5da70b7c8
Yikey <gtc122004@gmail.com>
2018-03-11 20:48:36 -0400

    Update mt.js

--- server/commands/mt.js
+++ server/commands/mt.js
@@ -2,17 +2,14 @@ module.exports = {
 	id: "mt",
 	load: () => {},
 	execute: (call) => {
-		let rolename = call.params.readRaw(" ").toLowerCase();
-		const prefixes = ["", "[f] ", "[c] "];
+		let role = call.params.readRole();
 		if(call.message.member.roles
-			.some(r=>["330919872630358026", "402175094312665098", "395265037356236810", "387768886096953355"]
-				.includes(r.id)) ) {
-			for (const prefix of prefixes) {
-				let role = call.message.guild.roles.find(r=> r.name.toLowerCase() === prefix+rolename);
-				if(role) role.setMentionable(!role.mentionable);
-			}
+		.some(r=>["330919872630358026", "402175094312665098", "395265037356236810", "387768886096953355"]
+			.includes(r.id)) ) {
+			if (!role) return call.message.reply("Invalid role!");
+			role.setMentionable(!role.mentionable);
 		} else {
-			call.message.channel.send(`${call.message.author}, you do not have permission to use this command!`);
+			call.message.reply("You do not have permission to use this command!");
 		}
 		call.message.delete();
 	}
