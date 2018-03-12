899f0a21fe255de9567edc014623fa519b515df9
Yikey <gtc122004@gmail.com>
2018-03-11 20:50:30 -0400

    Update mt.js

--- server/commands/mt.js
+++ server/commands/mt.js
@@ -4,8 +4,8 @@ module.exports = {
 	execute: (call) => {
 		let role = call.params.readRole();
 		if(call.message.member.roles
-		.some(r=>["330919872630358026", "402175094312665098", "395265037356236810", "387768886096953355"]
-			.includes(r.id)) ) {
+			.some(r=>["330919872630358026", "402175094312665098", "395265037356236810", "387768886096953355"]
+				.includes(r.id)) ) {
 			if (!role) return call.message.reply("Invalid role!");
 			role.setMentionable(!role.mentionable);
 		} else {
