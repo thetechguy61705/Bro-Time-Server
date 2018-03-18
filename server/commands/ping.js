31e7284b698bd28f209af4014a99191c406fa66a
Yikey <gtc122004@gmail.com>
2018-03-17 23:22:13 -0400

    Made ping whole number

--- server/commands/ping.js
+++ server/commands/ping.js
@@ -1,11 +1,8 @@
-const DECIMAL_PLACES = 3;
-
 module.exports = {
 	id: "ping",
 	load: () => {},
 	execute: (call) => {
-		var mult = Math.pow(10, DECIMAL_PLACES);
-		var delay = Math.round(call.client.ping*mult)/mult;
+		var delay = Math.round(call.client.ping);
 		var diag;
 		if (delay <= 0) {
 			diag = "impossible";
