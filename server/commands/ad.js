d499245d1b12341300c7d357000abe795ad8f8e1
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-21 21:13:01 -0400

    Update ad.js

--- server/commands/ad.js
+++ server/commands/ad.js
@@ -3,15 +3,16 @@ module.exports = {
 	load: () => {},
 	execute: (call) => {
 		var choice = call.params.readRaw();
-		var string1 = "__Bro Time__ \nWant to join a **fun**, **engaging** community? Do you enjoy participating in **giveaways**, posting **memes**, creating **stories**," 
-		var string2 = "talking with other **developers**, Playing **games** with bots, listening to **music**, joining **game nights**, or answering **QOTDs**?"
-		var string3 = "\nThen You Should **Join The Community Today!** \nhttps://discord.gg/rjM8wdZ"
+		var string1 = "__Bro Time__ \nWant to join a **fun**, **engaging** community?";
+		var string1B = "Do you enjoy participating in **giveaways**, posting **memes**, creating **stories**,";
+		var string2 = "talking with other **developers**, Playing **games** with bots, listening to **music**, joining **game nights**, or answering **QOTDs**?";
+		var string3 = "\nThen You Should **Join The Community Today!** \nhttps://discord.gg/rjM8wdZ";
 		if(choice.toLowerCase() === "computer") {
-			call.message.channel.send(`\`\`\`${string1} ${string2} ${string3}\`\`\``);
+			call.message.channel.send(`\`\`\`${string1} ${string1B} ${string2} ${string3}\`\`\``);
 		} else if(choice.toLowerCase() === "mobile") {
-			call.message.channel.send(`${string1} ${string2} ${string3}`);
+			call.message.channel.send(`${string1} ${string1B} ${string2} ${string3}`);
 		} else {
-			call.message.channel.send(`\`\`\`${string1} ${string2} ${string3}\`\`\``);
+			call.message.channel.send(`\`\`\`${string1} ${string1B} ${string2} ${string3}\`\`\``);
 		}
 	}
 };
