a72522caa47efa16fb9fedf5dd2d5b41e7b00c5b
Yikey <gtc122004@gmail.com>
2018-03-21 20:47:59 -0400

    Update ping.js

--- server/commands/ping.js
+++ server/commands/ping.js
@@ -19,6 +19,6 @@ module.exports = {
 		} else {
 			diag = "worse than dial up";
 		}
-		call.message.channel.send(`Pong! Recieved in \`${delay}ms\``);
+		call.message.channel.send(`Pong \`${delay}ms: ${diag}\`!`);
 	}
 };
