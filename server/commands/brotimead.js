0917dcba465b952702d2610ba9baf5da1f0f5231
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-21 20:54:06 -0400

    Create brotimead.js

---
+++ server/commands/brotimead.js
@@ -0,0 +1,16 @@
+module.exports = {
+	id: "freerole",
+	load: () => {},
+	execute: (call) =>
+	var choice = call.params.readRaw();
+	if(!choice) || (choice === "computer") {
+	call.message.channel.send("\`\`\`__Bro Time__ \nWant to join a **fun**, **engaging** community? Do you enjoy participating in **giveaways**, posting **memes**, creating **stories**, talking with other **developers**, Playing **games** with bots, listening to **music**, joining **game nights**, or answering **QOTDs**? \nThen You Should **Join The Community Today!** \nhttps://discord.gg/rjM8wdZ\`\`\`");
+	}
+	if(choice === "mobile") {
+	call.message.channel.send("__Bro Time__ \nWant to join a **fun**, **engaging** community? Do you enjoy participating in **giveaways**, posting **memes**, creating **stories**, talking with other **developers**, Playing **games** with bots, listening to **music**, joining **game nights**, or answering **QOTDs**? \nThen You Should **Join The Community Today!** \nhttps://discord.gg/rjM8wdZ");
+}
+}
+};
+return message.channel.send("```__Bro Time__ \nWant to join a **fun**, **engaging** community? Do you enjoy participating in **giveaways**, posting **memes**, creating **stories**, talking with other **developers**, Playing **games** with bots, listening to **music**, joining **game nights**, or answering **QOTDs**? \nThen You Should **Join The Community Today!** \nhttps://discord.gg/rjM8wdZ```")
+
+
