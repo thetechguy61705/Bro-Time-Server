365ed48d9b5446f3a0d505d8aec994708971215c
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-21 21:09:38 -0400

    Update ad.js

--- server/commands/ad.js
+++ server/commands/ad.js
@@ -6,11 +6,12 @@ module.exports = {
 		var string1 = "__Bro Time__ \nWant to join a **fun**, **engaging** community? Do you enjoy participating in **giveaways**, posting **memes**, creating **stories**," 
 		var string2 = "talking with other **developers**, Playing **games** with bots, listening to **music**, joining **game nights**, or answering **QOTDs**?"
 		var string3 = "\nThen You Should **Join The Community Today!** \nhttps://discord.gg/rjM8wdZ"
-		if(!choice) || (choice.toLowerCase() === "computer") {
+		if(choice.toLowerCase() === "computer") {
 			call.message.channel.send(`\`\`\`${string1} ${string2} ${string3}\`\`\``);
-		}
-		if(choice.toLowerCase() === "mobile") {
+		} else if(choice.toLowerCase() === "mobile") {
 			call.message.channel.send(`${string1} ${string2} ${string3}`);
+		} else {
+			call.message.channel.send(`\`\`\`${string1} ${string2} ${string3}\`\`\``);
 		}
 	}
 };
