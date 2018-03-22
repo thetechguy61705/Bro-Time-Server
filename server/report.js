6061a432f0c7ffd1e3f0108149af172514e67086
Colton Loewen <loewencjay@gmail.com>
2018-03-21 23:33:53 -0600

    Referenced proper Raven report function

--- server/report.js
+++ server/report.js
@@ -1,4 +1,4 @@
 var DSN = process.env.SENTRY_DSN;
 module.exports = DSN != null ?
-	require("raven").config(`https://${DSN}@sentry.io/286240`).install() :
+	require("raven").config(`https://${DSN}@sentry.io/286240`).install().captureException :
 	console.warn;
