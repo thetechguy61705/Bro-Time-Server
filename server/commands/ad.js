8e9d233e58d50f095b755c76e43da123451fccb3
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-21 21:04:05 -0400

    Update ad.js

--- server/commands/ad.js
+++ server/commands/ad.js
@@ -3,7 +3,7 @@ module.exports = {
 	load: () => {},
 	execute: (call) => {
 		var choice = call.params.readRaw();
-		if(!choice) || (choice.toLowerCase() === "computer") {
+		if((!choice) || (choice.toLowerCase() === "computer")) {
 			call.message.channel.send("\`\`\`__Bro Time__ \nWant to join a **fun**, **engaging** community? Do you enjoy participating in **giveaways**, posting **memes**, creating **stories**, talking with other **developers**, Playing **games** with bots, listening to **music**, joining **game nights**, or answering **QOTDs**? \nThen You Should **Join The Community Today!** \nhttps://discord.gg/rjM8wdZ\`\`\`");
 		}
 		if(choice.toLowerCase() === "mobile") {
