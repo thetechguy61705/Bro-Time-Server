394733d14730166bcd7fb6856089ddff84c121f1
Yikey <gtc122004@gmail.com>
2018-03-27 22:05:28 -0400

    Update info.js

--- server/commands/info.js
+++ server/commands/info.js
@@ -11,7 +11,7 @@ async function awaitReply(message, question, limit = 60000) {
 			errors: ["time"]
 		});
 		return collected.first().content;
-	} catch (error) {
+	} catch(error) {
 		return false;
 	}
 }
@@ -19,24 +19,24 @@ async function awaitReply(message, question, limit = 60000) {
 function ad(message, prompt) {
 	var toSend;
 	fs.readFile(__dirname + "/../info/ad.md", (err, data) => {
- 		if (err) {
- 			throw err;
- 		} else {
-			if (prompt = "computer") toSend = `\`\`\`${data.toString("utf8")}\`\`\``;
- 			message.channel.send(toSend);
- 		}
- 	});
+		if(err) {
+			throw err;
+		} else {
+			if(prompt == "computer") toSend = `\`\`\`${data.toString("utf8")}\`\`\``;
+			message.channel.send(toSend);
+		}
+	});
 }
 
 async function gameRoles(message, Discord, prompt, param) {
-	if (param !== undefined) prompt = param;
+	if(param !== undefined) prompt = param;
 	var currentRole;
 	var games = ["`Roblox`", "`Minecraft`", "`Cuphead`", "`Fortnite`", "`Undertale`", "`Unturned`", "`VRChat`",
 		"`PUBG`", "`FNAF`", "`Clash of Clans`", "`Clash Royale`", "`Sims`", "`Terraria`", "`Subnautica`", "`Rocket League`",
 		"`Portal`", "`Hat in Time`", "`CSGO`", "`Splatoon`", "`Mario`", "`Starbound`", "`Garry's Mod`", "`Overwatch`",
 		"`Call of Duty`", "`Destiny`"
 	];
-	if (prompt.toLowerCase() === "preview") {
+	if(prompt.toLowerCase() === "preview") {
 		var gameRoleEmbed = new Discord.RichEmbed()
 			.setTitle(games[0])
 			.setDescription(`Players: \`${message.guild.roles.find("name", games[0].substr(1).slice(0, -1)).members.size}\``)
@@ -44,7 +44,7 @@ async function gameRoles(message, Discord, prompt, param) {
 		var emojiArray = ["◀", "▶"];
 		message.channel.send(gameRoleEmbed).then(async function(embedMessage) {
 			var orderLoop = 0;
-			while (orderLoop != emojiArray.length) {
+			while(orderLoop != emojiArray.length) {
 				await embedMessage.react(emojiArray[orderLoop]);
 				orderLoop = orderLoop + 1;
 			}
@@ -54,14 +54,14 @@ async function gameRoles(message, Discord, prompt, param) {
 			});
 			var emojiNumber = 0;
 			reactions.on("collect", async function(reaction) {
-				if (reaction.emoji.name === emojiArray[0]) {
-					if (emojiNumber !== 0) {
+				if(reaction.emoji.name === emojiArray[0]) {
+					if(emojiNumber !== 0) {
 						emojiNumber = emojiNumber - 1;
 					} else {
 						emojiNumber = games.length - 1;
 					}
-				} else if (reaction.emoji.name === emojiArray[1]) {
-					if (emojiNumber !== games.length - 1) {
+				} else if(reaction.emoji.name === emojiArray[1]) {
+					if(emojiNumber !== games.length - 1) {
 						emojiNumber = emojiNumber + 1;
 					} else {
 						emojiNumber = 0;
@@ -79,12 +79,12 @@ async function gameRoles(message, Discord, prompt, param) {
 			});
 			reactions.on("end", () => embedMessage.edit("Interactive command ended: 2 minutes passed."));
 		});
-	} else if (prompt.toLowerCase() === "specify") {
+	} else if(prompt.toLowerCase() === "specify") {
 		var prompt2 = await awaitReply(message, "What game role do you want info on?");
 		prompt2 = games.find(function(role) {
 			return role.toLowerCase().substr(1).slice(0, -1).startsWith(prompt2.toLowerCase());
 		});
-		if (prompt2 !== undefined) {
+		if(prompt2 !== undefined) {
 			currentRole = message.guild.roles.find("name", prompt2.substr(1).slice(0, -1));
 			let roleEmbed = new Discord.RichEmbed()
 				.setTitle(prompt2.substr(1).slice(0, -1))
@@ -109,7 +109,7 @@ async function gameRoles(message, Discord, prompt, param) {
 }
 
 async function nameColors(message, Discord, prompt, param) {
-	if (param !== undefined) prompt = param;
+	if(param !== undefined) prompt = param;
 	var currentRole;
 	var endMessage = ["**Free**", "`Red`", "`Yellow`", "`Blue`", "`Orange`", "`Green`", "`Black`", "`Purple`",
 		"**Plus**", "`Pink`", "`Indigo`", "`Bronze`", "`HotPink`", "`Cyan`", "`LightGreen`", "**Premium**",
@@ -119,7 +119,7 @@ async function nameColors(message, Discord, prompt, param) {
 		"`Pink`", "`Indigo`", "`Bronze`", "`HotPink`", "`Cyan`", "`LightGreen`",
 		"`Silver`", "`BrightRed`", "`DarkViolet`", "`HotBrown`", "`DarkGreen`", "`Gold`"
 	];
-	if (prompt.toLowerCase() === "preview") {
+	if(prompt.toLowerCase() === "preview") {
 		currentRole = message.guild.roles.find("name", colorRoles[0].substr(1).slice(0, -1));
 		var nameColorEmbed = new Discord.RichEmbed()
 			.setTitle(colorRoles[0])
@@ -128,7 +128,7 @@ async function nameColors(message, Discord, prompt, param) {
 		var emojiArray = ["◀", "▶"];
 		message.channel.send(nameColorEmbed).then(async function(embedMessage) {
 			var orderLoop = 0;
-			while (orderLoop != emojiArray.length) {
+			while(orderLoop != emojiArray.length) {
 				await embedMessage.react(emojiArray[orderLoop]);
 				orderLoop = orderLoop + 1;
 			}
@@ -138,14 +138,14 @@ async function nameColors(message, Discord, prompt, param) {
 			});
 			var emojiNumber = 0;
 			reactions.on("collect", async function(reaction) {
-				if (reaction.emoji.name === emojiArray[0]) {
-					if (emojiNumber !== 0) {
+				if(reaction.emoji.name === emojiArray[0]) {
+					if(emojiNumber !== 0) {
 						emojiNumber = emojiNumber - 1;
 					} else {
 						emojiNumber = colorRoles.length - 1;
 					}
-				} else if (reaction.emoji.name === emojiArray[1]) {
-					if (emojiNumber !== colorRoles.length - 1) {
+				} else if(reaction.emoji.name === emojiArray[1]) {
+					if(emojiNumber !== colorRoles.length - 1) {
 						emojiNumber = emojiNumber + 1;
 					} else {
 						emojiNumber = 0;
@@ -163,12 +163,12 @@ async function nameColors(message, Discord, prompt, param) {
 			});
 			reactions.on("end", () => embedMessage.edit("Interactive command ended: 2 minutes passed."));
 		});
-	} else if (prompt.toLowerCase() === "specify") {
+	} else if(prompt.toLowerCase() === "specify") {
 		var prompt2 = await awaitReply(message, "What name color role do you want info on?");
 		prompt2 = colorRoles.find(function(role) {
 			return role.toLowerCase().substr(1).slice(0, -1).startsWith(prompt2.toLowerCase());
 		});
-		if (prompt2 !== undefined) {
+		if(prompt2 !== undefined) {
 			currentRole = message.guild.roles.find("name", prompt2.substr(1).slice(0, -1));
 			let roleEmbed = new Discord.RichEmbed()
 				.setTitle(prompt2.substr(1).slice(0, -1))
@@ -193,10 +193,11 @@ async function nameColors(message, Discord, prompt, param) {
 }
 
 async function howToGetRole(message, Discord, prompt, param) {
-	if (param !== undefined) prompt = param;
+	if(param !== undefined) prompt = param;
 	var currentRole;
 	var obtainableRoles = ["`Story Teller`", "`Dolphin`", "`Meme Master`", "`Inviter`", "`Pro Inviter`", "`Cuber`", "`Artist`", "`Partner`",
-		"`Contributor`", "`Donator`"];
+		"`Contributor`", "`Donator`"
+	];
 	var descriptions = ["Get part of your story on the starboard.",
 		"Tell cj your knowledge about dolphin’s dark deeds and he shall decide if you are worthy of the role.",
 		"Get one of your memes on the starboard.",
@@ -204,7 +205,7 @@ async function howToGetRole(message, Discord, prompt, param) {
 		"Get one of your art pieces on the starboard.", "Be the owner of a discord server partnered with us.", "Contribute to the server or assets relating to it",
 		"Donate at least 1 dollar, payment instructions in !info donate"
 	];
-	if (prompt.toLowerCase() === "preview") {
+	if(prompt.toLowerCase() === "preview") {
 		var roleEmbed = new Discord.RichEmbed()
 			.setTitle(obtainableRoles[0])
 			.setDescription(`Members: \`${message.guild.roles.find("name", obtainableRoles[0].substr(1).slice(0, -1)).members.size}\`\nObtain: \`${descriptions[0]}\``)
@@ -212,7 +213,7 @@ async function howToGetRole(message, Discord, prompt, param) {
 		var emojiArray = ["◀", "▶"];
 		message.channel.send(roleEmbed).then(async function(embedMessage) {
 			var orderLoop = 0;
-			while (orderLoop != emojiArray.length) {
+			while(orderLoop != emojiArray.length) {
 				await embedMessage.react(emojiArray[orderLoop]);
 				orderLoop = orderLoop + 1;
 			}
@@ -222,14 +223,14 @@ async function howToGetRole(message, Discord, prompt, param) {
 			});
 			var emojiNumber = 0;
 			reactions.on("collect", async function(reaction) {
-				if (reaction.emoji.name === emojiArray[0]) {
-					if (emojiNumber !== 0) {
+				if(reaction.emoji.name === emojiArray[0]) {
+					if(emojiNumber !== 0) {
 						emojiNumber = emojiNumber - 1;
 					} else {
 						emojiNumber = obtainableRoles.length - 1;
 					}
-				} else if (reaction.emoji.name === emojiArray[1]) {
-					if (emojiNumber !== obtainableRoles.length - 1) {
+				} else if(reaction.emoji.name === emojiArray[1]) {
+					if(emojiNumber !== obtainableRoles.length - 1) {
 						emojiNumber = emojiNumber + 1;
 					} else {
 						emojiNumber = 0;
@@ -247,12 +248,12 @@ async function howToGetRole(message, Discord, prompt, param) {
 			});
 			reactions.on("end", () => embedMessage.edit("Interactive command ended: 2 minutes passed."));
 		});
-	} else if (prompt.toLowerCase() === "specify") {
+	} else if(prompt.toLowerCase() === "specify") {
 		var prompt2 = await awaitReply(message, "What obtainable role do you want info on?");
 		prompt2 = obtainableRoles.find(function(role) {
 			return role.toLowerCase().substr(1).slice(0, -1).startsWith(prompt2.toLowerCase());
 		});
-		if (prompt2 !== undefined) {
+		if(prompt2 !== undefined) {
 			currentRole = message.guild.roles.find("name", prompt2.substr(1).slice(0, -1));
 			let roleEmbed = new Discord.RichEmbed()
 				.setTitle(prompt2.substr(1).slice(0, -1))
@@ -277,14 +278,14 @@ async function howToGetRole(message, Discord, prompt, param) {
 }
 
 async function levelRoles(message, Discord, prompt, param) {
-	if (param !== undefined) prompt = param;
+	if(param !== undefined) prompt = param;
 	var currentRole;
 	var levelRoles = ["`True Bro`", "`OP Elite Bro`", "`Cool Elite Bro`", "`Elite Bro`", "`OP Legendary Bro`", "`Cool Legendary Bro`",
 		"`Legendary Bro`", "`OP Epic Bro`", "`Cool Epic Bro`", "`Epic Bro`", "`OP Senior Bro`", "`Cool Senior Bro`", "`Senior Bro`", "`OP Bro`",
 		"`Cool Bro`", "`Bro`", "`OP Junior Bro`", "`Cool Junior Bro`", "`Junior Bro`", "`Newbie Bro`"
 	];
 	var level = ["61", "60", "55", "51", "50", "45", "41", "40", "35", "31", "30", "25", "21", "20", "15", "11", "10", "5", "1", "0"];
-	if (prompt.toLowerCase() === "preview") {
+	if(prompt.toLowerCase() === "preview") {
 		var roleEmbed = new Discord.RichEmbed()
 			.setTitle(levelRoles[0])
 			.setDescription(`Members: \`${message.guild.roles.find("name", levelRoles[0].substr(1).slice(0, -1)).members.size}\`\nObtain: \`level ${level[0]}\``)
@@ -292,7 +293,7 @@ async function levelRoles(message, Discord, prompt, param) {
 		var emojiArray = ["◀", "▶"];
 		message.channel.send(roleEmbed).then(async function(embedMessage) {
 			var orderLoop = 0;
-			while (orderLoop != emojiArray.length) {
+			while(orderLoop != emojiArray.length) {
 				await embedMessage.react(emojiArray[orderLoop]);
 				orderLoop = orderLoop + 1;
 			}
@@ -302,14 +303,14 @@ async function levelRoles(message, Discord, prompt, param) {
 			});
 			var emojiNumber = 0;
 			reactions.on("collect", async function(reaction) {
-				if (reaction.emoji.name === emojiArray[0]) {
-					if (emojiNumber !== 0) {
+				if(reaction.emoji.name === emojiArray[0]) {
+					if(emojiNumber !== 0) {
 						emojiNumber = emojiNumber - 1;
 					} else {
 						emojiNumber = levelRoles.length - 1;
 					}
-				} else if (reaction.emoji.name === emojiArray[1]) {
-					if (emojiNumber !== levelRoles.length - 1) {
+				} else if(reaction.emoji.name === emojiArray[1]) {
+					if(emojiNumber !== levelRoles.length - 1) {
 						emojiNumber = emojiNumber + 1;
 					} else {
 						emojiNumber = 0;
@@ -327,12 +328,12 @@ async function levelRoles(message, Discord, prompt, param) {
 			});
 			reactions.on("end", () => embedMessage.edit("Interactive command ended: 2 minutes passed."));
 		});
-	} else if (prompt.toLowerCase() === "specify") {
+	} else if(prompt.toLowerCase() === "specify") {
 		var prompt2 = await awaitReply(message, "What level role do you want info on?");
 		prompt2 = levelRoles.find(function(role) {
 			return role.toLowerCase().substr(1).slice(0, -1).startsWith(prompt2.toLowerCase());
 		});
-		if (prompt2 !== undefined) {
+		if(prompt2 !== undefined) {
 			currentRole = message.guild.roles.find("name", prompt2.substr(1).slice(0, -1));
 			let roleEmbed = new Discord.RichEmbed()
 				.setTitle(prompt2.substr(1).slice(0, -1))
@@ -358,7 +359,7 @@ async function levelRoles(message, Discord, prompt, param) {
 
 function donate(message) {
 	fs.readFile(__dirname + "/../info/donateinfo.md", (err, data) => {
-		if (err) {
+		if(err) {
 			throw err;
 		} else {
 			message.channel.send(data.toString("utf8"));
@@ -367,38 +368,38 @@ function donate(message) {
 }
 
 async function infoTarget(message, prompt, Discord, choice, param) {
-	if (choice.toLowerCase() === "ad" || choice.toLowerCase() === "advertisement") {
-		if (param !== "mobile" && param !== "computer") {
+	if(choice.toLowerCase() === "ad" || choice.toLowerCase() === "advertisement") {
+		if(param !== "mobile" && param !== "computer") {
 			prompt = await awaitReply(message, "Would you like to view the `mobile` ad (not in code block) or `computer` ad (in code block)? Default: Computer");
 			ad(message, prompt, undefined);
 		} else {
 			ad(message, prompt, param);
 		}
-	} else if (choice.toLowerCase() === "gameroles" || choice.toLowerCase() === "gamerole") {
-		if (param !== "preview" && param !== "list" && param !== "specify") {
+	} else if(choice.toLowerCase() === "gameroles" || choice.toLowerCase() === "gamerole") {
+		if(param !== "preview" && param !== "list" && param !== "specify") {
 			prompt = await awaitReply(message, "Would you like to `preview` all game roles, view a `list` of game roles or `specify` a specific role? Default: List");
 			gameRoles(message, Discord, prompt, undefined);
 		} else {
 			gameRoles(message, Discord, prompt, param);
 		}
-	} else if (choice.toLowerCase() === "namecolors" || choice.toLowerCase() === "colors") {
-		if (param !== "preview" && param !== "list" && param !== "specify") {
+	} else if(choice.toLowerCase() === "namecolors" || choice.toLowerCase() === "colors") {
+		if(param !== "preview" && param !== "list" && param !== "specify") {
 			prompt = await awaitReply(message, "Would you like to `preview` all color roles, view a `list` of color roles or `specify` a specific role? Default: List");
 			nameColors(message, Discord, prompt, undefined);
 		} else {
 			nameColors(message, Discord, prompt, param);
 		}
-	} else if (choice.toLowerCase() === "getrole" || choice.toLowerCase() === "howtogetrole" || choice.toLowerCase() === "htgr") {
-		if (param !== "preview" && param !== "list" && param !== "specify") {
+	} else if(choice.toLowerCase() === "getrole" || choice.toLowerCase() === "howtogetrole" || choice.toLowerCase() === "htgr") {
+		if(param !== "preview" && param !== "list" && param !== "specify") {
 			prompt = await awaitReply(message, "Would you like to `preview` all obtainable roles, view a `list` of roles or `specify` a specific role? Default: List");
 			howToGetRole(message, Discord, prompt, undefined);
 		} else {
 			howToGetRole(message, Discord, prompt, param);
 		}
-	} else if (choice.toLowerCase() === "donate" || choice.toLowerCase() === "donations" || choice.toLowerCase() === "donateinfo") {
+	} else if(choice.toLowerCase() === "donate" || choice.toLowerCase() === "donations" || choice.toLowerCase() === "donateinfo") {
 		donate(message);
-	} else if (choice.toLowerCase() === "levelroles" || choice.toLowerCase() === "levels") {
-		if (param !== "preview" && param !== "list" && param !== "specify") {
+	} else if(choice.toLowerCase() === "levelroles" || choice.toLowerCase() === "levels") {
+		if(param !== "preview" && param !== "list" && param !== "specify") {
 			prompt = await awaitReply(message, "Would you like to `preview` all obtainable roles, view a `list` of roles or `specify` a specific role? Default: List");
 			levelRoles(message, Discord, prompt, undefined);
 		} else {
@@ -419,19 +420,19 @@ module.exports = {
 			"donate", "donations", "donateinfo", "levelroles", "levels"
 		];
 		var plainOptions = ["advertisement", "gameroles", "namecolors", "howtogetrole", "donate", "levelroles"];
-		if (choice === null) {
-			choice = awaitReply (call.message, `Specify the information you want. Choices: \`${plainOptions.join("`, `")}\`.`).then(choice => {
+		if(choice === null) {
+			choice = awaitReply(call.message, `Specify the information you want. Choices: \`${plainOptions.join("`, `")}\`.`).then(choice => {
 				var choice = options.find(function(option) {
 					return option.toLowerCase().startsWith(choice.toLowerCase());
 				});
-				if (choice === undefined) return call.message.reply(`Invalid choice. Choices are: \`${plainOptions.join("`, `")}\`.`);
-				infoTarget (mcall.essage, prompt, Discord, choice, param);
+				if(choice === undefined) return call.message.reply(`Invalid choice. Choices are: \`${plainOptions.join("`, `")}\`.`);
+				infoTarget(mcall.essage, prompt, Discord, choice, param);
 			});
 		} else {
 			var choice = options.find(function(option) {
 				return option.toLowerCase().startsWith(choice.toLowerCase());
 			});
-			if (choice === undefined) return call.message.reply(`Invalid choice. Choices are: \`${plainOptions.join("`, `")}\`.`);
+			if(choice === undefined) return call.message.reply(`Invalid choice. Choices are: \`${plainOptions.join("`, `")}\`.`);
 			infoTarget(call.message, prompt, Discord, choice, param);
 		}
 	},
