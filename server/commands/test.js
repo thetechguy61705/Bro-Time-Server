cc73e5dd2ac251be8d2739f3578e3017ac57f456
Yikey <gtc122004@gmail.com>
2018-03-30 16:37:59 -0400

    Update test.js

--- server/commands/test.js
+++ server/commands/test.js
@@ -1,16 +1,16 @@
 module.exports = {
-  id: "testyoyoyo",
+	id: "testyoyoyo",
 	load: () => {},
 	execute: (call) => {
-  var commandDescs;
-    fs.readFile(__dirname + "/../info/commandinfo.md", (err, data) => {
-      if(err) {
-        throw err;
-      } else {
-        commandDescs = data.toString("utf8");
-      }
+		var commandDescs;
+		fs.readFile(__dirname + "/../info/commandinfo.md", (err, data) => {
+			if(err) {
+				throw err;
+			} else {
+				commandDescs = data.toString("utf8");
+			}
 		});
-    message.channel.send("1: "+commandDescs);
-    message.channel.send("2: "+commandDescs.split("\n"));
+		message.channel.send("1: "+commandDescs);
+		message.channel.send("2: "+commandDescs.split("\n"));
 	}
 };
