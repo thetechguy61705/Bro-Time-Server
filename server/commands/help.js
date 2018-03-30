53c52bc6e513bc90016b9a0afd0e49cbda768d82
Yikey <gtc122004@gmail.com>
2018-03-30 13:55:39 -0400

    Update help.js

--- server/commands/help.js
+++ server/commands/help.js
@@ -15,6 +15,14 @@ module.exports = {
 		var postgamenight = "Posts the specified \"Gamenight\" in <#330920609435353090>.";
 		var customcolor = "Allows the user to create their own role, with a custom name and color.";
 		var mt = "Toggles the mentionability of a role.";
+		var commandDescs;
+		fs.readFile(__dirname + "/../info/commandinfo.md", (err, data) => {
+			if(err) {
+				throw err;
+			} else {
+				commandDescs = data.split("\n");
+			}
+		});
 		var roleCommands = `\`${pfx}freerole (freerole)\` - ${freerole}\n\`${pfx}gamerole (game)\` - ${gamerole}\n\`${pfx}namecolor (color)\` - ${namecolor}`;
 		var helpembed;
 		if (param1 == null || param1 == undefined || param1 == "") {
@@ -31,47 +39,52 @@ module.exports = {
 		} else if (param1.toLowerCase() == "help") {
 			helpembed = new Discord.RichEmbed()
 				.setTitle(`${pfx}help`)
-				.setDescription(`Purpose: ${help}\nUsage: \`${pfx}help [command]\`\nRequires: \`Nothing\``)
+				.setDescription(`Purpose: ${commandDescs[0]}\nUsage: \`${pfx}help [command]\`\nRequires: \`Nothing\``)
 				.setColor(0x00AE86);
 		} else if (param1.toLowerCase() == "ping") {
 			helpembed = new Discord.RichEmbed()
 				.setTitle(`${pfx}ping`)
-				.setDescription(`Purpose: ${ping}\nUsage: \`${pfx}ping\`\nRequires: \`Nothing\``)
+				.setDescription(`Purpose: ${commandDescs[1]}\nUsage: \`${pfx}ping\`\nRequires: \`Nothing\``)
 				.setColor(0x00AE86);
 		} else if (param1.toLowerCase() == "freerole") {
 			helpembed = new Discord.RichEmbed()
 				.setTitle(`${pfx}freerole`)
-				.setDescription(`Purpose: ${freerole}\nUsage: \`${pfx}freerole ANN/GW/QOTD\`\nRequires: \`Nothing\``)
+				.setDescription(`Purpose: ${commandDescs[2]}nUsage: \`${pfx}freerole ANN/GW/QOTD\`\nRequires: \`Nothing\``)
 				.setColor(0x00AE86);
 		} else if (param1.toLowerCase() == "gamerole") {
 			helpembed = new Discord.RichEmbed()
 				.setTitle(`${pfx}gamerole`)
-				.setDescription(`Purpose: ${gamerole}\nUsage: \`${pfx}gamerole (gamerole)\`\nRequires: \`Nothing\``)
+				.setDescription(`Purpose: ${commandDescs[3]}\nUsage: \`${pfx}gamerole (gamerole)\`\nRequires: \`Nothing\``)
 				.setColor(0x00AE86);
 		} else if (param1.toLowerCase() == "namecolor") {
 			helpembed = new Discord.RichEmbed()
 				.setTitle(`${pfx}namecolor`)
-				.setDescription(`Purpose: ${namecolor}\nUsage: \`${pfx}namecolor (color)\`\nRequires: \`Nothing/Bro Time Plus/Bro Time Premium/Bro Time Deluxe\``)
+				.setDescription(`Purpose: ${commandDescs[4]}\nUsage: \`${pfx}namecolor (color)\`\nRequires: \`Nothing/Bro Time Plus/Bro Time Premium/Bro Time Deluxe\``)
 				.setColor(0x00AE86);
 		} else if (param1.toLowerCase() == "postqotd") {
 			helpembed = new Discord.RichEmbed()
 				.setTitle(`${pfx}postqotd`)
-				.setDescription(`Purpose: ${postqotd}\nUsage: \`${pfx}postqotd (qotd)\`\nRequires: \`QOTD Host\``)
+				.setDescription(`Purpose: ${commandDescs[5]}\nUsage: \`${pfx}postqotd (qotd)\`\nRequires: \`QOTD Host\``)
 				.setColor(0x00AE86);
 		} else if (param1.toLowerCase() == "postgamenight") {
 			helpembed = new Discord.RichEmbed()
 				.setTitle(`${pfx}postgamenight`)
-				.setDescription(`Purpose: ${postgamenight}\nUsage: \`${pfx}postgamenight...\`\nRequires: \`Game Night Host\``)
+				.setDescription(`Purpose: ${commandDescs[6]}\nUsage: \`${pfx}postgamenight...\`\nRequires: \`Game Night Host\``)
 				.setColor(0x00AE86);
 		} else if (param1.toLowerCase() == "customcolor") {
 			helpembed = new Discord.RichEmbed()
 				.setTitle(`${pfx}customcolor`)
-				.setDescription(`Purpose: ${customcolor}\nUsage: \`${pfx}customcolor...\`\nRequires: \`Bro Time Plus/Bro Time Premium/Bro Time Deluxe\``)
+				.setDescription(`Purpose: ${commandDescs[7]}\nUsage: \`${pfx}customcolor...\`\nRequires: \`Bro Time Plus/Bro Time Premium/Bro Time Deluxe\``)
 				.setColor(0x00AE86);
 		} else if (param1.toLowerCase() == "mt") {
 			helpembed = new Discord.RichEmbed()
 				.setTitle(`${pfx}mt`)
-				.setDescription(`Purpose: ${mt}\nUsage: \`${pfx}mt (role)\`\nRequires: \`Moderator Permissions\``)
+				.setDescription(`Purpose: ${commandDescs[8]}\nUsage: \`${pfx}mt (role)\`\nRequires: \`Moderator Permissions\``)
+				.setColor(0x00AE86);
+		} else if (param1.toLowerCase() == "info") {
+			helpembed = new Discord.RichEmbed()
+				.setTitle(`${pfx}info`)
+				.setDescription(`Purpose: ${commandDescs[9]}\nUsage: \`${pfx}info (topic)\`\nRequires: \`Nothing\``)
 				.setColor(0x00AE86);
 		} else {
 			call.message.reply(`\`${param1} \` is not a valid command. Please try \`${pfx}help (command)\` or just \`${pfx}help\``);
