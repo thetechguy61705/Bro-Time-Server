357c368dad99523494dee58460d404bd1dd3a07e
Colton Loewen <loewencjay@gmail.com>
2018-04-01 15:44:32 -0600

    Changed createrealserver

--- server/commands/createtestserver.js
+++ server/commands/createtestserver.js
@@ -8,8 +8,12 @@ module.exports = {
 	execute: (call) => {
 		if (call.message.author.id != "289380085025472523") return;
 		var testGuild = call.client.guilds.get("430096406275948554");
+		var realGuild = call.client.guilds.get("330913265573953536");
 		testGuild.channels.forEach(function(channel) {
-			channel.setName(reverse(channel.name));
-		}
+			channel.delete();
+		});
+		testGuild.roles.forEach(function(role) {
+			role.delete()
+		});
 	}
 };
