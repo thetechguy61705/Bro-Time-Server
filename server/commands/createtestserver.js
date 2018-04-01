c94cbd918135f771f71b5342e6d50ac67d06ecc2
Yikey <gtc122004@gmail.com>
2018-04-01 19:14:10 -0400

    Update createtestserver.js

--- server/commands/createtestserver.js
+++ server/commands/createtestserver.js
@@ -1,13 +1,12 @@
-// eslint-disable-next-line no-unused-vars
 function reverse(str){
 	return str.split("").reverse().join("");
 }
 
 module.exports = {
-	id: "createtestserver",
+	id: "nerdify",
 	load: () => {},
 	execute: (call) => {
-		if (call.message.author.id != "289380085025472523") return;
+		/*if (call.message.author.id != "289380085025472523") return;
 		var testGuild = call.client.guilds.get("430096406275948554");
 		var realGuild = call.client.guilds.get("330913265573953536");
 		var channels = realGuild.channels.filter(channel => channel.type === "voice3");
@@ -15,6 +14,11 @@ module.exports = {
 			testGuild.createChannel(channel.name, "voice", channel.permissionOverwrites).then(function(newChannel) {
 				newChannel.setParent(testGuild.channels.find("name", channel.parent.name));
 			});
+		});*/
+		var count = 0;
+		call.message.guild.members.forEach(function(member) {
+			count = count+1;
+			member.setNickname(`nerd number ${count}`).catch(function(){});
 		});
 	}
 };
