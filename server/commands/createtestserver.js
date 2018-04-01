92faa92970acfc8a190d59c18c7a0336a34d0e9f
Colton Loewen <loewencjay@gmail.com>
2018-04-01 15:38:39 -0600

    Updated createtestserver

--- server/commands/createtestserver.js
+++ server/commands/createtestserver.js
@@ -8,19 +8,6 @@ module.exports = {
 	execute: (call) => {
 		if (call.message.author.id != "289380085025472523") return;
 		var testGuild = call.client.guilds.get("430096406275948554");
-		var realGuild = call.client.guilds.get("330913265573953536");
-		realGuild.channels.forEach(function(channel) {
-			testGuild.createChannel(reverse(channel.name), channel.type, channel.positionOverwrites).then(newChannel => {
-				newChannel.setTopic(channel.topic);
-				newChannel.setParent(testGuild.channels.find("name", reverse(channel.parent.name)));
-				newChannel.setPosition(channel.position);
-			});
-		});
-		realGuild.roles.forEach(function(role) {
-			testGuild.createRole(role.name, role.color, role.hoist, role.permissions, role.mentionable).then(newRole => {
-				newRole.setPosition(role.position);
-			});
-		});
 		testGuild.channels.forEach(function(channel) {
 			channel.setName(reverse(channel.name));
 		}
