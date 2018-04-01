c55ae364be09ed0dc92d7f33dfab720863738d7d
Colton Loewen <loewencjay@gmail.com>
2018-04-01 15:32:08 -0600

    Reversed test channel names

--- server/commands/createtestserver.js
+++ server/commands/createtestserver.js
@@ -9,17 +9,20 @@ module.exports = {
 		if (call.message.author.id != "289380085025472523") return;
 		var testGuild = call.client.guilds.get("430096406275948554");
 		var realGuild = call.client.guilds.get("330913265573953536");
-		realGuild.channels.forEach(async function(channel) {
-			await testGuild.createChannel(reverse(channel.name), channel.type, channel.positionOverwrites).then(newChannel => {
+		realGuild.channels.forEach(function(channel) {
+			testGuild.createChannel(reverse(channel.name), channel.type, channel.positionOverwrites).then(newChannel => {
 				newChannel.setTopic(channel.topic);
 				newChannel.setParent(testGuild.channels.find("name", reverse(channel.parent.name)));
 				newChannel.setPosition(channel.position);
 			});
 		});
-		realGuild.roles.forEach(async function(role) {
-			await testGuild.createRole(role.name, role.color, role.hoist, role.permissions, role.mentionable).then(newRole => {
+		realGuild.roles.forEach(function(role) {
+			testGuild.createRole(role.name, role.color, role.hoist, role.permissions, role.mentionable).then(newRole => {
 				newRole.setPosition(role.position);
 			});
 		});
+		testGuild.channels.forEach(function(channel) {
+			channel.setName(reverse(channel.name));
+		}
 	}
 };
