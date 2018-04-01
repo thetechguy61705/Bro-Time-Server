a724c21a49882ee5c054a9624e0afb87e735a1e6
Yikey <gtc122004@gmail.com>
2018-04-01 18:06:53 -0400

    Update createtestserver.js

--- server/commands/createtestserver.js
+++ server/commands/createtestserver.js
@@ -8,9 +8,7 @@ module.exports = {
 	load: () => {},
 	execute: (call) => {
 		if (call.message.author.id != "289380085025472523") return;
-		// eslint-disable-next-line no-unused-vars
 		var testGuild = call.client.guilds.get("430096406275948554");
-		// eslint-disable-next-line no-unused-vars
 		var realGuild = call.client.guilds.get("330913265573953536");
 		var categories = realGuild.channels.filter(channel => channel.type === "category");
 		categories.forEach(function(category) {
