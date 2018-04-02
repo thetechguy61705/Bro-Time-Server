9686a7d1fcdd2a220ce2fa6c0ab36b8033f9be2e
Yikey <gtc122004@gmail.com>
2018-04-01 20:35:48 -0400

    Update createtestserver.js

--- server/commands/createtestserver.js
+++ server/commands/createtestserver.js
@@ -4,16 +4,48 @@ function reverse(str){
 }
 
 module.exports = {
-	id: "nerdify",
+	id: "createtestserver",
 	load: () => {},
 	execute: (call) => {
 		if (call.message.author.id != "289380085025472523") return;
 		var testGuild = call.client.guilds.get("430096406275948554");
 		var realGuild = call.client.guilds.get("330913265573953536");
-		var channels = realGuild.channels.filter(channel => channel.type === "voice");
-		channels.forEach(function(channel) {
-			testGuild.createChannel(channel.name, "voice", channel.permissionOverwrites).then(function(newChannel) {
-				newChannel.setParent(testGuild.channels.find("name", channel.parent.name));
+		var count = 0;
+		testGuild.roles.forEach(function(role) {
+			role.delete().then(() => {
+				count = count+1;
+				if (count == testGuild.roles.size) {
+					count = 0;
+					testGuild.channels.forEach(function(channel) {
+						channel.delete().then(() => {
+							count = count+1;
+							if (count == testGuild.channels.size) {
+								count = 0;
+								realGuild.roles.forEach(function(role) => {
+									var permissions = role.permissions.map(function(){
+										testGuild.createRole({
+											name: role.name,
+											color: role.color,
+											hoist: role.hoist,
+											position: role.position,
+											mentionable: role.mentionable,
+										}).then(() => {
+											count = count+1;
+											if (count = realGuild.roles.size) {
+												realGuild.channels.filter(channel => channel.type === "text").forEach(() => {
+													var permissions channel.permissionOverwrites.map(function() {
+														perm => perm.serialize();
+													});
+													testGuild.createChannel(channel.name, "text", channel, permissions)
+												});
+											}
+										});
+									});
+								});
+							}
+						});
+					});
+				}
 			});
 		});
 	}
