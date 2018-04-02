817aad8bae1790d78471e7b10cc4a9bae126c0a5
Yikey <gtc122004@gmail.com>
2018-04-01 23:45:11 -0400

    Update createtestserver.js

--- server/commands/createtestserver.js
+++ server/commands/createtestserver.js
@@ -7,18 +7,15 @@ module.exports = {
 	id: "createtestserver",
 	load: () => {},
 	execute: (call) => {
-		//if (call.message.author.id != "289380085025472523") return;
-		// eslint-disable-next-line no-unused-vars
+		if (call.message.author.id != "289380085025472523") return;
 		var testGuild = call.client.guilds.get("430096406275948554");
-		// eslint-disable-next-line no-unused-vars
 		var realGuild = call.client.guilds.get("330913265573953536");
-		/*realGuild.channels.forEach(async function(channel) {
-			await testGuild.createChannel(channel.name, channel.type);
-		});*/
-		realGuild.channels.forEach(async function(channel) {
-			var targetChannel = testGuild.channels.find("name", channel.name);
-			if (targetChannel.type === "text") targetChannel.setTopic(channel.topic);
-			if (targetChannel.type !== "category") targetChannel.setParent(testGuild.channels.find("name", channel.parent.name));
-		});
+		var count = 1;
+		while (count != realGuild.roles.size-1) {
+			role = realGuild.roles.array()[count];
+			testGuild.createRole(role.name, role.color, role.hoist, count).then(() {
+				count = count+1;
+			});
+		}
 	}
 };
