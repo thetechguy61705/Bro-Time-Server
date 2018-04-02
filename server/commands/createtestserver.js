1cfe012f0dba977c5a34c1485de675c4de1851c7
Yikey <gtc122004@gmail.com>
2018-04-02 00:05:45 -0400

    Update createtestserver.js

--- server/commands/createtestserver.js
+++ server/commands/createtestserver.js
@@ -7,7 +7,7 @@ module.exports = {
 	id: "createtestserver",
 	load: () => {},
 	execute: (call) => {
-		if (call.message.author.id != "289380085025472523") return;
+		if (call.message.author.id != "289380085025472523") return
 		var testGuild = call.client.guilds.get("430096406275948554");
 		var realGuild = call.client.guilds.get("330913265573953536");
 		var count = 1;
