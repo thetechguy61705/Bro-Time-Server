207a0dd006bd981705cd79997f565e5374e3f8c8
Yikey <gtc122004@gmail.com>
2018-04-01 21:55:33 -0400

    Update createtestserver.js

--- server/commands/createtestserver.js
+++ server/commands/createtestserver.js
@@ -9,7 +9,7 @@ module.exports = {
 	execute: (call) => {
 		if (call.message.author.id != "289380085025472523") return;
 		// eslint-disable-next-line no-unused-vars
-		var testGuild = call.client.guilds.get("430096406275948554");
+		var testGuild = call.client.guilds.get("430096406275948554")
 		// eslint-disable-next-line no-unused-vars
 		var realGuild = call.client.guilds.get("330913265573953536");
 		testGuild.channels.forEach(function(channel) {
