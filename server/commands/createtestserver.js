970bcd561a86c3d9584e5673a500dd6b178e2bfb
Yikey <gtc122004@gmail.com>
2018-04-01 21:54:37 -0400

    Update createtestserver.js

--- server/commands/createtestserver.js
+++ server/commands/createtestserver.js
@@ -8,12 +8,17 @@ module.exports = {
 	load: () => {},
 	execute: (call) => {
 		if (call.message.author.id != "289380085025472523") return;
+		// eslint-disable-next-line no-unused-vars
 		var testGuild = call.client.guilds.get("430096406275948554");
+		// eslint-disable-next-line no-unused-vars
 		var realGuild = call.client.guilds.get("330913265573953536");
-		realGuild.channels.forEach(async function(channel) {
+		testGuild.channels.forEach(function(channel) {
+			channel.delete();
+		});
+		/*realGuild.channels.forEach(async function(channel) {
 			var targetChannel = testGuild.channels.find("name", channel.name);
 			if (targetChannel.type === "text") targetChannel.setTopic(channel.topic);
 			if (targetChannel.type !== "category") targetChannel.setParent(testGuild.channels.find("name", channel.parent.name));
-		});
+		});*/
 	}
 };
