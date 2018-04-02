7916fa2d77f7fa4fc7692969b79470e3e365b7da
Yikey <gtc122004@gmail.com>
2018-04-02 00:15:15 -0400

    Update createtestserver.js

--- server/commands/createtestserver.js
+++ server/commands/createtestserver.js
@@ -7,11 +7,11 @@ module.exports = {
 	id: "createtestserver",
 	load: () => {},
 	execute: (call) => {
-		if (call.message.author.id != "289380085025472523") return
+		if (call.message.author.id != "289380085025472523") return;
 		var testGuild = call.client.guilds.get("430096406275948554");
 		var realGuild = call.client.guilds.get("330913265573953536");
 		var count = 1;
-		realGuild.roles.forEach(function(role) {
+		realGuild.roles.forEach(function() {
 			let realRole = realGuild.roles.array()[count];
 			testGuild.createRole(realRole.name, realRole.color, realRole.hoist, count);
 			count = count+1;
