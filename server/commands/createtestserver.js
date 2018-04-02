3baa5c3f9c22a7cabe27eeb80977a5274556f4cd
Yikey <gtc122004@gmail.com>
2018-04-02 00:22:30 -0400

    Update createtestserver.js

--- server/commands/createtestserver.js
+++ server/commands/createtestserver.js
@@ -9,12 +9,21 @@ module.exports = {
 	execute: (call) => {
 		if (call.message.author.id != "289380085025472523") return;
 		var testGuild = call.client.guilds.get("430096406275948554");
-		var realGuild = call.client.guilds.get("330913265573953536");
-		var count = 1;
-		realGuild.roles.forEach(function() {
-			let realRole = realGuild.roles.array()[count];
-			testGuild.createRole(realRole.name, realRole.color, realRole.hoist, count);
-			count = count+1;
+		//var realGuild = call.client.guilds.get("330913265573953536");
+		//var count = 1;
+		testGuild.roles.forEach(function(role) {
+			role.delete();
 		});
+		/*realGuild.roles.forEach(function() {
+			let realRole = realGuild.roles.array()[count];
+			testGuild.createRole({
+				name: realRole.name,
+				color: realRole.color,
+				hoist: realRole.hoist,
+				position: count,
+				mentionable: realRole.mentionable,
+			});
+			count = count+1;
+		});*/
 	}
 };
