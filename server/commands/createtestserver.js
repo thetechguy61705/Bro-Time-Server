a880023f03eaf38bcd62c3c9b29fb114f43c4d71
Yikey <gtc122004@gmail.com>
2018-04-02 00:39:17 -0400

    Update createtestserver.js

--- server/commands/createtestserver.js
+++ server/commands/createtestserver.js
@@ -12,13 +12,10 @@ module.exports = {
 		var testGuild = call.client.guilds.get("430096406275948554");
 		// eslint-disable-next-line no-unused-vars
 		var realGuild = call.client.guilds.get("330913265573953536");
-		realGuild.roles.forEach(function(role) {
-			testGuild.createRole({
-				name: role.name,
-				color: role.color,
-				hoist: role.hoist,
-				mentionable: role.mentionable,
-			});
+		testGuild.roles.forEach(function(role) {
+			if (role.name !== "GOD") {
+				role.setPosition(realGuild.roles.find("name", role.name).position);
+			}
 		});
 	}
 };
