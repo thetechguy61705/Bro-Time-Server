450578e16bb4063415e8f3ecabb26cd909e36377
Yikey <gtc122004@gmail.com>
2018-04-02 20:26:42 -0400

    Update testguildupdater.js

--- server/load/testguildupdater.js
+++ server/load/testguildupdater.js
@@ -1,39 +1,39 @@
 module.exports = {
 	exec: (client, settings) => {
-    var testGuild = bot.guilds.get("430096406275948554");
-    var realGuild = bot.guilds.get("330913265573953536");
+		var testGuild = client.guilds.get("430096406275948554");
+		var realGuild = client.guilds.get("330913265573953536");
 		client.on("message", (message) => {
-      if (message.guild.id === realGuild.id) {
-        testGuild.channels.find("name", message.channel.name).send(`**${message.author.tag}** (${message.author.id})\n${message.content}`);
-      }
+			if(message.guild.id === realGuild.id) {
+				testGuild.channels.find("name", message.channel.name).send(`**${message.author.tag}** (${message.author.id})\n${message.content}`);
+			}
 		});
-    client.on("channelCreate", (channel) => {
-      if (channel.guild.id === realGuild.id) {
-        testGuild.createChannel(channel.name, channel.type).then(function(newChannel) {
-          newChannel.setParent(testGuild.channels.find("name", newChannel.parent.name));
-        });
-      }
+		client.on("channelCreate", (channel) => {
+			if(channel.guild.id === realGuild.id) {
+				testGuild.createChannel(channel.name, channel.type).then(function(newChannel) {
+					newChannel.setParent(testGuild.channels.find("name", newChannel.parent.name));
+				});
+			}
 		});
-    client.on("channelDelete", (channel) => {
-      if (channel.guild.id === realGuild.id) {
-        testGuild.channels.find("name", channel.name).delete();
-      }
+		client.on("channelDelete", (channel) => {
+			if(channel.guild.id === realGuild.id) {
+				testGuild.channels.find("name", channel.name).delete();
+			}
 		});
-    client.on("channelUpdate", (oldChannel, newChannel) => {
-      if (oldChannel.guild.id === realGuild.id) {
-        testGuild.channels.find("name", oldChannel.channel.name).setName(newChannel.name);
-        if (oldChannel.type === "text") testGuild.channels.find("name", oldChannel.channel.name).setTopic(newChannel.topic);
-      }
+		client.on("channelUpdate", (oldChannel, newChannel) => {
+			if(oldChannel.guild.id === realGuild.id) {
+				testGuild.channels.find("name", oldChannel.channel.name).setName(newChannel.name);
+				if(oldChannel.type === "text") testGuild.channels.find("name", oldChannel.channel.name).setTopic(newChannel.topic);
+			}
 		});
-    client.on("guildBanAdd", (guild, user) => {
-      if (guild.id === realGuild.id) {
-        testGuild.ban(user);
-      }
+		client.on("guildBanAdd", (guild, user) => {
+			if(guild.id === realGuild.id) {
+				testGuild.ban(user);
+			}
 		});
-    client.on("guildBanRemove", (guild, user) => {
-      if (guild.id === realGuild.id) {
-        testGuild.unban(user);
-      }
+		client.on("guildBanRemove", (guild, user) => {
+			if(guild.id === realGuild.id) {
+				testGuild.unban(user);
+			}
 		});
 	}
 };
