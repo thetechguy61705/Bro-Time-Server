641047803f842b01839a4b640f12deb66ec8ce63
Yikey <gtc122004@gmail.com>
2018-04-03 23:08:44 -0400

    Update freerole.js

--- server/commands/freerole.js
+++ server/commands/freerole.js
@@ -10,28 +10,28 @@ module.exports = {
 			if (call.message.member.roles.has(role.id)) {
 				call.message.member.removeRole(role).then(() => {
 					call.message.channel.send(`Successfully removed the \`${finput}\` free role from you.`).catch(() => {
-						call.message.author.send(`Successfully removed the \`${finput}\` free role from you.`).catch();
+						call.message.author.send(`Successfully removed the \`${finput}\` free role from you.`).catch(function(){});
 					});
 				}).catch(() => {
 					call.message.channel.send(`Unable to remove the \`${finput}\` free role from you.`).catch(() => {
-						call.message.author.send(`Unable to remove the \`${finput}\` free role from you.`).catch();
+						call.message.author.send(`Unable to remove the \`${finput}\` free role from you.`).catch(function(){});
 					});
 				});
 			} else {
 				call.message.member.addRole(role).then(() => {
 					call.message.channel.send(`Successfully added the \`${finput}\` free role to you.`).catch(() => {
-						call.message.author.send(`Successfully added the \`${finput}\` free role to you.`).catch();
+						call.message.author.send(`Successfully added the \`${finput}\` free role to you.`).catch(function(){});
 					});
 				}).catch(() => {
 					call.message.channel.send(`Unable to add the \`${finput}\` free role to you.`).catch(() => {
-						call.message.author.send(`Unable to add the \`${finput}\` free role to you.`).catch();
+						call.message.author.send(`Unable to add the \`${finput}\` free role to you.`).catch(function(){});
 					});
 				});
 			}
 		} else {
 			call.message.channel.send(`\`${finput} \` is not a valid freerole option`).catch(() => {
 				call.message.author.send(`You atttempted to use the \`freerole\` command in ${call.message.channel}, but I do not have permissions to chat there.`)
-					.catch();
+					.catch(function(){});
 			});
 		}
 	}
