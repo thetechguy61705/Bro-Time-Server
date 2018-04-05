bc8ac8e03935d9b06ed64ac8483f5fa16e25293b
Yikey <gtc122004@gmail.com>
2018-04-05 15:36:08 -0400

    Update testguildupdater.js

--- server/load/testguildupdater.js
+++ server/load/testguildupdater.js
@@ -85,8 +85,8 @@ module.exports = {
 						if(oldChannel.type === "text" || oldChannel.type === "voice") {
 							testGuild.channels.find("name", newChannel.name)
 								.setParent(testGuild.channels.find("name", newChannel.parent.name)).then(() => {
-							testGuild.channels.find("name", newChannel.name).setPosition(newChannel.position + noParentChannels.size);
-							});
+									testGuild.channels.find("name", newChannel.name).setPosition(newChannel.position + noParentChannels.size);
+								});
 						}
 					});
 				}
