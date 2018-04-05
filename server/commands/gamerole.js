b01c1f0b29513ec0d52ff2663b8c62a42a22ca0d
Yikey <gtc122004@gmail.com>
2018-04-04 22:10:41 -0400

    Update gamerole.js

--- server/commands/gamerole.js
+++ server/commands/gamerole.js
@@ -9,7 +9,8 @@ module.exports = {
 	execute: (call) => {
 		var rawinput = call.params.readRaw();
 		if (rawinput === "") return call.message.reply("You must specify a gamerole!").catch(() => {
-			call.message.author.send(`You attempted to use the \`gamerole\` command in ${call.message.channel}, but I do not have permission to chat there.`).catch();
+			call.message.author.send(`You attempted to use the \`gamerole\` command in ${call.message.channel}, but I do not have permission to chat there.`)
+				.catch(function(){});
 		});
 		var game = games.find(function(g) {
 			return g.toLowerCase().startsWith(rawinput.toLowerCase());
@@ -19,21 +20,21 @@ module.exports = {
 			if (call.message.member.roles.has(role.id)) {
 				call.message.member.removeRole(role).then(() => {
 					call.message.channel.send(`Since you already had the \`${game}\` game role, it has been removed from you.`).catch(() => {
-						call.message.author.send(`Since you already had the \`${game}\` gamerole, it has been removed from you.`).catch();
+						call.message.author.send(`Since you already had the \`${game}\` gamerole, it has been removed from you.`).catch(function(){});
 					});
 				}).catch(() => {
 					call.message.channel.send(`Unable to remove the \`${game}\` game role.`).catch(() => {
-						call.message.author.send(`Unable to remove the \`${game}\` game role from you.`).catch();
+						call.message.author.send(`Unable to remove the \`${game}\` game role from you.`).catch(function(){});
 					});
 				});
 			} else {
 				call.message.member.addRole(role).then(() => {
 					call.message.channel.send(`Successfully given you the \`${game}\` game role.`).catch(() => {
-						call.message.author.send(`Successfully given you the \`${game}\` game role.`).catch();
+						call.message.author.send(`Successfully given you the \`${game}\` game role.`).catch(function(){});
 					});
 				}).catch(() => {
 					call.message.channel.send(`Unable to give you the \`${game}\` game role.`).catch(() => {
-						call.message.author.send(`Unable to give you the \`${game}\` game role.`).catch();
+						call.message.author.send(`Unable to give you the \`${game}\` game role.`).catch(function(){});
 					});
 				});
 			}
@@ -41,7 +42,7 @@ module.exports = {
 			call.message.channel.send("Invalid game option. Game options can be found in `!info gameroles list`.").catch(() => {
 				call.message.author
 					.send(`You attempted to use the \`gamerole\` command in ${call.message.channel}, but I do not have permission to chat there.`)
-					.catch();
+					.catch(function(){});
 			});
 		}
 	}
