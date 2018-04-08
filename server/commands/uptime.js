a1697343865e931188d2956060ffe477ab49746c
Yikey <gtc122004@gmail.com>
2018-04-08 17:01:34 -0400

    Update uptime.js

--- server/commands/uptime.js
+++ server/commands/uptime.js
@@ -9,7 +9,7 @@ module.exports = {
 		const seconds = ((uptime % 3600000) % 60000) - (((uptime % 3600000) % 60000) % 1000);
 		const milliseconds = (((uptime % 3600000) % 60000) % 1000) - (((uptime % 3600000) % 60000) % 1);
 		call.message.channel
-			.send(`The bot has been offline for ${hours} hours, ${minutes} minutes, ${seconds/1000} seconds and ${milliseconds} milliseconds`)
+			.send(`The bot has been offline for \`${hours}\` hours, \`${minutes}\` minutes, \`${seconds/1000}\` seconds and \`${milliseconds}\` milliseconds`)
 			.catch(() => {
 				call.message.author.send(`You attempted to run the \`uptime\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
 			});
