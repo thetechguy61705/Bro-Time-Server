19c44c1f3eab1b328dbb7a94c1f95143a90a4a3e
Yikey <gtc122004@gmail.com>
2018-04-08 01:50:49 -0400

    Update multicolorrole.js

--- server/load/multicolorrole.js
+++ server/load/multicolorrole.js
@@ -1,20 +1,22 @@
 module.exports = {
 	exec: (client) => {
-		var loopNumber = 0;
-		var colors = ["Red", "Blue", "Orange", "Green", "Black", "Purple", "Pink", "Yellow",
-			"HotPink", "Indigo", "Bronze", "Cyan", "LightGreen", "Silver", "BrightRed", "HotBrown",
-			"DarkViolet", "Gold"
-		];
-		var realGuild = client.guilds.get("330913265573953536");
-		var multiColorRole = realGuild.roles.find("name", "Multicolored");
-		setInterval(() => {
-			loopNumber = loopNumber + 1;
-			if(loopNumber !== colors.length) {
-				loopNumber + 1;
-			} else {
-				loopNumber = 0;
-			}
-			multiColorRole.setColor(realGuild.roles.find("name", colors[loopNumber]).hexColor);
-		}, 500);
+		client.on("ready", function() {
+			var loopNumber = 0;
+			var colors = ["Red", "Blue", "Orange", "Green", "Black", "Purple", "Pink", "Yellow",
+				"HotPink", "Indigo", "Bronze", "Cyan", "LightGreen", "Silver", "BrightRed", "HotBrown",
+				"DarkViolet", "Gold"
+			];
+			var realGuild = client.guilds.get("330913265573953536");
+			var multiColorRole = realGuild.roles.find("name", "Multicolored");
+			setInterval(() => {
+				loopNumber = loopNumber + 1;
+				if(loopNumber !== colors.length) {
+					loopNumber + 1;
+				} else {
+					loopNumber = 0;
+				}
+				multiColorRole.setColor(realGuild.roles.find("name", colors[loopNumber]).hexColor);
+			}, 500);
+		});
 	}
 };
