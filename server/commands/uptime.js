1f5af6866461d78bba534d04e334e53da5b989e5
Yikey <gtc122004@gmail.com>
2018-04-08 15:44:06 -0400

    Update uptime.js

--- server/commands/uptime.js
+++ server/commands/uptime.js
@@ -4,12 +4,12 @@ module.exports = {
 	load: () => {},
 	execute: (call) => {
 		const uptime = call.client.uptime;
-		const hours = (uptime) - (uptime % 3600000);
-		const minutes = (uptime % 3600000) - (uptime % 3600000) % (60000);
-		const seconds = ((uptime % 3600000) % 60000) - (((uptime % 3600000) % 60000) % 1000);
-		const milliseconds = (((uptime % 3600000) % 60000) % 1000) - (((uptime % 3600000) % 60000) % 1);
+		const hours = `\`${(uptime) - (uptime % 3600000)}\` hours, `;
+		const minutes = `\`${(uptime % 3600000) - (uptime % 3600000) % (60000)}\` minutes, `;
+		const seconds = `\`${((uptime % 3600000) % 60000) - (((uptime % 3600000) % 60000) % 1000)}\` seconds and `;
+		const milliseconds = `\`${(((uptime % 3600000) % 60000) % 1000) - (((uptime % 3600000) % 60000) % 1)}\` milliseconds.`;
 		call.message.channel
-			.send(`The bot has been online for \`${hours/3600000}\` hours, \`${minutes/60000}\` minutes,\` ${seconds/1000}\` seconds and \`${milliseconds}\` milliseconds.`)
+			.send("The bot has been online for"+hours+minutes+seconds+milliseconds)
 			.catch(() => {
 				call.message.author.send(`You attempted to run the \`uptime\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
 			});
