05b0792dd5ba68df6103ec049e54d484f07f19fa
Colton Loewen <loewencjay@gmail.com>
2018-04-07 23:28:10 -0600

    Structured settings

---
+++ data/settings.js
@@ -0,0 +1,18 @@
+class Settings {
+	static CACHE = 0x00000001;
+	static SAVE = 0x00000002;
+
+	constructor() {
+
+	}
+
+	get(key, options) {
+
+	}
+
+	set(key, value, options) {
+
+	}
+}
+
+module.exports = Settings;
