ab51d5498aaacc8cb035b703bac8c1a12ea772f7
Yikey <gtc122004@gmail.com>
2018-04-08 15:15:02 -0400

    Update uptime.js

--- server/commands/uptime.js
+++ server/commands/uptime.js
@@ -1,6 +1,6 @@
 module.exports = {
 	id: "uptime",
-  aliases: ["timeup"],
+	aliases: ["timeup"],
 	load: () => {},
 	execute: (call) => {
 		const uptime = call.client.uptime;
@@ -9,8 +9,8 @@ module.exports = {
 		const seconds = ((uptime % 3600000) % 60000) - (((uptime % 3600000) % 60000) % 1000);
 		const milliseconds = (((uptime % 3600000) % 60000) % 1000) - (((uptime % 3600000) % 60000) % 1);
 		call.message.channel.send(`\`${hours/3600000}\` hours, \`${minutes/60000}\` minutes,\` ${seconds/1000}\` seconds and \`${milliseconds}\` milliseconds.`)
-      .catch(() => {
-        call.message.author.send(`You attempted to run the \`uptime\` command in ${message.channel}, but I can not chat there.`).catch(function(){});
-      });
+			.catch(() => {
+				call.message.author.send(`You attempted to run the \`uptime\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
+			});
 	}
 };
