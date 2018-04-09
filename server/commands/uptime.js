59b812e70c726a9ab367a01275a1c56d21a0d756
Yikey <gtc122004@gmail.com>
2018-04-08 21:04:06 -0400

    Update uptime.js

--- server/commands/uptime.js
+++ server/commands/uptime.js
@@ -4,12 +4,13 @@ module.exports = {
 	load: () => {},
 	execute: (call) => {
 		const uptime = call.client.uptime;
-		const hours = ((uptime) - (uptime % 3600000))/3600000;
-		const minutes = ((uptime % 3600000) - (uptime % 3600000) % (60000))/60000;
+		const days = ((uptime) - (uptime % 86400000)) / 86400000;
+		const hours = (((uptime) - (uptime % 3600000)) / 3600000) - (days * 24);
+		const minutes = ((uptime % 3600000) - (uptime % 3600000) % (60000)) / 60000;
 		const seconds = ((uptime % 3600000) % 60000) - (((uptime % 3600000) % 60000) % 1000);
 		const milliseconds = (((uptime % 3600000) % 60000) % 1000) - (((uptime % 3600000) % 60000) % 1);
 		call.message.channel
-			.send(`The bot has been offline for \`${hours}\` hours, \`${minutes}\` minutes, \`${seconds/1000}\` seconds and \`${milliseconds}\` milliseconds`)
+			.send(`The bot has been offline for \`${days}\` days, \`${hours}\` hours, \`${minutes}\` minutes, \`${seconds/1000}\` seconds and \`${milliseconds}\` milliseconds`)
 			.catch(() => {
 				call.message.author.send(`You attempted to run the \`uptime\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
 			});
