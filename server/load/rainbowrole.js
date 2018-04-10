fa5eda92642039bd8f3b336047717fe3eefaca4a
Yikey <gtc122004@gmail.com>
2018-04-10 16:00:17 -0400

    Create rainbowrole.js

---
+++ server/load/rainbowrole.js
@@ -0,0 +1,16 @@
+module.exports = {
+	exec: (client) => {
+    const realGuild = client.guilds.get("330913265573953536");
+    console.log(realGuild.name);
+    const multiColorRole = realGuild.roles.find("name", "Multicolored");
+    const colors = ["red", "blue", "orange", "green", "black", "purple", "pink", "yellow",
+      "hotpink", "indigo", "bronze", "cyan", "lightgreen", "silver", "brightred", "hotbrown",
+      "darkviolet", "gold"];
+    var loopNumber = 0;
+		setInterval(function() {
+      multiColorRole.setColor(realGuild.roles.find("name", colors[loopNumber]));
+      loopNumber = loopNumber+1;
+      if(loopNumber === colors.length) loopNumber = 0;
+    }, 1000);
+	}
+};
