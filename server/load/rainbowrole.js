f483332e3d706978062e89b3f7994e02dfd38f1e
Yikey <gtc122004@gmail.com>
2018-04-10 16:43:11 -0400

    Update rainbowrole.js

--- server/load/rainbowrole.js
+++ server/load/rainbowrole.js
@@ -1,7 +1,7 @@
 module.exports = {
 	exec: (client) => {
 		const realGuild = client.guilds.get("330913265573953536");
-		console.log(realGuild.name);
+		console.log(require("util").inspect(realGuild));
 		const multiColorRole = realGuild.roles.find("name", "Multicolored");
 		const colors = ["red", "blue", "orange", "green", "black", "purple", "pink", "yellow",
 			"hotpink", "indigo", "bronze", "cyan", "lightgreen", "silver", "brightred", "hotbrown",
