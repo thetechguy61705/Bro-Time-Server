5e35228a06a7b46d4810cdc394f6c7c63ada3fee
Yikey <gtc122004@gmail.com>
2018-04-10 19:57:09 -0400

    Update togglemulticolor.js

--- server/commands/togglemulticolor.js
+++ server/commands/togglemulticolor.js
@@ -5,7 +5,7 @@ const colors = ["Red", "Blue", "Orange", "Green", "Purple", "Pink", "Yellow",
 
 function changeColors(realGuild, colors, loopNumber) {
 	realGuild.roles.find("name", "Multicolored").setColor(realGuild.roles.find("name", colors[loopNumber]).hexColor)
-		.catch(function(){});
+		.catch(function() {});
 	loopNumber = loopNumber + 1;
 	if(loopNumber === colors.length) loopNumber = 0;
 }
@@ -16,19 +16,22 @@ module.exports = {
 	execute: (call) => {
 		const realGuild = call.client.guilds.get("330913265573953536");
 		var loopNumber = 0;
-		var loopThroughColors = setInterval(function(){ changeColors(realGuild, colors, loopNumber) }, 1000);
+		var loopThroughColors = setInterval(function() {
+			changeColors(realGuild, colors, loopNumber)
+		}, 1000);
 		if(realGuild.roles.find("name", "Multicolored").hexColor !== "#000001") {
 			clearInterval(loopThroughColors);
-			realGuild.roles.find("name", "Multicolored").setColor("#000001").catch(function(){});
+			realGuild.roles.find("name", "Multicolored").setColor("#000001").catch(function() {});
 			call.message.channel.send("Toggled Multicolored role to `off`").catch(() => {
 				call.message.author
 					.send(`You attempted to run the \`togglemulticolor\` command in ${call.message.channel}, but I can not chat there.`)
-					.catch(function(){});
+					.catch(function() {});
+			});
 		} else {
 			call.message.channel.send("Toggled Multicolored role to `on`").catch(() => {
 				call.message.author
 					.send(`You attempted to run the \`togglemulticolor\` command in ${call.message.channel}, but I can not chat there.`)
-					.catch(function(){});
+					.catch(function() {});
 			});
 		}
 	}
