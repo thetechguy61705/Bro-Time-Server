8e44367c340222b3aea2df5ac1380ce1926b784d
Yikey <gtc122004@gmail.com>
2018-04-10 21:11:17 -0400

    Update togglemulticolor.js

--- server/commands/togglemulticolor.js
+++ server/commands/togglemulticolor.js
@@ -2,34 +2,17 @@ module.exports = {
 	id: "togglemulticolor",
 	load: () => {},
 	execute: (call) => {
-		const colors = ["Red", "Blue", "Orange", "Green", "Purple", "Pink", "Yellow",
+		const realGuild = call.client.guilds.get("330913265573953536");
+		const multiColorRole = realGuild.roles.find("name", "Multicolored");
+		const colors = ["Red", "Blue", "Orange", "Green", "Black", "Purple", "Pink", "Yellow",
 			"HotPink", "Indigo", "Bronze", "Cyan", "LightGreen", "Silver", "BrightRed", "HotBrown",
 			"DarkViolet", "Gold"
 		];
-
-		function changeColors(realGuild, colors, loopNumber) {
-			realGuild.roles.find("name", "Multicolored").setColor(realGuild.roles.find("name", colors[loopNumber]).hexColor)
-				.catch(function() {});
+		var loopNumber = 0;
+		setInterval(function() {
+			multiColorRole.setColor(realGuild.roles.find("name", colors[loopNumber]));
 			loopNumber = loopNumber + 1;
 			if(loopNumber === colors.length) loopNumber = 0;
-		}
-		const realGuild = call.client.guilds.get("330913265573953536");
-		var loopNumber = 0;
-		var loopThroughColors = setInterval(changeColors(realGuild, colors, loopNumber), 1000);
-		if(realGuild.roles.find("name", "Multicolored").hexColor !== "#000001") {
-			clearInterval(loopThroughColors);
-			realGuild.roles.find("name", "Multicolored").setColor("#000001").catch(function() {});
-			call.message.channel.send("Toggled Multicolored role to `off`").catch(() => {
-				call.message.author
-					.send(`You attempted to run the \`togglemulticolor\` command in ${call.message.channel}, but I can not chat there.`)
-					.catch(function() {});
-			});
-		} else {
-			call.message.channel.send("Toggled Multicolored role to `on`").catch(() => {
-				call.message.author
-					.send(`You attempted to run the \`togglemulticolor\` command in ${call.message.channel}, but I can not chat there.`)
-					.catch(function() {});
-			});
-		}
+		}, 1000);
 	}
 };
