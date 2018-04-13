bcc5c5d3f27ad4ea1d000a9ea1e986920bc401a1
Yikey <gtc122004@gmail.com>
2018-04-13 07:03:31 -0400

    Create starboard.js

---
+++ server/load/starboard.js
@@ -0,0 +1,79 @@
+module.exports = {
+	exec: (client) => {
+		const amountUntilStarboard = 2;
+		var testGuild = bot.guilds.get("430096406275948554");
+		var realGuild = bot.guilds.get("330913265573953536");
+
+		bot.on("messageReactionAdd", (messageReaction, user) => {
+			if (messageReaction.message.guild.id === testGuild.id) {
+				var starReactions = messageReaction.message.reactions.find(reaction => reaction.emoji.name === "⭐");
+				var count = starReactions.count;
+				if (starReactions.users.get(messageReaction.message.author.id)) count = count - 1;
+				if (count >= amountUntilStarboard) {
+					var starboardEmbed = new Discord.RichEmbed()
+						.setAuthor(messageReaction.message.author.tag, messageReaction.message.author.avatarURL)
+						.setDescription(messageReaction.message.content)
+						.setColor("ORANGE");
+					if (messageReaction.message.attachments.size !== 0) {
+						starboardEmbed = starboardEmbed.setImage(messageReaction.message.attachments.first().url);
+					}
+
+					var loopStop = 0;
+					var starboardChannel = realGuild.channels.find("name", "starboard");
+					starboardChannel.fetchMessages({
+						limit: 100
+					}).then(msgs => {
+						msgs.forEach(msg => {
+							loopStop = loopStop + 1;
+							if (msg.content.includes(`${messageReaction.message.id}`)) {
+								loopStop = loopStop - 1;
+								msg.edit(`**${count} ⭐ ${messageReaction.message.channel} ID:** ${messageReaction.message.id}`, {
+									embed: starboardEmbed
+								});
+							}
+							if (loopStop === msgs.size) {
+								starboardChannel.send(`**${count} ⭐ ${messageReaction.message.channel} ID:** ${messageReaction.message.id}`, {
+									embed: starboardEmbed
+								});
+							}
+						});
+					});
+				}
+			}
+		});
+
+		bot.on("messageReactionRemove", (messageReaction, user) => {
+			if (messageReaction.message.guild.id === testGuild.id) {
+				var starReactions = messageReaction.message.reactions.find(reaction => reaction.emoji.name === "⭐");
+				var count = starReactions.count;
+				if (starReactions.users.get(messageReaction.message.author.id)) count = count - 1;
+				var starboardEmbed = new Discord.RichEmbed()
+					.setAuthor(messageReaction.message.author.tag, messageReaction.message.author.avatarURL)
+					.setDescription(messageReaction.message.content)
+					.setColor("ORANGE");
+				if (messageReaction.message.attachments.size !== 0) {
+					starboardEmbed = starboardEmbed.setImage(messageReaction.message.attachments.first().url);
+				}
+				var loopStop = 0;
+				var starboardChannel = realGuild.channels.find("name", "starboard");
+				starboardChannel.fetchMessages({
+					limit: 100
+				}).then(msgs => {
+					msgs.forEach(msg => {
+						loopStop = loopStop + 1;
+						if (msg.content.includes(`${messageReaction.message.id}`)) {
+							loopStop = loopStop - 1;
+							if (count < amountUntilStarboard) {
+								msg.delete();
+							} else {
+								msg.edit(`**${count} ⭐ ${messageReaction.message.channel} ID:** ${messageReaction.message.id}`, {
+									embed: starboardEmbed
+								});
+							}
+						}
+					});
+				});
+			}
+		});
+	}
+};
