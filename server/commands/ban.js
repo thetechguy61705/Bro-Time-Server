fd4533880a14ec1356c6034320efdb1595e6510f
Yikey <gtc122004@gmail.com>
2018-04-15 15:37:30 -0400

    Update ban.js

--- server/commands/ban.js
+++ server/commands/ban.js
@@ -7,14 +7,14 @@ module.exports = {
 		const parameterTwo = rawContent.split(" ")[1];
 		if (call.message.member.roles.some(role => ["330919872630358026", "402175094312665098", "395265037356236810", "387768886096953355"].includes(role.id))) {
 			const target = call.message.guild.members.find(m => parameterOne.includes(`${m.user.id}`));
-			if (call.message.member.highestRole.position <= target.highestRole.position) {
-				var reason;
-				if (parameterTwo != undefined) {
-					reason = "`" + rawContent.substr(parameterOne.length + 1) + "`";
-				} else {
-					reason = "`No reason specified.`";
-				}
-				if (target != undefined) {
+			if (target !== null) {
+				if (call.message.member.highestRole.position <= target.highestRole.position) {
+					var reason;
+					if (parameterTwo != undefined) {
+						reason = "`" + rawContent.substr(parameterOne.length + 1) + "`";
+					} else {
+						reason = "`No reason specified.`";
+					}
 					if (target.bannable) {
 						target.send(`You have been banned from the \`${call.message.guild.name}\` server by \`${call.message.author.tag}\` for ${reason}`).then(() => {
 							target.ban(7).then(() => {
@@ -35,11 +35,12 @@ module.exports = {
 								.catch(function() {});
 						});
 					}
-				} else {
-					call.message.reply("Please mention or supply the id of a valid user.").catch(() => {
-						call.message.author.send(`You attempted to use the \`ban\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
-					});
+
 				}
+			} else {
+				call.message.reply("Please mention or supply the id of a valid user.").catch(() => {
+					call.message.author.send(`You attempted to use the \`ban\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
+				});
 			}
 		} else {
 			call.message.reply("You do not have permissions to trigger this command.").catch(() => {
