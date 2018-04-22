dd59271da9f606fa0511d33393885fe6a0b31790
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-22 15:07:12 -0400

    Update and rename color.js to togglecolor.js

--- server/commands/togglecolor.js
+++ server/commands/togglecolor.js
@@ -1,5 +1,5 @@
 module.exports = {
-	id: "color",
+	id: "togglecolor",
 	load: () => {},
 	execute: (call) => {
 		if(call.message.member.hasPermission("MANAGE_ROLES")) {
@@ -12,7 +12,7 @@ module.exports = {
 				ha = "on";
 			}
 			call.message.channel.send(`Switched ${ha}.`).catch(() => {
-				call.message.author.send(`You attempted to run the \`color\` command in ${call.message.channel}, but I do not have permission to chat there.\nSwitched ${ha}.`)
+				call.message.author.send(`You attempted to run the \`togglecolor\` command in ${call.message.channel}, but I do not have permission to chat there.\nSwitched ${ha}.`)
 					.catch(function(){});
 			});
 		}
