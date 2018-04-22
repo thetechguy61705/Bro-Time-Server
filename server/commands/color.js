b467b4030a64432a3e99842c4998b71765339aa3
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-22 14:53:40 -0400

    Update color.js

--- server/commands/color.js
+++ server/commands/color.js
@@ -3,12 +3,12 @@ module.exports = {
 	load: () => {},
 	execute: (call) => {
 		if(call.client.multicolor) {
-    call.client.multicolor = false
-    var ha = "off"
-    } else {
-    call.client.multicolor = true
-    var ha = "on"
-    }
+			call.client.multicolor = false
+			var ha = "off"
+		} else {
+			call.client.multicolor = true
+			var ha = "on"
+			}
 		call.message.channel.send(`Switched ${ha}`).catch(() => {
 			call.message.author.send(`You attempted to run the \`color\` command in ${call.message.channel}, but I do not have permission to chat there.`)
 				.catch(function(){});
