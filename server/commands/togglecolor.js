c4e461ee9833b56b6789a03d2aa46da8cefbb423
Yikey <gtc122004@gmail.com>
2018-04-22 15:19:00 -0400

    Update togglecolor.js

--- server/commands/togglecolor.js
+++ server/commands/togglecolor.js
@@ -2,16 +2,16 @@ module.exports = {
 	id: "togglecolor",
 	load: () => {},
 	execute: (call) => {
-		if(call.message.member.hasPermission("MANAGE_ROLES")) {
-			var ha;
-			if(call.client.multicolor) {
+		if (call.message.member.hasPermission("MANAGE_ROLES")) {
+			var toggle;
+			if (call.client.multicolor) {
 				call.client.multicolor = false;
-				ha = "off";
+				toggle = "off";
 			} else {
 				call.client.multicolor = true;
-				ha = "on";
+				toggle = "on";
 			}
-			call.message.channel.send(`Switched ${ha}.`).catch(() => {
+			call.message.channel.send(`Toggled the multicolor role to \`${toggle}\`.`).catch(() => {
 				call.message.author.send(`You attempted to run the \`togglecolor\` command in ${call.message.channel}, but I do not have permission to chat there.\nSwitched ${ha}.`)
 					.catch(function(){});
 			});
