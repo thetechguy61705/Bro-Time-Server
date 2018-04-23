195d260b501f1bcf76574fb8df5af1e289a427b9
Yikey <gtc122004@gmail.com>
2018-04-23 19:26:46 -0400

    Update addpartner.js

--- server/commands/addpartner.js
+++ server/commands/addpartner.js
@@ -3,32 +3,38 @@ const Discord = require("discord.js");
 module.exports = {
 	id: "addpartner",
 	description: "Adds a new partner in the #partners channel",
-	parameters: "(title) (description) (thumbnail URL)",
+	arguments: "(title) (description) (thumbnail URL)",
 	requires: "Role: Community Manager Bro",
 	load: () => {},
 	execute: (call) => {
-		let PartnersChannel = call.message.guild.channels.find("id", "409156491640045571");
-		if(call.message.member.roles.has("409153912558583818")) {
+		const partnersChannel = call.message.guild.channels.find("id", "409156491640045571");
+		if (call.message.member.roles.has("409153912558583818")) {
 			if(call.params.readRaw()) {
-				var Title = call.params.readRaw().split(" |")[0];
-				var Description = call.params.readRaw().split(" |")[1].slice(1);
-				var Thumbnail = call.message.content.slice(18+Title.length+Description.length);
-				const PartnerEmbed = new Discord.RichEmbed()
-					.setTitle(Title)
+				const title = call.params.readRaw().split(" |")[0];
+				const description = call.params.readRaw().split(" |")[1].slice(1);
+				const thumbnail = call.message.content.slice(18+title.length+description.length);
+				const partnerEmbed = new Discord.RichEmbed()
+					.setTitle(title)
 					.setColor("#FFA500")
-					.setDescription(Description)
-					.setThumbnail(Thumbnail);
-				PartnersChannel.send(PartnerEmbed).then(() => {
-					call.message.reply("Successfully sent message!");
+					.setDescription(description)
+					.setThumbnail(thumbnail);
+				partnersChannel.send(partnerEmbed).then(() => {
+					call.message.reply("Successfully sent message!").catch(function() {});
+					partnersChannel.send("-------------------------------------------------").catch(function() {});
 				}).catch(() => {
-					call.message.reply("Couldn't send the partner message in the partners channel!");
+					call.message.reply("Couldn't send the partner message in the partners channel!").catch(() => {
+						call.message.author.send(`You attempted to use the \`addpartner\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
+					});
 				});
-				PartnersChannel.send("-------------------------------------------------");
 			} else {
-				call.message.reply("You did not provide the necessary parameters! `!addpartner (title) | (description) | (thumbnail URL)`");
+				call.message.reply("You did not provide the necessary parameters! `!addpartner (title) | (description) | (thumbnail URL)`").catch(() => {
+					call.message.author.send(`You attempted to use the \`addpartner\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
+				});
 			}
 		} else {
-			call.message.reply("You do not have permission to use this command!\n`Requires: Community Manager Bro`");
+			call.message.reply("You do not have permission to use this command!\n`Requires: Community Manager Bro`").catch(() => {
+					call.message.author.send(`You attempted to use the \`addpartner\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
+				});;
 		}
 	}
 };
