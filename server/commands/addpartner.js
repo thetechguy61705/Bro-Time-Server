ed82a482dd9e165de239e6241d7e0f413f81c397
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-23 16:14:40 -0400

    Create addpartner.js

---
+++ server/commands/addpartner.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+module.exports = {
+	id: "addpartner",
+	load: () => {},
+	execute: (call) => {
+		var CommunityManagerBro = call.message.guild.roles.find(`id`, `409153912558583818`)
+		let PartnersChannel = await call.message.guild.channels.find(`id`, `409156491640045571`)
+		if(call.message.member.roles.has(CommunityManagerBro) {
+		var Title = call.params.readRaw().split("|")[0];
+		var Description = call.params.readRaw().split("|")[1];
+		var Thumbnail = call.params.readRaw().split("|")[2];
+		const PartnerEmbed = new Discord.RichEmbed()
+		.setTitle(Title)
+		.setColor("#FFA500")
+		.setDescription(Description)
+		.setThumbnail(Thumbnail)
+		await PartnersChannel(PartnerEmbed)
+		}
+		}
+
+			
+      
+ 
+      
