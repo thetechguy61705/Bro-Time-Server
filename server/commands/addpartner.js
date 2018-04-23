ad54ebc7f673540fb3d0ae8806884b36a0c8b62e
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-23 16:23:25 -0400

    Update addpartner.js

--- server/commands/addpartner.js
+++ server/commands/addpartner.js
@@ -7,17 +7,17 @@ module.exports = {
 		var CommunityManagerBro = call.message.guild.roles.find(`id`, `409153912558583818`);
 		let PartnersChannel = call.message.guild.channels.find(`id`, `409156491640045571`);
 		if(call.message.member.roles.has(CommunityManagerBro)) {
-		var Title = call.params.readRaw().split("|")[0];
-		var Description = call.params.readRaw().split("|")[1];
-		var Thumbnail = call.params.readRaw().split("|")[2];
-		const PartnerEmbed = new Discord.RichEmbed()
-		.setTitle(Title)
-		.setColor("#FFA500")
-		.setDescription(Description)
-		.setThumbnail(Thumbnail);
-		PartnersChannel(PartnerEmbed).catch((e) => {
-			call.message.reply(`Couldn't send the partner message in the partners channel!\n\`${e}\``);
-		});
+			var Title = call.params.readRaw().split("|")[0];
+			var Description = call.params.readRaw().split("|")[1];
+			var Thumbnail = call.params.readRaw().split("|")[2];
+			const PartnerEmbed = new Discord.RichEmbed()
+				.setTitle(Title)
+				.setColor("#FFA500")
+				.setDescription(Description)
+				.setThumbnail(Thumbnail);
+			PartnersChannel(PartnerEmbed).catch((e) => {
+				call.message.reply(`Couldn't send the partner message in the partners channel!\n\`${e}\``);
+			});
 		}
 	}
 }
