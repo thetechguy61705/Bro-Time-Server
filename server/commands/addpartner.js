8a90719a6cef07d9f735de425311c7aa68df55d7
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-23 17:50:07 -0400

    Update addpartner.js

--- server/commands/addpartner.js
+++ server/commands/addpartner.js
@@ -4,13 +4,11 @@ module.exports = {
 	id: "addpartner",
 	load: () => {},
 	execute: (call) => {
-		//var CommunityManagerBro = call.message.guild.roles.find("id", "409153912558583818");
 		let PartnersChannel = call.message.guild.channels.find("id", "409156491640045571");
 		if(call.message.member.roles.has("409153912558583818")) {
-			var content = call.params.readRaw().slice(12);
-			var Title = content.split(" |")[0];
-			var Description = content.split(" |")[1].slice(1);
-			var Thumbnail = content.split(" |")[2].slice(1);
+			var Title = call.params.readRaw().split(" |")[0];
+			var Description = call.params.readRaw().split(" |")[1].slice(1);
+			var Thumbnail = call.message.content.slice(18+Title.length+Description.length);
 			const PartnerEmbed = new Discord.RichEmbed()
 				.setTitle(Title)
 				.setColor("#FFA500")
