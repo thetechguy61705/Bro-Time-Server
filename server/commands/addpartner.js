a6388d7b455947092f0980f5cdb5634f6310ef2f
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-23 18:04:59 -0400

    Update addpartner.js

--- server/commands/addpartner.js
+++ server/commands/addpartner.js
@@ -6,21 +6,28 @@ module.exports = {
 	execute: (call) => {
 		let PartnersChannel = call.message.guild.channels.find("id", "409156491640045571");
 		if(call.message.member.roles.has("409153912558583818")) {
-			var Title = call.params.readRaw().split(" |")[0];
-			var Description = call.params.readRaw().split(" |")[1].slice(1);
-			var Thumbnail = call.message.content.slice(18+Title.length+Description.length);
-			const PartnerEmbed = new Discord.RichEmbed()
-				.setTitle(Title)
-				.setColor("#FFA500")
-				.setDescription(Description)
-				.setThumbnail(Thumbnail);
-			PartnersChannel.send(PartnerEmbed).then(() => {
-				call.message.reply("Successfully send message!");
-			}).catch((e) => {
-				call.message.reply(`Couldn't send the partner message in the partners channel!\n\`${e}\``);
-			});
-		} else {
-			call.message.reply("You do not have permission to use this command!");
+			if(call.params.readRaw()) {
+				var Title = call.params.readRaw().split(" |")[0];
+				if(!Title) call.message.reply("You did not provide the necessary parameters! `!addpartner (title) | (description) | (thumbnail URL)`");
+				var Description = call.params.readRaw().split(" |")[1].slice(1);
+				if(!Description) call.message.reply("You did not provide the necessary parameters! `!addpartner (title) | (description) | (thumbnail URL)`");
+				var Thumbnail = call.message.content.slice(18+Title.length+Description.length);
+				if(!Thumbnail) call.message.reply("You did not provide the necessary parameters! `!addpartner (title) | (description) | (thumbnail URL)`");
+				const PartnerEmbed = new Discord.RichEmbed()
+					.setTitle(Title)
+					.setColor("#FFA500")
+					.setDescription(Description)
+					.setThumbnail(Thumbnail);
+				PartnersChannel.send(PartnerEmbed).then(() => {
+					call.message.reply("Successfully sent message!");
+				}).catch((e) => {
+					call.message.reply("Couldn't send the partner message in the partners channel!");
+				});
+			} else {
+				call.message.reply("You do not have permission to use this command!\n`Requires: Community Manager Bro`");
+			} else {
+				call.message.reply("You did not provide the necessary parameters! `!addpartner (title) | (description) | (thumbnail URL)`");
+			}
 		}
 	}
 };
