589a7c8fa49fbd97cbbf446fff1d23e7c486f936
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-23 18:58:10 -0400

    Update servericon.js

--- server/commands/servericon.js
+++ server/commands/servericon.js
@@ -2,11 +2,15 @@ module.exports = {
 	id: "servericon",
 	load: () => {},
 	execute: (call) => {
-		var unfetchedinvite = call.params.readRaw();
-    		call.client.fetchInvite(unfetchedinvite).then((invite) => {
-    			call.message.reply(invite.guild.icon);
-    				}).catch(() => {
-    					call.message.author.send(`You did not provide a valid invite!`);
-		});
-	}
-};
+		if(call.params.readRaw()) {
+			var unfetchedinvite = call.params.readRaw();
+			call.client.fetchInvite(unfetchedinvite).then((invite) => {
+				call.message.reply(invite.guild.icon);
+					}).catch(() => {
+						call.message.author.send("You did not provide a valid invite!");
+			});
+		} else {
+			call.message.reply("You did not provide the necessary parameter(s)! `!servericon (invite)`"
+					   }
+					   }
+					   }
