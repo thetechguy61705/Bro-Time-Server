2dcead91b2e3489260498979ad79329ff04d67ad
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-23 16:19:05 -0400

    Update addpartner.js

--- server/commands/addpartner.js
+++ server/commands/addpartner.js
@@ -4,9 +4,9 @@ module.exports = {
 	id: "addpartner",
 	load: () => {},
 	execute: (call) => {
-		var CommunityManagerBro = call.message.guild.roles.find(`id`, `409153912558583818`)
-		let PartnersChannel = await call.message.guild.channels.find(`id`, `409156491640045571`)
-		if(call.message.member.roles.has(CommunityManagerBro) {
+		var CommunityManagerBro = call.message.guild.roles.find(`id`, `409153912558583818`);
+		let PartnersChannel = call.message.guild.channels.find(`id`, `409156491640045571`);
+		if(call.message.member.roles.has(CommunityManagerBro)) {
 		var Title = call.params.readRaw().split("|")[0];
 		var Description = call.params.readRaw().split("|")[1];
 		var Thumbnail = call.params.readRaw().split("|")[2];
@@ -14,10 +14,12 @@ module.exports = {
 		.setTitle(Title)
 		.setColor("#FFA500")
 		.setDescription(Description)
-		.setThumbnail(Thumbnail)
-		await PartnersChannel(PartnerEmbed)
+		.setThumbnail(Thumbnail);
+		PartnersChannel(PartnerEmbed).catch((e) => {
+			call.message.reply(`Couldn't send the partner message in the partners channel!\n\`${e}\``);
+		});
 		}
-		}
+		};
 
 			
       
