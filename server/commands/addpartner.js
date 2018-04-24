b5b28f61bef8fd5818241464a4696d401230c395
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-24 18:39:53 -0400

    Update addpartner.js

--- server/commands/addpartner.js
+++ server/commands/addpartner.js
@@ -9,46 +9,49 @@ module.exports = {
 	execute: (call) => {
 		const partnersChannel = call.message.guild.channels.find("id", "409156491640045571");
 		if (call.message.member.roles.has("409153912558583818")) {
-			var ooftext = "Couldn't send the partner message in the partners channel!";
 			if (call.params.readRaw()) {
 				const title = call.params.readRaw().split("|")[0];
 				const description = call.params.readRaw().split("|")[1].trim();
 				const thumbnail = call.params.readRaw().split("|")[2].trim();
-				call.client.fetchInvite(thumbnail).then((invite) => {
-					const partnerEmbed = new Discord.RichEmbed()
-						.setTitle(title)
-						.setColor("#FFA500")
-						.setDescription(description)
-						.setThumbnail(`https://cdn.discordapp.com/icons/${invite.guild.id}/${invite.guild.icon}.png`);
-					partnersChannel.send(partnerEmbed).then(() => {
-						call.message.reply("Successfully sent message!").catch(function() {});
-						partnersChannel.send("-------------------------------------------------").catch(function() {});
+				if (!description.length > 2048) {
+					call.client.fetchInvite(thumbnail).then((invite) => {
+						const partnerEmbed = new Discord.RichEmbed()
+							.setTitle(title)
+							.setColor("#FFA500")
+							.setDescription(description)
+							.setThumbnail(`https://cdn.discordapp.com/icons/${invite.guild.id}/${invite.guild.icon}.png`);
+						partnersChannel.send(partnerEmbed).then(() => {
+							call.message.reply("Successfully sent message!").catch(function() {});
+							partnersChannel.send("-------------------------------------------------").catch(function() {});
+						}).catch(() => {
+							call
+								.message
+								.reply(`${ooftext}\n\n**Make sure that:**\n-You have a valid discord server invite or a valid thumbnail url\n-The description is not over 2048 characters`)
+								.catch(() => {
+									call.message.author.send(`You attempted to use the \`addpartner\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
+								});
+						});
 					}).catch(() => {
-						call
-							.message
-							.reply(`${ooftext}\n\n**Make sure that:**\n-You have a valid discord server invite or a valid thumbnail url\n-The description is not over 2048 characters`)
-							.catch(() => {
+						const partnerEmbedb = new Discord.RichEmbed()
+							.setTitle(title)
+							.setColor("#FFA500")
+							.setDescription(description)
+							.setThumbnail(thumbnail);
+						partnersChannel.send(partnerEmbedb).then(() => {
+							call.message.reply("Successfully sent message!").catch(function() {});
+							partnersChannel.send("-------------------------------------------------").catch(function() {});
+						}).catch(() => {
+							call.message.reply("Couldn't send the partner message in the partners channel, make sure that you have a valid discord server invite or a valid thumbnail url!`)
+									   .catch(() => {
 								call.message.author.send(`You attempted to use the \`addpartner\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
 							});
+						});
 					});
-				}).catch(() => {
-					const partnerEmbedb = new Discord.RichEmbed()
-						.setTitle(title)
-						.setColor("#FFA500")
-						.setDescription(description)
-						.setThumbnail(thumbnail);
-					partnersChannel.send(partnerEmbedb).then(() => {
-						call.message.reply("Successfully sent message!").catch(function() {});
-						partnersChannel.send("-------------------------------------------------").catch(function() {});
-					}).catch(() => {
-						call
-							.message
-							.reply(`${ooftext}\n\n**Make sure that:**\n-You have a valid discord server invite or a valid thumbnail url\n-The description is not over 2048 characters`)
-							.catch(() => {
-								call.message.author.send(`You attempted to use the \`addpartner\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
-							});
-					});
-				});
+					} else {
+						call.message.reply("The description cannot be over 2048 characters!").catch(() => {
+							call.message.author.send(`You attempted to use the \`addpartner\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
+						});
+				}
 			} else {
 				call.message.reply("You did not provide the necessary parameters! `!addpartner (title) (description) (discord invite OR thumbnail url)`").catch(() => {
 					call.message.author.send(`You attempted to use the \`addpartner\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
