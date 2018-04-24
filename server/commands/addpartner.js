3c60aac3b25a4625abcc39215b4141c106e43081
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-23 21:08:01 -0400

    Update addpartner.js

--- server/commands/addpartner.js
+++ server/commands/addpartner.js
@@ -33,16 +33,16 @@ module.exports = {
 						.setColor("#FFA500")
 						.setDescription(description)
 						.setThumbnail(thumbnail);
+					var ooftext = "Couldn't send the partner message in the partners channel!"
 					partnersChannel.send(partnerEmbedb).then(() => {
 						call.message.reply("Successfully sent message!").catch(function() {});
 						partnersChannel.send("-------------------------------------------------").catch(function() {});
 					}).catch(() => {
-						call.message.reply("Couldn't send the partner message in the partners channel!\n**Make sure that:**\n-You have a valid discord server invite or a valid thumbnail url\n-Your description is not over 2048 characters").catch(() => {
-							call
-								.message
-								.author
-								.send(`You attempted to use the \`addpartner\` command in ${call.message.channel}, but I can not chat there.`)
-								.catch(function() {});
+						call
+							.message
+							.reply(`${ooftext}\n**Make sure that:**\n-You have a valid discord server invite or a valid thumbnail url\n-Your description is not over 2048 characters`)
+							.catch(() => {
+							call.message.author.send(`You attempted to use the \`addpartner\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
 						});
 					});
 				});
