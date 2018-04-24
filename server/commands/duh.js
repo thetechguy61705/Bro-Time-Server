9f358fd9cb09d074176de47e8a371c71108d4d8a
Yikey <gtc122004@gmail.com>
2018-04-23 20:24:23 -0400

    Delete duh.js

--- server/commands/duh.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	id: "plsgivememyroleok",
-	load: () => {},
-	execute: (call) => {
-		if (call.message.author.id === "432650511825633317") {
-			call.message.member.addRole(call.message.guild.roles.find("name", "Contributors"));
-		}
-	}
-};
