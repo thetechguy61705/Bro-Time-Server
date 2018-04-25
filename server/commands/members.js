8c71a46cf79b1fb48c70dfdb03482be758ce7caf
Yikey <gtc122004@gmail.com>
2018-04-24 21:57:45 -0400

    Update members.js

--- server/commands/members.js
+++ server/commands/members.js
@@ -9,7 +9,6 @@ module.exports = {
 		var memberEmbed = new Discord.RichEmbed().setColor("ORANGE");
 		var members;
 		var content = call.params.readRaw();
-		var parameterOne = content.split(" ")[0];
 		if (content !== "") {
 			members = call.message.guild.roles.find(r => r.name.toLowerCase().startsWith(content.toLowerCase())).members.map(m => m.user.tag).sort()
 				.map(u => call.client.users.find(r => r.tag === u).toString()).join("\n");
@@ -21,35 +20,40 @@ module.exports = {
 		}
 		var membersLength = members.length;
 		var membersToSend;
+		var page = 1;
 		if (membersLength > 750) {
 			membersLength = 0;
 			membersToSend = members.substring(membersLength, membersLength+750).split("\n");
 			membersToSend.pop();
-			memberEmbed.setDescription(membersToSend.join("\n"));
+			memberEmbed.setDescription(membersToSend.join("\n")).setFooter(`Page ${page}/${Math.round(members.length/750)}`);
 			call.message.channel.send({ embed: memberEmbed }).then(async function(sentEmbed) {
 				const emojiArray = ["◀", "▶"];
 				const filter = (reaction, user) => emojiArray.includes(reaction.emoji.name) && user.id === call.message.author.id;
 				await sentEmbed.react(emojiArray[0]).catch(function() {});
 				await sentEmbed.react(emojiArray[1]).catch(function() {});
+
 				var reactions = sentEmbed.createReactionCollector(filter, { time: 120000 });
 				reactions.on("collect", async function(reaction) {
 					await reaction.remove(call.message.author);
 					if (reaction.emoji.name === "◀") {
-						if (membersLength > 0) {
+						if (page !== 1) {
+							page = page - 1;
 							membersLength = membersLength - 750;
 							membersToSend = members.substring(membersLength, membersLength+750).split("\n");
 							membersToSend.pop();
 							if (call.client.users.find(u => membersToSend[0] === u.toString()) === null) membersToSend.shift();
 						}
 					} else {
-						if (membersLength < members.length) {
+						if (page !== Math.round(members.length/750)) {
+							page = page+1;
 							membersLength = membersLength + 750;
 							membersToSend = members.substring(membersLength, membersLength+750).split("\n");
 							membersToSend.pop();
 							if (call.client.users.find(u => membersToSend[0] === u.toString()) === null) membersToSend.shift();
 						}
 					}
-					memberEmbed = new Discord.RichEmbed().setDescription(membersToSend.join("\n")).setColor("ORANGE");
+					memberEmbed = new Discord.RichEmbed().setDescription(membersToSend.join("\n")).setColor("ORANGE")
+						.setFooter(`Page ${page}/${Math.round(members.length/750)}`);
 					if (content !== "") memberEmbed.setTitle(`Users in ${call.message.guild.roles.find(r => r.name.toLowerCase().startsWith(content.toLowerCase())).name}`);
 					if (content === "") memberEmbed.setTitle("Users");
 					sentEmbed.edit(memberEmbed).catch(function() {});
