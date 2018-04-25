1f5623d094a18f28d1ae8e84b0250fd4b15aea5f
Yikey <gtc122004@gmail.com>
2018-04-24 22:31:29 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -179,43 +179,6 @@ config.BOTS.forEach((bot) => {
 			}
 
 		});
-		client.on("guildMemberAdd", member => {
-			if (client.user.id === "393532251398209536") {
-				if (member.guild.id === "330913265573953536") {
-					if (!member.bot) {
-						let WelcomeMessage = new discord.RichEmbed()
-							.setTitle("Welcome")
-							.setColor("#FFA500")
-							.setDescription(`Welcome to Bro Time ${member.user}! Have a good time here!`)
-							.setFooter(`Bro Time is now at ${member.guild.memberCount} members!`);
-						let hangoutchannel = client.channels.find("id", "433831764105101332");
-						hangoutchannel.send(WelcomeMessage).then(() => {
-							fs.readFile(__dirname + "/./info/welcomemsg.md", (err, data) => {
-								if (err) {
-									throw err;
-								} else {
-									member.user.send(data.toString("utf8")).catch(function() {});
-								}
-							}).catch(function() {});
-						}).catch(function() {});
-					}
-				}
-			}
-		});
-		client.on("guildMemberRemove", member => {
-			if (client.user.id === "393532251398209536") {
-				if (member.guild.id === "330913265573953536") {
-					if (!member.bot) {
-						let GoodbyeMessage = new discord.RichEmbed()
-							.setTitle("Goodbye")
-							.setColor("#0000ff")
-							.setDescription(`Sad to see you leave ${member.user}.`);
-						let hangoutchannel = client.channels.find("id", "433831764105101332");
-						hangoutchannel.send(GoodbyeMessage).catch(function() {});
-					}
-				}
-			}
-		});
 
 		client.on("message", message => {
 			if (!message.author.bot) {
