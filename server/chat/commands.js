cecd291539cc4884572e51250c9240ada25a185e
Yikey <gtc122004@gmail.com>
2018-05-01 17:21:57 -0400

    Update commands.js

--- server/chat/commands.js
+++ server/chat/commands.js
@@ -58,7 +58,7 @@ function load(command) {
 
 var walker = walk.walk(__dirname + COMMANDS);
 
-walker.on("file", (root, stat) => {
+walker.on("file", (root, stat, next) => {
 	var match = stat.name.match(/^(.*)\.js$/);
 	if (match != null) {
 		new Promise((resolve, reject) => {
@@ -80,7 +80,7 @@ walker.on("file", (root, stat) => {
 	next();
 });
 
-walker.on("errors", (root, stats, next) => {
+walker.on("errors", (root, stats) => {
 	console.warn("Unable to load some command files:");
 	stats.forEach((stat) => {
 		console.warn(stat.error.stack);
