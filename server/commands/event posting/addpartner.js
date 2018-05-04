956ea4c0c5d417ed0e239bdb6e502db60ed48061
Yikey <gtc122004@gmail.com>
2018-05-03 22:35:32 -0400

    Update addpartner.js

--- server/commands/event posting/addpartner.js
+++ server/commands/event posting/addpartner.js
@@ -13,7 +13,7 @@ module.exports = {
 				const title = call.params.readRaw().split("|")[0];
 				const description = call.params.readRaw().split("|")[1].trim();
 				const thumbnail = call.params.readRaw().split("|")[2].trim();
-				if (description.length > 2048) {
+				if (description.length < 2048) {
 					call.client.fetchInvite(thumbnail).then((invite) => {
 						const partnerEmbed = new Discord.RichEmbed()
 							.setTitle(title)
