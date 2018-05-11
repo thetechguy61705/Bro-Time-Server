c744eb75b6e2cd3a3b2e4f14bc9af30fa85392e0
Colton Loewen <loewencjay@gmail.com>
2018-05-10 21:03:53 -0600

    Removed old help

--- server/commands/information/help.js
+++ server/commands/information/help.js
@@ -1,46 +1,47 @@
 const Discord = require("discord.js");
-var commandHelp;
+
+function add(command, help, prefix) {
+	var category = command.category || "Other";
+	var array = help[category];
+	if (array == null) {
+		array = [];
+		help[category] = array;
+	}
+	if (command.arguments != null) {
+		array.push(`\`${prefix}${command.id} ${command.arguments}\``);
+	} else {
+		array.push(`\`${prefix}${command.id}\``);
+	}
+}
 
 module.exports = {
 	id: "help",
 	aliases: ["h"],
 	description: "Returns information and commands on the bot.",
 	arguments: "[command]",
-	load: (access) => {
-		commandHelp = {};
-
-		function add(command) {
-			var category = command.category || "Other";
-			var array = commandHelp[category];
-			if (array == null) {
-				array = [];
-				commandHelp[category] = array;
-			}
-			if (command.arguments != null) {
-				array.push(`\`${command.id} ${command.arguments}\``);
-			} else {
-				array.push(`\`${command.id}\``);
-			}
-		}
-
-		access.commands.loaded.array()
-			.sort((a, b) => {
-				return a.id.localeCompare(b.id);
-			})
-			.forEach(add);
-	},
+	load: () => {},
 	execute: (call) => {
 		var pfx = call.message.data.prefix;
 		var param1 = call.params.readRaw();
 		var helpEmbed;
 		if (param1 == null || param1 == undefined || param1 == "") {
+			var commandHelp = {};
+
+			call.commands.loaded.array()
+				.sort((a, b) => {
+					return a.id.localeCompare(b.id);
+				})
+				.forEach((command) => {
+					add(command, commandHelp);
+				});
+
 			helpEmbed = new Discord.RichEmbed()
 				.setTitle("Commands")
 				.setDescription(`Prefix: \`${pfx}\`\nUptime: \`soon™\``)
 				.setColor(0x00AE86)
 				.setFooter(`Ran by ${call.message.author.username} (${call.message.author.id})`, call.message.author.displayAvatarURL);
 			for (var [category, commands] of Object.entries(commandHelp)) {
-				helpEmbed.addField(category, pfx + commands.join("\n"));
+				helpEmbed.addField(category, commands.join("\n"));
 			}
 			call.message.channel.send({ embed: helpEmbed }).catch(() => {
 				call.message.author.send(`You attempted to run the \`!help\` command in ${call.message.channel}, but I can not speak and/or send embeds there.`)
