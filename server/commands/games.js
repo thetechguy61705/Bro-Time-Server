029be41ac758e2b047fcad8e5161c268363d6fa1
Colton Loewen <loewencjay@gmail.com>
2018-05-13 14:24:47 -0600

    Added debug information

--- server/commands/games.js
+++ server/commands/games.js
@@ -156,10 +156,10 @@ function startGame(game, context) {
 
 		if (game.updateInterval > 0)
 			session.updateTimer = context.client.setInterval(1/Math.min(game.updateInterval, MAX_UPDATE_CYCLES)*1000, game.update);
+		console.log(game.timeout, session.endGame);
 		session.endTimer = context.client.setTimeout(game.timeout, session.endGame);
 		session.restartEndTimer = (() => {
 			clearTimeout(this.endTimer);
-			console.log(this.game.timeout, this.endGame);
 			this.endTimer = this.context.client.setTimeout(this.game.timeout, this.endGame);
 		}).bind(session);
 
