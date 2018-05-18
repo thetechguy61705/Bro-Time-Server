d523478aa66cf84bda476b116ba97b498424bfcd
Colton Loewen <loewencjay@gmail.com>
2018-05-17 21:35:32 -0600

    Corrected source

--- server/music/youtube.js
+++ server/music/youtube.js
@@ -1,31 +1,54 @@
 var ytdl = require("ytdl-core");
 
+const MIN_RATING = 2;
+
 module.exports = {
+	filter: "audio",
+	retires: 0,
+	// Cloudflare Proxy
+	requestOptions: {
+		transform: (parsed) => {
+			return {
+				host: "1.1.1.1",
+				port: 8888,
+				path: "/" + parsed.href,
+				headers: { Host: parsed.host }
+			};
+		}
+	},
+
 	getTicket(query) {
 		return new Promise((resolve) => {
 			ytdl.getInfo(query, this, (err, info) => {
 				resolve(err == null ? info : null);
 			});
 		});
-	}
+	},
 
 	getPlayable(ticket) {
+		// start good
+		// Check the rating. mature
+		// Known issue? bad
+		var play = "good";
+		if (ticket.allow_ratings !== "1" || parseFloat(ticket.avg_rating) < MIN_RATING) {
+			play = "unknown";
+		}
+		return play;
+	},
 
-	}
+	relay() {
 
-	relay(ticket, stream) {
-
-	}
+	},
 
 	pause() {
 
-	}
+	},
 
 	continue() {
 
-	}
+	},
 
-	search(query) {
+	search() {
 
 	}
 };
