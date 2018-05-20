d43eb9b14cf4a388e69a47d813bc024f39288b04
Yikey <gtc122004@gmail.com>
2018-05-20 14:11:04 -0400

    Update multicolor.js

--- server/load/multicolor.js
+++ server/load/multicolor.js
@@ -4,10 +4,8 @@ module.exports = {
 		var realGuild = client.guilds.get("330913265573953536");
 		if (client.user.id === "393532251398209536") {
 			const multiColorRole = realGuild.roles.find("name", "Multicolored");
-			const colors = ["Red", "Blue", "Orange", "Green", "Purple", "Pink", "Yellow", "HotPink",
-				"Indigo", "Bronze", "Cyan", "LightGreen", "Silver", "BrightRed", "HotBrown",
-				"DarkViolet", "Gold"
-			];
+			const colors = ["Red", "Blue", "Orange", "Green", "Purple", "Pink", "Yellow"];
+
 			if (require("./commands/Utility/togglecolor").multicolor) {
 				var randomchoice = Math.floor(Math.random() * colors.length);
 				multiColorRole.setColor(realGuild.roles.find("name", colors[randomchoice]).hexColor).catch(function() {});
