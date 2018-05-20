d3ee5c4d416c12e2c4f51741fc1bf1be124e6297
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-20 18:36:58 -0400

    Update multicolor.js

--- server/load/multicolor.js
+++ server/load/multicolor.js
@@ -5,22 +5,16 @@ module.exports = {
 		if (client.user.id === "393532251398209536") {
 			const multiColorRole = realGuild.roles.find("name", "Multicolored");
 			const colors = ["Red", "Blue", "Orange", "Green", "Purple", "Pink", "Yellow"];
-
-			if (require("./commands/Utility/togglecolor").multicolor) {
-				var randomchoice = Math.floor(Math.random() * colors.length);
-				multiColorRole.setColor(realGuild.roles.find("name", colors[randomchoice]).hexColor).catch(function() {});
-				console.log("Set The Color");
-			}
+			var randomchoice = Math.floor(Math.random() * colors.length);
+			multiColorRole.setColor(realGuild.roles.find("name", colors[randomchoice]).hexColor).catch(function() {});
 			var loopNumber = 0;
 			var offlineInRole;
 			client.setInterval(function() {
-				if (require("./commands/Utility/togglecolor").multicolor) {
-					offlineInRole = multiColorRole.members.filter(member => member.presence.status === "offline");
-					if (offlineInRole.size !== multiColorRole.members.size) {
-						multiColorRole.setColor(realGuild.roles.find("name", colors[loopNumber]).hexColor).catch(function() {});
-						loopNumber = loopNumber + 1;
-						if (loopNumber === colors.length) loopNumber = 0;
-					}
+				offlineInRole = multiColorRole.members.filter(member => member.presence.status === "offline");
+				if (offlineInRole.size !== multiColorRole.members.size) {
+					multiColorRole.setColor(realGuild.roles.find("name", colors[loopNumber]).hexColor).catch(function() {});
+					loopNumber = loopNumber + 1;
+					if (loopNumber === colors.length) loopNumber = 0;
 				}
 			}, 3600000);
 		}
