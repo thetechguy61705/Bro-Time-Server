d115894b81bb06ed95d0a9db90106aca6dec17de
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-20 19:37:24 -0400

    Update multicolor.js

--- server/load/multicolor.js
+++ server/load/multicolor.js
@@ -1,25 +1,20 @@
 module.exports = {
 	exec: (client) => {
-		var realGuild = client.guilds.get("330913265573953536");
-		if (client.user.id === "393532251398209536") {
+		var realGuild = bot.guilds.get("330913265573953536");
+		if (bot.user.id === "433065327836790784") {
 			const multiColorRole = realGuild.roles.find("name", "Multicolored");
-			const colors = ["Red", "Blue", "Orange", "Green", "Purple", "Pink", "Yellow"];
+			const colors = ["Red", "Blue", "Orange", "Green", "Purple", "Yellow"];
 			var loopNumber = 0;
 			var randomchoice;
-			var index;
 			var othercolors;
-			client.setInterval(function() {
-				othercolors = colors.map(r => realGuild.roles.find("name", r).hexColor);
-				index = othercolors.indexOf(multiColorRole.hexColor);
-				if (index > -1) {
-					othercolors.splice(index, 1);
-				}
+			bot.setInterval(function() {
+				othercolors = colors.filter(c => multiColorRole.hexColor !== c)
 				randomchoice = Math.floor(Math.random() * othercolors.length);
 				multiColorRole.setColor(realGuild.roles.find("name", othercolors[randomchoice]).hexColor).catch(function() {});
 				console.log(`Changed color to ${othercolors[randomchoice]}.`);
-				loopNumber = loopNumber + 1;
+				loopNumber++;
 				if (loopNumber === colors.length) loopNumber = 0;
-			}, 3600000);
+			}, 1000);
 		}
 	}
 };
