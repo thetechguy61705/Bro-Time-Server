3c93c4e702a6f1ec0d2edf68e404d62bd8e536cc
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-20 18:47:11 -0400

    Update multicolor.js

--- server/load/multicolor.js
+++ server/load/multicolor.js
@@ -5,8 +5,13 @@ module.exports = {
 		if (client.user.id === "393532251398209536") {
 			const multiColorRole = realGuild.roles.find("name", "Multicolored");
 			const colors = ["Red", "Blue", "Orange", "Green", "Purple", "Pink", "Yellow"];
-			var randomchoice = Math.floor(Math.random() * colors.length);
-			multiColorRole.setColor(realGuild.roles.find("name", colors[randomchoice]).hexColor).catch(function() {});
+			const othercolors = colors.map(r => realGuild.roles.find("name", r).hexColor)
+			var index = othercolors.indexOf(multiColorRole.hexColor);
+    			if (index > -1) {
+       			othercolors.splice(index, 1);
+   			}
+			var randomchoice = Math.floor(Math.random() * othercolors.length);
+			multiColorRole.setColor(realGuild.roles.find("name", othercolors[randomchoice]).hexColor).catch(function() {});
 			var loopNumber = 0;
 			var offlineInRole;
 			client.setInterval(function() {
