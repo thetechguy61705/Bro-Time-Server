b2d5360b2049dcc408fe95f533be0629b5c24d40
Colton Loewen <loewencjay@gmail.com>
2018-05-20 01:29:28 -0600

    Structured queue

--- server/load/music.js
+++ server/load/music.js
@@ -1,4 +1,5 @@
-var tokens = {};
+var { Collection } = require("discord.js");
+var tokens = new Collection();
 
 // A map from source to token.
 const TOKENS_MAPPING = {
@@ -6,16 +7,53 @@ const TOKENS_MAPPING = {
 };
 
 class Queue {
+	static request(user, action) {
+		// if DJ, accept immidiatly.
+		// else, start vote.
+		console.log(user, action);
+	}
 
+	play(query, user, client) {
+		this.reserve(user, client);
+
+		console.log("queueing:", query);
+	}
+
+	stop() {
+		this.release();
+
+		console.log("stopping");
+	}
+
+	skip() {
+
+	}
+
+	repeat() {
+
+	}
+
+	reserve(user, client) {
+		// If the user is in a music channel, join it, and create a voice connection
+		console.log(user, client);
+	}
+
+	release () {
+		// If a voice connection is active, Disconnect the voice connection, and leave the voice channel.
+	}
+
+	pause() {
+
+	}
 }
 
 module.exports = {
 	exec: (client, bot) => {
-		var botTokens = new Map();
+		var botTokens = new Collection();
 		for (var [source, key] of Object.entries(TOKENS_MAPPING)) {
 			botTokens.set(source, bot[key]);
 		}
-		tokens[client.token] = botTokens;
+		tokens.set(client.user.id, botTokens);
 		client.music = new Queue();
 	}
 };
