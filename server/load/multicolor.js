ff224afd13320932105d5ca0db8cefa5caf3bcd5
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-19 21:25:10 -0400

    Update multicolor.js

--- server/load/multicolor.js
+++ server/load/multicolor.js
@@ -1,10 +1,11 @@
 module.exports = {
 	exec: (client) => {
+		var realGuild = client.guilds.get("330913265573953536");
 		if (client.user.id === "393532251398209536") {
 			const multiColorRole = realGuild.roles.find("name", "Multicolored");
-			const colors = ["Red", "Blue", "Orange", "Green", "Purple", "Pink", "Yellow", "HotPink",
-					"Indigo", "Bronze", "Cyan", "LightGreen", "Silver", "BrightRed", "HotBrown",
-					"DarkViolet", "Gold"
+		const colors = ["Red", "Blue", "Orange", "Green", "Purple", "Pink", "Yellow", "HotPink",
+				"Indigo", "Bronze", "Cyan", "LightGreen", "Silver", "BrightRed", "HotBrown",
+				"DarkViolet", "Gold"
 				       ];
 			var loopNumber = 0;
 			var offlineInRole;
