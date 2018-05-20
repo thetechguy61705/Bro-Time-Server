2b1213c960484ae7083101d141c1033d7cef5588
Yikey <gtc122004@gmail.com>
2018-05-20 11:10:44 -0400

    Update reactroles.js

--- server/load/reactroles.js
+++ server/load/reactroles.js
@@ -1,17 +1,19 @@
-const Discord = require("discord.js");
+//const Discord = require("discord.js");
 module.exports = {
 	exec: (client) => {
 		if (client.user.id === "393532251398209536") {
+			/*
 			const freeRoles = ["QOTD", "ANN", "GW", "MOVIES", "Roblox", "Minecraft", "Cuphead", "Fortnite", "Undertale", "Unturned", "VRChat",
 			"PUBG", "FNAF", "Clash of Clans", "Clash Royale", "Sims", "Terraria", "Subnautica", "Rocket League",
 			"Portal", "Hat in Time", "CSGO", "Splatoon", "Mario", "Starbound", "Garry's Mod", "Overwatch",
 			"Call of Duty", "Destiny", "Psych"];
+			*/
 			client.channels.get("447205162436788235").fetchMessages({ limit: 100 }).then(messages => {
 				if (messages.size > 0) {
 					messages.forEach(message => {
 						if (message.author.id === client.user.id) {
 							if (message.reactions.has("404768960014450689")) {
-								const filter = (reaction, user) => reaction.emoji.id === "404768960014450689";
+								const filter = (reaction) => reaction.emoji.id === "404768960014450689";
 								const collector = message.createReactionCollector(filter);
 								collector.on("collect", reaction => {
 									var member = message.guild.member(reaction.users.last());
