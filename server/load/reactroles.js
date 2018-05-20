09443df8abacea7290323393028b95fc0eb7a31e
Yikey <gtc122004@gmail.com>
2018-05-20 12:44:12 -0400

    Update reactroles.js

--- server/load/reactroles.js
+++ server/load/reactroles.js
@@ -1,41 +1,55 @@
-//const Discord = require("discord.js");
+const Discord = require("discord.js");
+
 module.exports = {
 	exec: (client) => {
-		console.log("0");
-		/*
 		const freeRoles = ["QOTD", "ANN", "GW", "MOVIES", "Roblox", "Minecraft", "Cuphead", "Fortnite", "Undertale", "Unturned", "VRChat",
-		"PUBG", "FNAF", "Clash of Clans", "Clash Royale", "Sims", "Terraria", "Subnautica", "Rocket League",
-		"Portal", "Hat in Time", "CSGO", "Splatoon", "Mario", "Starbound", "Garry's Mod", "Overwatch",
-		"Call of Duty", "Destiny", "Psych"];
-		*/
+			"PUBG", "FNAF", "Clash of Clans", "Clash Royale", "Sims", "Terraria", "Subnautica", "Rocket League",
+			"Portal", "Hat in Time", "CSGO", "Splatoon", "Mario", "Starbound", "Garry's Mod", "Overwatch",
+			"Call of Duty", "Destiny", "Psych"];
 		client.channels.get("447205162436788235").fetchMessages({ limit: 100 }).then(messages => {
-			console.log("1");
-			if (messages.size > 0) {
-				console.log("2");
-				messages.forEach(message => {
-					console.log("3");
+			if (messages.size === freeRoles.length) {
+				messages.forEach(async function(message) {
 					if (message.author.id !== client.user.id) {
-						console.log("4");
-						if (message.reactions.has("pixeldolphin:404768960014450689")) {
-							console.log("5");
-							const filter = (reaction) => reaction.emoji.id === "404768960014450689";
-							const collector = message.createReactionCollector(filter);
-							collector.on("collect", reaction => {
-								console.log("6");
-								var member = message.guild.member(reaction.users.last());
-								if (member) {
-									console.log("7");
-									if (member.roles.has(message.guild.roles.find("name", message.embeds[0].title).id)) {
-										console.log("8");
-										member.removeRole(message.guild.roles.find("name", message.embeds[0].title));
-									} else {
-										console.log("9");
-										member.addRole(message.guild.roles.find("name", message.embeds[0].title));
-									}
+						if (!message.reactions.has("pixeldolphin:404768960014450689"))
+							await message.react("404768960014450689");
+						const filter = (reaction) => reaction.emoji.id === "404768960014450689";
+						const collector = message.createReactionCollector(filter);
+						collector.on("collect", (reaction) => {
+							reaction.remove(reaction.users.last());
+							var member = message.guild.member(reaction.users.last());
+							if (member) {
+								if (member.roles.has(message.guild.roles.find("name", message.embeds[0].title).id)) {
+									member.removeRole(message.guild.roles.find("name", message.embeds[0].title));
+								} else {
+									member.addRole(message.guild.roles.find("name", message.embeds[0].title));
 								}
-							});
-						}
+							}
+						});
 					}
+				});
+			} else {
+				freeRoles.difference(messages.map(message => message.embeds[0].title)).forEach(newItem => {
+					const newItemEmbed = new Discord.RichEmbed()
+						.setTitle(newItem)
+						.setDescription(`React to get the ${newItem} role!`)
+						.setColor("CYAN");
+					client.channels.get("447205162436788235").send({ embed: newItemEmbed }).then(async function(newItemMessage) {
+						await newItemMessage.react("404768960014450689");
+						const filter = (reaction) => reaction.emoji.id === "404768960014450689";
+						const collector = newItemMessage.createReactionCollector(filter);
+						collector.on("collect", (reaction) => {
+							reaction.remove(reaction.users.last());
+							var member = newItemMessage.guild.member(reaction.users.last());
+							if (member) {
+								if (member.roles.has(newItemMessage.guild.roles.find("name", newItemMessage.embeds[0].title).id)) {
+									member.removeRole(newItemMessage.guild.roles.find("name", newItemMessage.embeds[0].title));
+								} else {
+									member.addRole(newItemMessage.guild.roles.find("name", newItemMessage.embeds[0].title));
+								}
+							}
+						});
+
+					});
 				});
 			}
 		});
