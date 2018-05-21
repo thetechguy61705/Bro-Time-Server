a6cac0084a5e98f83f996769d960e22e2adf8e67
Yikey <gtc122004@gmail.com>
2018-05-20 23:11:17 -0400

    Update fetchtest.js

--- server/commands/fetchtest.js
+++ server/commands/fetchtest.js
@@ -2,7 +2,7 @@ module.exports = {
 	id: "fetchtest",
 	execute: (call) => {
 		call.client.channels.get("447205162436788235").fetchMessage("447958383144730625").then(msg => {
-      console.log(msg);
-    });
+			console.log(msg);
+		});
 	}
 };
