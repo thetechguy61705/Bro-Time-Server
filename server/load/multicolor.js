b9cfb4839670867678393d857d67659779007d94
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-20 20:33:01 -0400

    Update multicolor.js

--- server/load/multicolor.js
+++ server/load/multicolor.js
@@ -1,25 +1,33 @@
 module.exports = {
 	exec: (client) => {
 		client.guilds.forEach((guild) => {
-			var realGuild = client.guilds.get("330913265573953536");
 			if (client.user.id === "393532251398209536") {
 				const multiColorRole = guild.roles.find("name", "Multicolored");
-				const colors = ["Red", "Blue", "Orange", "Green", "Purple", "Yellow"];
-				var loopNumber = 0;
-				var randomchoice;
-				var othercolors;
-				var onetimecolors = colors.filter(c => multiColorRole.hex !== realGuild.roles.find("name", c).hex);
-				var onetimerandomchoice = Math.floor(Math.random() * onetimecolors.length);
-				multiColorRole.setColor(guild.roles.find("name", onetimecolors[onetimerandomchoice]).hexColor).catch(function() {});
-				console.log(`Changed color to ${onetimecolors[onetimerandomchoice]}.`);
-				client.setInterval(function() {
-					othercolors = colors.filter(c => multiColorRole.hex !== realGuild.roles.find("name", c).hex);
-					randomchoice = Math.floor(Math.random() * othercolors.length);
-					multiColorRole.setColor(guild.roles.find("name", othercolors[randomchoice]).hexColor).catch(function() {});
-					console.log(`Changed color to ${othercolors[randomchoice]}.`);
-					loopNumber++;
-					if (loopNumber === colors.length) loopNumber = 0;
-				}, 2000);
+				if (multiColorRole) {
+					const colors = ["Red", "Blue", "Orange", "Green", "Purple", "Yellow"];
+					if (guild.roles.find("name", "Red") 
+					&& guild.roles.find("name", "Blue")
+					&& guild.roles.find("name", "Orange")
+					&& guild.roles.find("name", "Green")
+					&& guild.roles.find("name", "Purple")
+					&& guild.roles.find("name", "Yellow")) {
+						var loopNumber = 0;
+						var randomchoice;
+						var othercolors;
+						var onetimecolors = colors.filter(c => multiColorRole.hex !== guild.roles.find("name", c).hex);
+						var onetimerandomchoice = Math.floor(Math.random() * onetimecolors.length);
+						multiColorRole.setColor(guild.roles.find("name", onetimecolors[onetimerandomchoice]).hexColor).catch(function() {});
+						console.log(`Changed color to ${onetimecolors[onetimerandomchoice]}.`);
+						client.setInterval(function() {
+							othercolors = colors.filter(c => multiColorRole.hex !== guild.roles.find("name", c).hex);
+							randomchoice = Math.floor(Math.random() * othercolors.length);
+							multiColorRole.setColor(guild.roles.find("name", othercolors[randomchoice]).hexColor).catch(function() {});
+							console.log(`Changed color to ${othercolors[randomchoice]}.`);
+							loopNumber++;
+							if (loopNumber === colors.length) loopNumber = 0;
+						}, 2000);
+					}
+				}
 			}
 		});
 	}
