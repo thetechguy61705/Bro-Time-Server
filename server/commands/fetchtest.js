511e3408a7fe692d7451bdfa045952cfc80318a3
Colton Loewen <loewencjay@gmail.com>
2018-05-22 23:24:57 -0600

    Added test property

--- server/commands/fetchtest.js
+++ server/commands/fetchtest.js
@@ -1,5 +1,6 @@
 module.exports = {
 	id: "fetchtest",
+	test: true,
 	execute: (call) => {
 		call.client.channels.get("447205162436788235").fetchMessage("447958383144730625").then(msg => {
 			console.log(msg);
