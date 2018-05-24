f7c5f6fb0f8077b5ba64c44471066789d4d8dcc8
Colton Loewen <loewencjay@gmail.com>
2018-05-23 21:50:42 -0600

    Simplified config
    fixes #445

--- config.js
+++ config.js
@@ -1,32 +1,16 @@
 var config = {
-	BOTS: [
-		{
-			token: process.env.BRO_TIME_TOKEN,
-			google: process.env.GOOGLE_KEY
-		},
-		{
-			token: process.env.KITCHEN_TOKEN
-		}
-	],
+	TOKEN: process.env.BRO_TIME_TOKEN,
+	GOOGLE: process.env.GOOGLE_KEY,
 	DB_CONNECTIONS: 20,
 	DB: process.env.DB
 };
 
 try {
 	var userConfig = require("./user_config");
-	if (userConfig.DB != null)
-		config.DB = userConfig.DB;
-	for (var i = 0; i < userConfig.BOTS.length; i++) {
-		if (userConfig.BOTS[i] != null && userConfig.BOTS[i] !== "") {
-			if (userConfig.BOTS[i] instanceof Object) {
-				for (var [key, value] of Object.entries(userConfig.BOTS[i])) {
-					config.BOTS[i][key] = value;
-				}
-			} else {
-				config.BOTS[i].token = userConfig.BOTS[i];
-			}
-		}
+	for (var [key, value] of Object.entries(userConfig)) {
+		config[key] = value;
 	}
+	console.log(config);
 } catch (exc) {
 	if (exc.code !== "MODULE_NOT_FOUND")
 		throw exc;
