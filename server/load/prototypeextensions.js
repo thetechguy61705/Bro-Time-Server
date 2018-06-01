66e1bbf17443705dbc33a106fe5995af8a1e88bc
Yikey <gtc122004@gmail.com>
2018-06-01 11:43:28 -0400

    Update prototypeextensions.js

--- server/load/prototypeextensions.js
+++ server/load/prototypeextensions.js
@@ -6,11 +6,11 @@ Number.prototype.expandPretty = function() {
 	var minutes = ((this % 3600000) - (this % 3600000) % (60000)) / 60000;
 	var seconds = ((this % 3600000) % 60000) - (((this % 3600000) % 60000) % 1000);
 	var milliseconds = (((this % 3600000) % 60000) % 1000) - (((this % 3600000) % 60000) % 1);
-	(days > 1) ? days = `\`${days}\` days, `: (days === 1) ? days = `\`${days}\` day, ` : days = "";
-	(hours > 1) ? hours = `\`${hours}\` hours, `: (hours === 1) ? hours = `\`${hours}\` hour, ` : hours = "";
-	(minutes > 1) ? minutes = `\`${minutes}\` minutes, `: (hours === 1) ? hours = `\`${minutes}\` minute, ` : minutes = "";
-	((seconds / 1000) > 1) ? seconds = `\`${seconds/1000}\` seconds, `: ((seconds / 1000) === 1) ? seconds = `\`${seconds/1000}\` second, ` : seconds = "";
-	(days === "" && hours === "" && minutes === "" && seconds === "") ? milliseconds = `\`${milliseconds}\` milliseconds.`: milliseconds = `and \`${milliseconds}\` milliseconds.`;
+	days = (days > 1) ? `\`${days}\` days, `: (days === 1) ? `\`${days}\` day, ` : "";
+	hours = (hours > 1) ? `\`${hours}\` hours, `: (hours === 1) ? `\`${hours}\` hour, ` : "";
+	minutes = (minutes > 1) ? minutes = `\`${minutes}\` minutes, `: (minutes === 1) ? `\`${minutes}\` minute, ` : "";
+	seconds = ((seconds / 1000) > 1) ? `\`${seconds/1000}\` seconds, `: ((seconds / 1000) === 1) ? `\`${seconds/1000}\` second, ` : "";
+	milliseconds = (days === "" && hours === "" && minutes === "" && seconds === "") ? `\`${milliseconds}\` milliseconds.` : `and \`${milliseconds}\` milliseconds.`;
 	return `${days}${hours}${minutes}${seconds}${milliseconds}`;
 };
 
