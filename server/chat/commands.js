a2fa1df1d95dd79978758fe3fa372fbca7ca033e
Colton Loewen <loewencjay@gmail.com>
2018-05-31 21:16:08 -0600

    Organized commands module

--- server/chat/commands.js
+++ server/chat/commands.js
@@ -99,50 +99,64 @@ module.exports = {
 	_requests: new Collection(),
 
 	loaded: modules,
-	exec: function(message, client) {
-		var used = false;
+	getRequesting: function(message) {
 		var requests = this._requests.filter((request) => request.channel === message.channel.id);
-		var request = null;
+		var request;
 		if (requests.has(message.author.id)) {
 			request = requests.get(message.author.id);
 		} else if (requests.some((request) => request.settings&this.ANYONE !== 0)) {
 			request = requests.find((request) => request.settings&this.ANYONE !== 0);
 		}
-		if (request != null) {
-			clearTimeout(request.timeout);
-			if (request.settings&this.CANCELLABLE !== 0 && message.content.toLowerCase() === "cancel") {
-				request.reject();
-			} else {
-				request.resolve(new Call(this, message, client, new Parameters(message)));
-			}
-			this._requests.delete(request.author);
+		return request || null;
+	},
+	processRequest: function(request, message, client) {
+		clearTimeout(request.timeout);
+		if (request.settings&this.CANCELLABLE !== 0 && message.content.toLowerCase() === "cancel") {
+			request.reject();
 		} else {
-			var data = (message.guild || message.channel).data;
-			var prefix = message.content.match(new RegExp(util.format(prefixPattern,
-				escapeStringRegexp(data != null ? data.prefix : "/")), "i"));
-			var using;
-			if (prefix != null) {
-				using = true;
-			} else {
-				prefix = message.content.match("^" + MessageMentions.USERS_PATTERN.source);
-				using = prefix != null &&
-					message.mentions.users.size === 1 &&
-					message.mentions.users.first().id == client.user.id;
-			} if (using) {
-				var params = new Parameters(message);
-				params.offset(prefix[0].length);
-				params.readSeparator();
-				var name = params.readParameter();
-				if (name != null) {
-					var command = modules.get(name.toLowerCase()) || modules.find((module) => module.aliases != null && module.aliases.indexOf(name) > -1);
+			request.resolve(new Call(this, message, client, new Parameters(message)));
+		}
+		this._requests.delete(request.author);
+		return true;
+	},
+	processCommand: function(message, client) {
+		var data = (message.guild || message.channel).data;
+		var prefix = message.content.match(new RegExp(util.format(prefixPattern,
+			escapeStringRegexp(data != null ? data.prefix : "/")), "i"));
+		var using;
+		var used = false;
+		if (prefix != null) {
+			using = true;
+		} else {
+			prefix = message.content.match("^" + MessageMentions.USERS_PATTERN.source);
+			using = prefix != null &&
+				message.mentions.users.size === 1 &&
+				message.mentions.users.first().id == client.user.id;
+		}
+		if (using) {
+			var params = new Parameters(message);
+			params.offset(prefix[0].length);
+			params.readSeparator();
+			var name = params.readParameter();
+			if (name != null) {
+				var command = modules.get(name.toLowerCase()) || modules.find((module) => module.aliases != null && module.aliases.indexOf(name) > -1);
 
-					if (command != null) {
-						params.readSeparator();
-						command.execute(new Call(this, message, client, params));
-						used = true;
-					}
+				if (command != null) {
+					params.readSeparator();
+					command.execute(new Call(this, message, client, params));
+					used = true;
 				}
 			}
+		}
+		return used;
+	},
+	exec: function(message, client) {
+		var used;
+		var request = this.getRequesting(message);
+		if (request != null) {
+			used = this.processRequest(request, message, client);
+		} else {
+			used = this.processCommand(message, client);
 		}
 		return used;
 	}
