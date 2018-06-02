140d336c0d912f21f6443e7a469fee09400e6cd6
Yikey <gtc122004@gmail.com>
2018-06-02 17:52:22 -0400

    Update collect.js

--- server/commands/Currency/collect.js
+++ server/commands/Currency/collect.js
@@ -2,6 +2,6 @@ module.exports = {
 	id: "collect",
 	aliases: ["pick", "grab", "take", "steal", "mine", "snatch", "pull", "select", "choose", "get"],
 	execute: (call) => {
-		call.message.delete(5000).catch(() => {});
+		call.message.delete(3000).catch(() => {});
 	}
 };
