da5d1fb88725e406ab826d78c8bfa407415bffc0
Yikey <gtc122004@gmail.com>
2018-06-03 13:34:21 -0400

    Update balance.js

--- server/commands/Currency/balance.js
+++ server/commands/Currency/balance.js
@@ -6,8 +6,8 @@ module.exports = {
 		param = (param != null) ? param.toLowerCase() : "";
 		var target = call.message.guild.members.find((member) => param.includes(member.id) || param === member.user.tag.toLowerCase());
 		target = (target != null) ? target : call.message.author;
-		call.getWallet(TARGET.id).getTotal().then((total) => {
-			call.message.channel.send(TARGET.user.tag + " has " + total + " Bro Bits.").catch(() => {});
+		call.getWallet(target.id).getTotal().then((total) => {
+			call.message.channel.send(target.user.tag + " has " + total + " Bro Bits.").catch(() => {});
 		});
 	}
 };
