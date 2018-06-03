e1fd8dc4be555c7e343836509fa640a68322ae5e
Yikey <gtc122004@gmail.com>
2018-06-03 12:37:22 -0400

    Update wallettest.js

--- server/commands/wallettest.js
+++ server/commands/wallettest.js
@@ -1,8 +1,17 @@
 module.exports = {
 	id: "wallettest",
 	execute: (call) => {
-		call.getWallet(call.message.author.id).getTotal().then((total) => {
-			call.message.channel.send(total.toString());
-		});
+		var param = call.params.readParameter();
+		if (param != null) {
+			call.getWallet(call.message.author.id).getTotal().then((total) => {
+				call.message.channel.send(total.toString());
+			});
+		} else {
+			call.getWallet(call.message.author.id).change(1).then(() => {
+				call.getWallet(call.message.author.id).getTotal().then((newTotal) => {
+					call.message.channel.send(newTotal.toString());
+				});
+			});
+		}
 	}
 };
