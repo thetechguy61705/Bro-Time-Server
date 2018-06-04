2b2442283c1de16d263a75bc85a749e8b90e3db1
Yikey <gtc122004@gmail.com>
2018-06-03 21:14:31 -0400

    Update balance.js

--- server/commands/Currency/balance.js
+++ server/commands/Currency/balance.js
@@ -4,7 +4,7 @@ module.exports = {
 	execute: (call) => {
 		var param = call.params.readParameter();
 		param = (param != null) ? param.toLowerCase() : null;
-		var target = call.message.guild.members.find((member) => param.includes(member.id) || member.user.tag.toLowerCase().startsWith(param));
+		var target = call.message.guild.members.find((member) => (param || "").includes(member.id) || member.user.tag.toLowerCase().startsWith(param));
 		target = (target != null) ? target : call.message.member;
 		call.getWallet(target.id).getTotal().then((total) => {
 			call.message.channel.send(target.user.tag + " has " + total + " Bro Bits.").catch(() => {});
