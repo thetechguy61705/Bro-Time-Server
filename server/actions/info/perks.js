3ed7c450eb9bf64d2cf775de6af2cbd42bb38539
Yikey <gtc122004@gmail.com>
2018-06-05 17:35:39 -0400

    Update perks.js

--- server/actions/info/perks.js
+++ server/actions/info/perks.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 module.exports = {
 	id: "perks",
 	aliases: ["donations"],
-	run: (call, actions) => {
+	run: (call) => {
 		fs.readFile(__dirname + "/../../info/perks.md", (err, data) => {
 			if (err) {
 				console.error(err.stack);
