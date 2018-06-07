1cf8559049b18755d592f82d06a7c3d7f45863a8
Yikey <gtc122004@gmail.com>
2018-06-06 22:47:06 -0400

    Create loot.js

---
+++ server/commands/Currency/loot.js
@@ -0,0 +1,19 @@
+module.exports = {
+	id: "loot",
+	description: "Gives you a random amount of Bro Bits.",
+	rateLimit: [],
+	execute: (call) => {
+		if (module.exports.rateLimit.indexOf(call.message.author.id) === -1) {
+			var randomBits = Math.ceil(Math.random() * 20);
+			call.getWallet(call.message.author.id).change(randomBits).then(() => {
+				call.message.reply("You looted " + randomBits + " Bro Bits!").catch(() => {});
+				module.exports.rateLimit.push(call.message.author.id);
+				call.client.setTimeout(() => {
+					module.exports.rateLimit.splice(module.exports.rateLimit.indexOf(call.message.author.id), 1);
+				}), 300000;
+			}).catch(() => {
+				call.message.reply("Failed to give you Bro Bits.").catch(() => {});
+			});
+		}
+	}
+};
