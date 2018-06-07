a615eeefdbc43691ce759b4753033a7b636229b9
Yikey <gtc122004@gmail.com>
2018-06-06 23:29:04 -0400

    Update loot.js

--- server/commands/Currency/loot.js
+++ server/commands/Currency/loot.js
@@ -1,10 +1,12 @@
+var premium = require("app/premium");
+
 module.exports = {
 	id: "loot",
 	description: "Gives you a random amount of Bro Bits.",
 	rateLimit: [],
 	execute: (call) => {
 		if (module.exports.rateLimit.indexOf(call.message.author.id) === -1) {
-			var randomBits = Math.ceil(Math.random() * 20);
+			var randomBits = Math.ceil(Math.random() * (premium(call.message.member)) ? 30 : 20);
 			call.getWallet(call.message.author.id).change(randomBits).then(() => {
 				call.message.reply("You looted " + randomBits + " Bro Bits!").catch(() => {});
 				module.exports.rateLimit.push(call.message.author.id);
