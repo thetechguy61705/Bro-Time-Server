09509837041b0e2448ca77fb0b6c647673ef220c
Yikey <gtc122004@gmail.com>
2018-06-07 15:26:16 -0400

    Update loot.js

--- server/commands/Currency/loot.js
+++ server/commands/Currency/loot.js
@@ -6,7 +6,7 @@ module.exports = {
 	rateLimit: [],
 	execute: (call) => {
 		if (module.exports.rateLimit.indexOf(call.message.author.id) === -1) {
-			var randomBits = Math.ceil(Math.random() * (premium(call.message.member)) ? 30 : 20);
+			var randomBits = Math.ceil(Math.random() * (premium(call.message.member)) ? 20 : 10);
 			call.getWallet(call.message.author.id).change(randomBits).then(() => {
 				call.message.reply("You looted " + randomBits + " Bro Bits!").catch(() => {});
 				module.exports.rateLimit.push(call.message.author.id);
