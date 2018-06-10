b62d2ba0ce2db55dbc8a556254a6fd9f2ff63228
Yikey <gtc122004@gmail.com>
2018-06-10 11:03:35 -0400

    Update lockdown.js

--- server/commands/Utility/lockdown.js
+++ server/commands/Utility/lockdown.js
@@ -1,8 +1,13 @@
+const Moderator = require("app/moderator");
+
 module.exports = {
 	id: "lockdown",
+	aliases: ["unlockdown"],
 	execute: (call) => {
-		call.client.locked = !call.client.locked;
-		if (!call.client.locked) call.client.lockedChannels.length = 0;
-		call.message.reply(`The client is now ${(call.client.locked) ? "inaccessible" : "accessible"}.`).catch(() => {});
+		if (Moderator(call.message.member)) {
+			call.client.locked = !call.client.locked;
+			if (!call.client.locked) call.client.lockedChannels.length = 0;
+			call.message.reply(`The client is now ${(call.client.locked) ? "inaccessible" : "accessible"}.`).catch(() => {});
+		}
 	}
 };
