b1c1e4496f1afbf4effcf584b24e9aba50d69e5f
Yikey <gtc122004@gmail.com>
2018-06-15 19:10:19 -0400

    Update membercount.js

--- server/commands/Information/membercount.js
+++ server/commands/Information/membercount.js
@@ -1,19 +1,22 @@
-const Discord = require("discord.js");
+const { RichEmbed } = require("discord.js");
 module.exports = {
 	id: "membercount",
 	description: "Displays the current member count",
 	execute: (call) => {
-		var members = call.message.guild.memberCount;
-		var online = call.message.guild.members.filter((m) => m.presence.status !== "offline").size;
-		var humans = call.message.guild.members.filter((m) => !m.user.bot).size;
-		var bots = members - humans;
-		var memberEmbed = new Discord.RichEmbed()
-			.setColor("BLUE")
-			.setFooter(`Ran by ${call.message.author.tag}`)
+		var members = call.message.guild.memberCount,
+			online = call.message.guild.members.filter((m) => m.presence.status !== "offline").size,
+			humans = call.message.guild.members.filter((m) => !m.user.bot).size,
+			bots = members - humans;
+		var memberEmbed = new RichEmbed()
 			.addField("Members", members)
 			.addField("Online", online)
 			.addField("Humans", humans)
-			.addField("Bots", bots);
-		call.message.channel.send({ embed: memberEmbed });
+			.addField("Bots", bots)
+			.setFooter(`Ran by ${call.message.author.tag}`)
+			.setColor("BLUE");
+		call.message.channel.send({ embed: memberEmbed }).catch(() => {
+			call.message.author.send(`You attempted to use the \`membercount\` command in ${call.message.channel}, but I can not chat there.`)
+				.catch(() => {});
+		});
 	}
 };
