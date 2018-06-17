7476cf36403d969e8b99923e72596019e56cbbf4
Yikey <gtc122004@gmail.com>
2018-06-17 09:37:49 -0400

    Update menu.js

--- server/commands/Bro Bot Kitchen/menu.js
+++ server/commands/Bro Bot Kitchen/menu.js
@@ -1,12 +1,11 @@
-var menu = require("app/menu").menuEmbed;
+var { getMenu } = require("app/menu");
 
 
 module.exports = {
 	id: "menu",
 	description: "Sends you the menu",
 	execute: (call) => {
-		menu.setFooter(`Max: 3 | Ran by ${call.message.author.tag}`);
-		call.message.channel.send(menu).catch(() => {
+		call.message.channel.send({ embed: getMenu(call.message.author) }).catch(() => {
 			call.message.author.send(`You attempted to use the \`menu\` command in ${call.message.channel}, but I can not chat there.`).catch(() => {});
 		});
 	}
