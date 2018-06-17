f93db5b598ec91b6a96c95c0559f01e3aa64d1df
Yikey <gtc122004@gmail.com>
2018-06-16 23:22:07 -0400

    Update forcedelorder.js

--- server/commands/Bro Bot Kitchen/forcedelorder.js
+++ server/commands/Bro Bot Kitchen/forcedelorder.js
@@ -1,4 +1,4 @@
-var worker = require("app/workers");
+var isWorker = require("app/workers");
 module.exports = {
 	id: "forcedelorder",
 	description: "Deletes someone else's order",
@@ -7,19 +7,19 @@ module.exports = {
 		var ordersChannel = call.client.channels.get("399290151932526593");
 		var kitchenServer = ordersChannel.guild;
 		var member = kitchenServer.members.get(call.message.author.id);
-		if(member && worker(member)) {
+		if(member != null && isWorker(member)) {
 			var code = call.params.readParameter();
 			var reason = call.params.readParameter(true);
-			if(code && reason) {
+			if (code != null && reason != null) {
 				ordersChannel.fetchMessages({ limit: 100 }).then((orders) => {
-					var filteredOrder = orders.find((m) => m && m.embeds && m.embeds[0] && m.embeds[0].fields[0].value === code.toUpperCase());
+					var filteredOrder = orders.find((m) => m.embeds[0] && m.embeds[0].fields[0].value === code.toUpperCase());
 					if (filteredOrder) {
 						var userToMessage = call.client.users.find((m) => m.tag === filteredOrder.embeds[0].fields[2].value);
 						filteredOrder.delete().then(() => {
 							call.message.reply("Successfully cancelled this order.").catch(() => {
 								call.message.author.send(`You attempted to use the \`forcedelorder\` command in ${call.message.channel}, but I can not chat there.`).catch(() => {});
 							});
-							if(userToMessage) {
+							if (userToMessage != null) {
 								userToMessage.send(`Your order has been cancelled. Reason: \`${reason}\``).catch(() => {
 									call.message.reply("Couldn't DM this user, but I deleted the order anyways").catch(() => {
 										call.message.author.send(`You attempted to use the \`forcedelorder\` command in ${call.message.channel}, but I can not chat there.`).catch(() => {});
