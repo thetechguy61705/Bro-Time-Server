6de1e78a4c1a6c95f2e26777d79620b1b2ac1894
Yikey <gtc122004@gmail.com>
2018-06-17 10:00:34 -0400

    Update order.js

--- server/commands/Bro Bot Kitchen/order.js
+++ server/commands/Bro Bot Kitchen/order.js
@@ -1,14 +1,15 @@
-var menu = require("app/menu").menu;
+var { MENU } = require("app/menu");
 var random = require("random-letters");
-const Discord = require("discord.js");
+const { RichEmbed } = require("discord.js");
+
 function titleCase(str) {
-	str = str.toLowerCase();
-	str = str.split(" ");
-	for (var i = 0; i < str.length; i++) {
-		str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+	var newString = "";
+	for (let i = 0; i < str.length; i++) {
+		newString += (i === 0 || str.charAt(i - 1) === " ") ? str.charAt(i).toUpperCase() : str.charAt(i).toLowerCase();
 	}
-	return str.join(" ");
+	return newString;
 }
+
 module.exports = {
 	id: "order",
 	description: "Orders food",
@@ -16,32 +17,30 @@ module.exports = {
 	execute: (call) => {
 		var ordersChannel = call.client.channels.get("399290151932526593");
 		ordersChannel.fetchMessages({ limit: 100 }).then((orders) => {
-			var filteredOrder = orders.find((m) => m && m.embeds && m.embeds[0] && m.embeds[0].fields[2].value === call.message.author.tag);
+			var filteredOrder = orders.find((m) => m.embeds[0] != null && m.embeds[0].fields[2].value === call.message.author.tag);
 			if (!filteredOrder) {
 				var foods = call.params.readRaw().split(",").map((val) => val.trim());
-				if(foods[0] !== ``) { // eslint-disable-line quotes
+				if(foods[0] !== "") {
 					if(foods.length < 4) {
 						var noMenu = [];
 						for (let food of foods) {
-							if(!menu.includes(food.toLowerCase())) {
+							if(!MENU.includes(food.toLowerCase())) {
 								noMenu.push(food.toLowerCase());
 							}
 						}
 						if(noMenu.length === 0) {
-							var id = random(3).toUpperCase();
-							var ordersChannel = call.client.channels.get("399290151932526593");
-							var orderedFromString = `${call.message.channel.toString()} (${call.message.channel.id}) in ${call.message.channel.guild.name} (${call.message.channel.guild.id})`;
-							var orderEmbed = new Discord.RichEmbed()
+							var id = random(3).toUpperCase(),
+								ordersChannel = call.client.channels.get("399290151932526593"),
+								orderedFromString = `${call.message.channel.toString()} (${call.message.channel.id}) in ${call.message.channel.guild.name} (${call.message.channel.guild.id})`;
+							var orderEmbed = new RichEmbed()
 								.setColor("RED")
 								.addField("Order ID", id)
-								.addField("Order", foods.map((m) => `\`${titleCase(m)}\``).join("\n"))
+								.addField("Order", foods.map((m) => "`" + titleCase(m) + "`").join("\n"))
 								.addField("Customer", call.message.author.tag)
 								.addField("Ordered From", orderedFromString)
 								.addField("Status", "Awaiting Cook");
-							ordersChannel.send(orderEmbed).then(() => {
-								call.message.reply("Your order has been sent!").catch(() => {
-									call.message.author.send(`You attempted to use the \`order\` command in ${call.message.channel}, but I can not chat there.`).catch(() => {});
-								});
+							ordersChannel.send({ embed: orderEmbed }).then(() => {
+								call.message.reply("Your order has been sent!").catch(() => {});
 							}).catch(() => {
 								call.message.reply("Couldn't deliver your order, please try again.").catch(() => {
 									call.message.author.send(`You attempted to use the \`order\` command in ${call.message.channel}, but I can not chat there.`).catch(() => {});
