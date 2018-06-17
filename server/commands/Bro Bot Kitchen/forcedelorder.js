84eb00102aaebc2f5bffbde11195ee74b1e21945
Yikey <gtc122004@gmail.com>
2018-06-17 09:33:32 -0400

    Update forcedelorder.js

--- server/commands/Bro Bot Kitchen/forcedelorder.js
+++ server/commands/Bro Bot Kitchen/forcedelorder.js
@@ -1,24 +1,23 @@
 var isWorker = require("app/workers");
+
 module.exports = {
 	id: "forcedelorder",
 	description: "Deletes someone else's order",
 	paramsHelp: "(order number) (reason)",
 	execute: (call) => {
-		var ordersChannel = call.client.channels.get("399290151932526593");
-		var kitchenServer = ordersChannel.guild;
-		var member = kitchenServer.members.get(call.message.author.id);
+		var ordersChannel = call.client.channels.get("399290151932526593"),
+			kitchenServer = ordersChannel.guild,
+			member = kitchenServer.members.get(call.message.author.id);
 		if(member != null && isWorker(member)) {
-			var code = call.params.readParameter();
-			var reason = call.params.readParameter(true);
+			var code = call.params.readParameter(),
+				reason = call.params.readParameter(true);
 			if (code != null && reason != null) {
 				ordersChannel.fetchMessages({ limit: 100 }).then((orders) => {
-					var filteredOrder = orders.find((m) => m.embeds[0] && m.embeds[0].fields[0].value === code.toUpperCase());
-					if (filteredOrder) {
+					var filteredOrder = orders.find((m) => m.embeds[0] != null && m.embeds[0].fields[0].value === code.toUpperCase());
+					if (filteredOrder != null) {
 						var userToMessage = call.client.users.find((m) => m.tag === filteredOrder.embeds[0].fields[2].value);
 						filteredOrder.delete().then(() => {
-							call.message.reply("Successfully cancelled this order.").catch(() => {
-								call.message.author.send(`You attempted to use the \`forcedelorder\` command in ${call.message.channel}, but I can not chat there.`).catch(() => {});
-							});
+							call.message.reply("Successfully cancelled this order.").catch(() => {});
 							if (userToMessage != null) {
 								userToMessage.send(`Your order has been cancelled. Reason: \`${reason}\``).catch(() => {
 									call.message.reply("Couldn't DM this user, but I deleted the order anyways").catch(() => {
