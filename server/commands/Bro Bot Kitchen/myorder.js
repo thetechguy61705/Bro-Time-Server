f3723a1c77458feacbb3d1ad6536602d954826fa
Yikey <gtc122004@gmail.com>
2018-06-17 09:39:18 -0400

    Update myorder.js

--- server/commands/Bro Bot Kitchen/myorder.js
+++ server/commands/Bro Bot Kitchen/myorder.js
@@ -1,20 +1,21 @@
 const Discord = require("discord.js");
+
 module.exports = {
 	id: "myorder",
 	description: "Sends your current order and its status",
 	execute: (call) => {
 		var ordersChannel = call.client.channels.get("399290151932526593");
 		ordersChannel.fetchMessages({ limit: 100 }).then((orders) => {
-			var filteredOrder = orders.find((m) => m && m.embeds && m.embeds[0] && m.embeds[0].fields[2].value === call.message.author.tag);
-			if (filteredOrder) {
+			var filteredOrder = orders.find((m) => m.embeds[0] != null && m.embeds[0].fields[2].value === call.message.author.tag);
+			if (filteredOrder != null) {
 				var status = filteredOrder.embeds[0].fields[4].value;
-				if(status.startsWith("Claimed")) status = "Claimed";
+				if (status.startsWith("Claimed")) status = "Claimed";
 				var orderEmbed = new Discord.RichEmbed()
 					.setColor("RED")
 					.addField("Order", filteredOrder.embeds[0].fields[1].value)
 					.addField("Status", status)
-					.setFooter(`Ran by ${call.message.author.tag}`);
-				call.message.channel.send(orderEmbed).catch(() => {
+					.setFooter(`Ran by ${call.message.author.tag}`, call.message.author.displayAvatarURL);
+				call.message.channel.send({ embed: orderEmbed }).catch(() => {
 					call.message.author.send(`You attempted to use the \`myorder\` command in ${call.message.channel}, but I can not chat there.`).catch(() => {});
 				});
 			} else {
