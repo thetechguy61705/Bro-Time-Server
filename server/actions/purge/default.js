97b57682cc21e8eec0c05a041b6c4cc9001cd8ab
Yikey <gtc122004@gmail.com>
2018-06-20 22:23:52 -0400

    Fix purge default

--- server/actions/purge/default.js
+++ server/actions/purge/default.js
@@ -4,9 +4,10 @@ module.exports = {
 		var amountToPurge = Number((parameter !== "") ? parameter : undefined),
 			user = call.params.readParameter();
 		user = (user != null) ? call.message.guild.members.find((member) => user.includes(member.id) || member.user.tag.toLowerCase().startsWith(user.toLowerCase())) : null;
+		var filter = (user != null) ? (msg) => msg.author.id === user.id : () => true;
 		if (!isNaN(amountToPurge) && amountToPurge > 0 && amountToPurge <= 100) {
 			call.message.delete().then(() => {
-				call.purgeMessages(amountToPurge, call.message.channel, (msg) => msg.author.id === (user || { id: null }).id).then((amountPurged) => {
+				call.purgeMessages(amountToPurge, call.message.channel, filter).then((amountPurged) => {
 					call.message.reply("Deleted " + amountPurged + " message" + ((amountPurged === 1) ? "" : "s") + " from the current channel.")
 						.then((msg) => msg.delete(5000))
 						.catch(() => {});
