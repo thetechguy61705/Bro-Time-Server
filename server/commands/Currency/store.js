456b3e4f0e0ef78949918c3341bbb96145170c3a
ethanlaj <ethanlaj@ericlaj.com>
2018-06-23 20:12:13 -0400

    create store.js

---
+++ server/commands/Currency/store.js
@@ -0,0 +1,16 @@
+var store = require("app/store");
+const Discord = require("discord.js");
+module.exports = {
+	id: "store",
+	description: "Shows you the store.",
+	access: "Public",
+	execute: (call) => {
+		var storeEmbed = new Discord.RichEmbed()
+			.setTitle("Store")
+			.setColor("GREEN");
+		for (let item of Object.values(store)) {
+			storeEmbed.addField(item.name, `Price: \`${item.price}\`\nDescription: \`${item.description}\``);
+		}
+		call.message.channel.send(storeEmbed);
+	}
+};
