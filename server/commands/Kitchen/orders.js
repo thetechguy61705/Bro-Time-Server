edb6a0ad16887ed0d7d89d5a670cc8be2d15e8e0
Yikey <gtc122004@gmail.com>
2018-06-28 19:39:48 -0400

    Kitchen upgrades, grammar fix for `!role for`

---
+++ server/commands/Kitchen/orders.js
@@ -0,0 +1,36 @@
+const { orders } = require("../../load/orders.js");
+const isWorker = require("app/workers");
+
+module.exports = {
+	id: "orders",
+	description: "Lists unclaimed orders",
+	paramsHelp: "[status]",
+	access: "Public",
+	execute: (call) => {
+		if (!call.client.bbkLocked) {
+			var kitchenServer = call.client.guilds.get("398948242790023168"),
+				member = kitchenServer.members.get(call.message.author.id);
+			if (member != null && isWorker(member)) {
+				var ordersToFilter = orders;
+				var filteredOrders;
+				if (call.params.readRaw()) {
+					filteredOrders = ordersToFilter.filter((m) => m.status.toLowerCase().startsWith(call.params.readRaw().toLowerCase()));
+				} else {
+					filteredOrders = ordersToFilter;
+				}
+				if (call.params.readRaw().toLowerCase() === "claimed") {
+					filteredOrders = ordersToFilter.filter((m) => m.status.startsWith("Claimed"));
+				}
+				if (filteredOrders[0] != null) {
+					var messageContent = filteredOrders.map((m) => "`" + m.id + "`").join(",");
+					call.message.reply(`Orders: ${messageContent}`);
+				} else call.safeSend("There are no orders with this status.");
+			} else call.safeSend("You don't have permission to use this command!");
+		} else {
+			if (!call.client.bbkLockedChannels.includes(call.message.channel.id)) {
+				call.client.bbkLockedChannels.push(call.message.channel.id);
+				call.message.channel.send("Bro Bot Kitchen is currently in lockdown and inaccessible by any user.");
+			}
+		}
+	}
+};
