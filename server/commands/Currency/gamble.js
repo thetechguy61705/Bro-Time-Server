94950e2bdafef293a3c4078a5e79a02844e246fa
Yikey <gtc122004@gmail.com>
2018-07-08 17:06:59 -0400

    wdym ethan ????

--- server/commands/Currency/gamble.js
+++ server/commands/Currency/gamble.js
@@ -15,7 +15,7 @@ module.exports = {
 		var param = call.params.readNumber(), amountToBet = Math.ceil(param || param.toNumber(userBalance));
 		if (amountToBet != null && !isNaN(amountToBet) && amountToBet > 0) {
 			if (amountToBet <= userBalance) {
-				var randomSuccess = ((Math.random() * 10) < 4) ? true : false;
+				var randomSuccess = (Math.random() * 10) < 4;
 				var randomMultiplier = (Math.random() * 0.8) + 0.6;
 				if (randomSuccess) {
 					call.getWallet(call.message.author.id).change(Math.round(amountToBet * randomMultiplier)).then(() => {
