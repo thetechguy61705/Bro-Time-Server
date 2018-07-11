fead50104ae52e8213b45064ffa00bb3cbbb3385
Yikey <gtc122004@gmail.com>
2018-07-11 09:40:03 -0400

    Remove shard-loading

--- server/launcher.js
+++ server/launcher.js
@@ -1,15 +1,6 @@
 const { ShardingManager } = require("discord.js");
-const fs = require("fs");
 const config = require("../config");
 const manager = new ShardingManager(__dirname + "/server.js", { token: config.TOKEN });
-
-var loaders = [];
-
-for (let file of fs.readdirSync(__dirname + "/shard-loading")) {
-	if (file.endsWith(".js")) {
-		loaders.push(require("./shard-loading/" + file));
-	}
-}
 
 /* eslint-disable no-console */
 
@@ -19,9 +10,6 @@ manager.spawn(manager.totalShards, 30000);
 
 manager.on("launch", (shard) => {
 	console.log(`Launched shard ${shard.id}`);
-	for (let loader of loaders) {
-		if (loader.exec) loader.exec(shard);
-	}
 });
 
 manager.on("message", (shard, message) => {
