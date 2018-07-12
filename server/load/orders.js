5bf9065598b4d8f9c0ae656ef1f69a7bfb373973
Yikey <gtc122004@gmail.com>
2018-07-12 16:55:25 -0400

    test dont yell at me if this dont work >_>

--- server/load/orders.js
+++ server/load/orders.js
@@ -15,23 +15,29 @@ module.exports = {
 	delOrder: function (order) {
 		this.orders.splice(this.orders.indexOf(order), 1);
 	},
-	exec: function (client) {
-		var ordersChannel = client.channels.get("399290151932526593");
-		if (ordersChannel != null) {
-			ordersChannel.fetchMessages({ limit: 100 }).then((orders) => {
-				orders = orders.filter((m) => m.embeds[0]).array();
-				for (let order of orders) {
-					this.orders.push({
-						msg: order,
-						id: order.embeds[0].fields[0].value,
-						order: order.embeds[0].fields[1].value,
-						customer: order.embeds[0].fields[2].value,
-						orderedFrom: order.embeds[0].fields[3].value,
-						status: order.embeds[0].fields[4].value,
-						links: order.embeds[0].fields[5].value || "None",
-					});
-				}
-			});
+	exec: async function (client) {
+		var containsOrders = await client.shard.broadcastEval("this.channels.keyArray();");
+		containsOrders = containsOrders.find((channels) => channels.includes("399290151932526593"));
+
+		if (containsOrders) {
+			client.shard.broadcastEval(
+				"var ordersChannel = this.channels.get('399290151932526593');" +
+				"if (ordersChannel == null) return;"
+				"ordersChannel.fetchMessages({ limit: 100 }).then((orders) => {" +
+					"orders = orders.filter((m) => m.embeds[0]).array();" +
+					"for (let order of orders) {" +
+						`${module.exports.orders}.push({` +
+							"msg: order," +
+							"id: order.embeds[0].fields[0].value," +
+							"order: order.embeds[0].fields[1].value," +
+							"customer: order.embeds[0].fields[2].value," +
+							"orderedFrom: order.embeds[0].fields[3].value," +
+							"status: order.embeds[0].fields[4].value,"  +
+							"links: order.embeds[0].fields[5].value || 'None'," +
+						"});" +
+					"}" +
+				"});" +
+			);
 		} else {
 			console.warn("The client does not have access to the orders channel.");
 			console.warn("All kitchen commands have been disabled on the client.");
