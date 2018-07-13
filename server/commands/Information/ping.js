ed47ecfc877f0b6bc4a7229013a8a499b663325c
Yikey <gtc122004@gmail.com>
2018-07-12 21:04:06 -0400

    ping absoute value

--- server/commands/Information/ping.js
+++ server/commands/Information/ping.js
@@ -6,7 +6,8 @@ module.exports = {
 	execute: (call) => {
 		var delay = call.client.pings[0];
 		call.message.channel.send(`Pong! Latency: \`pinging...\`. API latency: \`${delay} ms; ${delay.diagnostic()}.\`!`).then((msg) => {
-			var latency = msg.createdTimestamp - call.message.createdTimestamp;
+			var latency = Math.abs(msg.createdTimestamp - call.message.createdTimestamp);
+			latencey += (latency === 0 ? 5 : 0);
 			msg.edit(`Pong! Latency: \`${latency} ms; ${latency.diagnostic()}\`. API latency: \`${delay} ms; ${delay.diagnostic()}\`!`);
 		}).catch(() => {
 			call.message.author.send(`You attempted to run the \`ping\` command in ${call.message.channel}, but I do not have permission to chat there.`);
