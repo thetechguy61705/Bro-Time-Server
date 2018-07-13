25216ea241a24e4963ee9898cae6d934450bd15c
Yikey <gtc122004@gmail.com>
2018-07-12 21:09:06 -0400

    boi

--- server/commands/Information/ping.js
+++ server/commands/Information/ping.js
@@ -7,7 +7,7 @@ module.exports = {
 		var delay = call.client.pings[0];
 		call.message.channel.send(`Pong! Latency: \`pinging...\`. API latency: \`${delay} ms; ${delay.diagnostic()}.\`!`).then((msg) => {
 			var latency = Math.abs(msg.createdTimestamp - call.message.createdTimestamp);
-			latencey += (latency === 0 ? 5 : 0);
+			latency += (latency === 0 ? 5 : 0);
 			msg.edit(`Pong! Latency: \`${latency} ms; ${latency.diagnostic()}\`. API latency: \`${delay} ms; ${delay.diagnostic()}\`!`);
 		}).catch(() => {
 			call.message.author.send(`You attempted to run the \`ping\` command in ${call.message.channel}, but I do not have permission to chat there.`);
