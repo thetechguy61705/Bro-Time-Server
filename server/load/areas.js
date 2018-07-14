c0c040e6a63da5fa3b31b758b939b3015b93d8cc
Colton Loewen <loewencjay@gmail.com>
2018-07-14 16:17:56 -0600

    Implemented load in area loaders

--- server/load/areas.js
+++ server/load/areas.js
@@ -1,39 +1,37 @@
-var { GuildChannel } = require("discord.js");
-var fs = require("fs");
-var areaLoaders = [];
-
-async function load(area, client) {
-	for (var areaLoader of areaLoaders) {
-		try {
-			var promise = areaLoader.exec(area, client);
-			if (promise != null)
-				await promise;
-		} catch (exc) {
-			console.warn(exc.stack);
-		}
-	}
-}
-
-function loadChannel(channel, client) {
-	if (!(channel instanceof GuildChannel))
-		return load(channel, client);
-}
-
-for (let file of fs.readdirSync(__dirname + "/../areaLoad")) {
-	if (file.endsWith(".js"))
-		areaLoaders.push(require("@server/areaLoad/" + file));
-}
+const { GuildChannel } = require("discord.js");
+const { load } = require("@utility/filesloader.ts");
+const LOAD_TIMEOUT = 60000;
 
 module.exports = {
 	id: "areas",
 	exec: (client) => {
-		var promises = [];
-		for (var guild of client.guilds.values())
-			promises.push(load(guild, client));
-		for (var channel of client.channels.values())
-			promises.push(loadChannel(channel, client));
-		client.on("guildCreate", load);
-		client.on("channelCreate", loadChannel);
-		return Promise.all(promises);
+		return load("areaLoad", {
+			client: client,
+			displayErrorStack: false,
+			success: (exported) => {
+				for (var guild of client.guilds.values())
+					exported.exec(guild, client);
+				for (var channel of client.channels.values()) {
+					if (!(channel instanceof GuildChannel))
+						exported.exec(channel, client);
+				}
+				client.on("guildCreate", (guild) => exported.exec(guild, client));
+				client.on("channelCreate", (channel) => {
+					if (!(channel instanceof GuildChannel))
+						exported.exec(channel, client);
+				});
+			},
+			failure: (exc) => {
+				console.warn("Failed to load area:");
+				throw exc;
+			},
+			TimeoutTime: LOAD_TIMEOUT,
+			timeout: (exc) => {
+				throw exc;
+			},
+			predicate: (exported) => {
+				return !exported.needs || client.guilds.has(exported.needs);
+			}
+		});
 	}
 };
