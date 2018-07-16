35eccb2c4ab9771796babee36d190d080390eda3
Yikey <gtc122004@gmail.com>
2018-07-16 15:31:43 -0400

    disable speed-typing, remove canvas

--- server/games/speed-typing.js
+++ server/games/speed-typing.js
@@ -1,6 +1,8 @@
+/*
 const { Attachment } = require("discord.js");
 const textToPNG = require("text2png");
 const randomWords = require("random-words");
+
 
 function splitArray(arr) {
 	var newArr = [];
@@ -8,6 +10,7 @@ function splitArray(arr) {
 		newArr.push(arr.splice(0, 5));
 	} while (arr.length > 0);
 }
+*/
 
 module.exports = {
 	id: "speed-typing",
@@ -21,29 +24,30 @@ module.exports = {
 	allowLateJoin: false,
 	load: () => {},
 	start: (session) => {
-		if (session.context.channel.permissionsFor(session.client).has("ATTACH_FILES")) {
-			session.players.set(session.host.id, session.host);
-			var words = randomWords({ exactly: 20, maxLength: 16, formatter: (word) => { return word.toUpperCase(); }}).join(" ");
-			var image = textToPNG(splitArray.map((a) => a.join(" ")).join("\n"), { font: "20px Merriweather", padding: 3 });
-			var text = words.join(" ");
-			session.context.channel.send("Game starting in **3**.").then((msg) => {
-				var interval = session.client.setInterval(() => {
-					if (Number(msg.content.match(/\*\*\d\*\*/)[0]) !== 1) {
-						msg.edit(`Game starting in **${Number(msg.content.match(/\*\*\d\*\*/)[0]) - 1}**`);
-					} else {
-						call.client.clearInterval(interval);
-						msg.edit("GAME STARTED (this game is case insensitive). TWO MINUTES.", { attachment: new Attachment(image) }).then(() => {
-							msg.channel.awaitMessages((m) => session.players.keyArray().includes(m.author.id) &&
-								m.content.toUpperCase() === words,
-								{ time: 120000, maxMatches: 1, errors: ["time"] }).then((results) => {
-									session.winner = results[0].author;
-									session.endGame();
-								});
-						});
-					}
-				}, 1000);
-			});
-		} else session.context.channel.send("I don't have the permission to attach files to my messages in this channel. Please enable it to play this game.");
+		session.endGame();
+		// if (session.context.channel.permissionsFor(session.client).has("ATTACH_FILES")) {
+		//	session.players.set(session.host.id, session.host);
+		//	var words = randomWords({ exactly: 20, maxLength: 16, formatter: (word) => { return word.toUpperCase(); }}).join(" ");
+		//	var image = textToPNG(splitArray.map((a) => a.join(" ")).join("\n"), { font: "20px Merriweather", padding: 3 });
+		//	var text = words.join(" ");
+		//	session.context.channel.send("Game starting in **3**.").then((msg) => {
+		//		var interval = session.client.setInterval(() => {
+		//			if (Number(msg.content.match(/\*\*\d\*\*/)[0]) !== 1) {
+		//				msg.edit(`Game starting in **${Number(msg.content.match(/\*\*\d\*\*/)[0]) - 1}**`);
+		//			} else {
+		//				call.client.clearInterval(interval);
+		//				msg.edit("GAME STARTED (this game is case insensitive). TWO MINUTES.", { attachment: new Attachment(image) }).then(() => {
+		//					msg.channel.awaitMessages((m) => session.players.keyArray().includes(m.author.id) &&
+		//						m.content.toUpperCase() === words,
+		//						{ time: 120000, maxMatches: 1, errors: ["time"] }).then((results) => {
+		//							session.winner = results[0].author;
+		//							session.endGame();
+		//						});
+		//				});
+		//			}
+		//		}, 1000);
+		//	});
+		// } else session.context.channel.send("I don't have the permission to attach files to my messages in this channel. Please enable it to play this game.");
 	},
 	input: () => { return false; },
 	end: (session) => {
