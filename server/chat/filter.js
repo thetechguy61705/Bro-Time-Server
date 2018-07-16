17d5e414e3264a63c89d38703003e1d23384dfb8
Yikey <gtc122004@gmail.com>
2018-07-16 14:52:20 -0400

    url checking filter

--- server/chat/filter.js
+++ server/chat/filter.js
@@ -5,6 +5,7 @@ const isModerator = require("@utility/moderator");
 const { ALPHABET, NUMBERS } = require("@utility/alphaNumericChars.ts");
 const GUILDS = require("@server/server").guilds;
 const INVITE_REGEX = /(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li)|discordapp\.com\/invite)\/\w+/gi;
+const URL_REGEX = /(https?:\/\/(www\.)?)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
 const MARKDOWN = /(`|\*|_|~)+/g;
 
 function getKeyByValue(object, value) {
@@ -30,46 +31,57 @@ module.exports = {
 
 	checks: [
 		function(message) {
-			const BAD_WORDS = ["-".repeat(2001)];
-			// Currently settings/preferences is not available, until then bad words will be an impossible string to create, do to discord message limitations.
-			const BAD_WORDS_REGEX = new RegExp(`(${BAD_WORDS.map((word) => escapeRegExp(word)).join(")|(")})`, "gi");
-			var cleanedContent = message.content.toUpperCase();
-			var emojis = message.content.match(new RegExp(require("emoji-regex")(), "g"));
-			var nums = message.content.match(new RegExp(`(${Object.values(NUMBERS).join(")|(")})`, "g"));
-			for (let num of nums || []) {
-				cleanedContent = cleanedContent.replace(num, getKeyByValue(NUMBERS, num) || num);
+			if (!(message.channel.topic || "").includes("<ignore-bad>")) {
+				const BAD_WORDS = ["-".repeat(2001)];
+				// Currently settings/preferences is not available, until then bad words will be an impossible string to create, do to discord message limitations.
+				const BAD_WORDS_REGEX = new RegExp(`(${BAD_WORDS.map((word) => escapeRegExp(word)).join(")|(")})`, "gi");
+				var cleanedContent = message.content.toUpperCase();
+				var emojis = message.content.match(new RegExp(require("emoji-regex")(), "g"));
+				var nums = message.content.match(new RegExp(`(${Object.values(NUMBERS).join(")|(")})`, "g"));
+				for (let num of nums || []) {
+					cleanedContent = cleanedContent.replace(num, getKeyByValue(NUMBERS, num) || num);
+				}
+				for (let emoji of emojis || []) {
+					cleanedContent = cleanedContent.replace(emoji, getKeyByValue(ALPHABET, emoji) || emoji);
+				}
+				cleanedContent = normalizeString(cleanedContent.replace(MARKDOWN, ""));
+				var cursed = BAD_WORDS_REGEX.test(cleanedContent) ||
+					BAD_WORDS_REGEX.test(cleanedContent.replace(/\s+/g, "")) ||
+					BAD_WORDS_REGEX.test(specialCharsToLetters(cleanedContent)) ||
+					BAD_WORDS_REGEX.test(specialCharsToLetters(cleanedContent).replace(/\s+/g, ""));
+				if (cursed) {
+					return "Please don't send blocked words.";
+				}
 			}
-			for (let emoji of emojis || []) {
-				cleanedContent = cleanedContent.replace(emoji, getKeyByValue(ALPHABET, emoji) || emoji);
-			}
-			cleanedContent = normalizeString(cleanedContent.replace(MARKDOWN, ""));
-			var cursed = BAD_WORDS_REGEX.test(cleanedContent) ||
-				BAD_WORDS_REGEX.test(cleanedContent.replace(/\s+/g, "")) ||
-				BAD_WORDS_REGEX.test(specialCharsToLetters(cleanedContent)) ||
-				BAD_WORDS_REGEX.test(specialCharsToLetters(cleanedContent).replace(/\s+/g, ""));
-			if (cursed) {
-				return "Please don't send blocked words.";
+		},
+		function(message) {
+			return false;
+			// Currently disabled because this requires settings.
+			if (!(message.channel.topic || "").includes("<ignore-url>") && !isModerator(message.member) && URL_REGEX.test(message.content)) {
+				return "Please do not send links.";
 			}
 		},
 		async function(message) {
-			var invites = message.content.replace(MARKDOWN, "").match(INVITE_REGEX);
+			if (!(message.channel.topic || "").includes("<ignore-invite>")) {
+				var invites = message.content.replace(MARKDOWN, "").match(INVITE_REGEX);
 
-			if (invites != null) {
-				for (let possibleInvite of invites) {
-					try {
-						var invite = await message.client.fetchInvite(possibleInvite);
-						if (invite != null && invite.guild.id !== message.guild.id && !isModerator(message.member)) {
-							if (GUILDS.includes(message.guild.id)) {
-								if (!GUILDS.includes(invite.guild.id)) {
+				if (invites != null) {
+					for (let possibleInvite of invites) {
+						try {
+							var invite = await message.client.fetchInvite(possibleInvite);
+							if (invite != null && invite.guild.id !== message.guild.id && !isModerator(message.member)) {
+								if (GUILDS.includes(message.guild.id)) {
+									if (!GUILDS.includes(invite.guild.id)) {
+										return "Please do not send invite links to other Discord servers.";
+									}
+								} else {
 									return "Please do not send invite links to other Discord servers.";
 								}
-							} else {
-								return "Please do not send invite links to other Discord servers.";
 							}
+						} catch (exc) {
+							if (!exc.message.startsWith("Unknown Invite") && !exc.message.startsWith("404: Not Found"))
+								console.warn(exc.stack);
 						}
-					} catch (exc) {
-						if (!exc.message.startsWith("Unknown Invite") && !exc.message.startsWith("404: Not Found"))
-							console.warn(exc.stack);
 					}
 				}
 			}
--- server/commands/Information/channelinfo.js
+++ server/commands/Information/channelinfo.js
@@ -4,34 +4,32 @@ module.exports = {
 	id: "channelinfo",
 	aliases: ["channelinformation", "infochannel", "ci"],
 	description: "Displays information on the specified channel.",
-	paramsHelp: "(channel)",
+	paramsHelp: "[channel]",
 	access: "Server",
 	execute: async (call) => {
-		var channel = call.params.readChannel();
-		if (channel != null) {
-			var infoEmbed = new RichEmbed()
-				.setTitle("Information on " + channel.name)
-				.setColor(0x00AE86)
-				.addField("Name", "`" + channel.name + "`", true)
-				.addField("Mention", channel.toString(), true)
-				.addField("ID", channel.id, true)
-				.addField("Parent Channel", "`" + (channel.parent || { name: "none" }).name + "`", true)
-				.addField("Created At", channel.createdAt.toString().substring(0, 15), true)
-				.addField("Position", channel.position.toString(), true)
-				.addField("Type", channel.type.toString(), true);
-			if (channel.type === "text") {
-				infoEmbed.addField("Topic", channel.topic || "none", true)
-					.addField("NSFW", channel.nsfw.toString(), true);
-			} else if (channel.type === "voice") {
-				infoEmbed.addField("User Limit", channel.userLimit.toString(), true)
-					.addField("Users", channel.members.size.toString(), true)
-					.addField("Bitrate", channel.bitrate.toString(), true)
-					.addField("Full", channel.full.toString(), true);
-			} else if (channel.type === "category") {
-				infoEmbed.addField("Children", channel.children.size <= 0 ? "none" : "`" + channel.children.map((child) => child.name).join("`, `") + "`");
-			}
-			infoEmbed.setDefaultFooter(call.message.author);
-			call.safeSend(null, call.message, { embed: infoEmbed });
-		} else call.safeSend("You did not specify a valid channel.");
+		var channel = call.params.readChannel() || call.message.channel;
+		var infoEmbed = new RichEmbed()
+			.setTitle("Information on " + channel.name)
+			.setColor(0x00AE86)
+			.addField("Name", "`" + channel.name + "`", true)
+			.addField("Mention", channel.toString(), true)
+			.addField("ID", channel.id, true)
+			.addField("Parent Channel", "`" + (channel.parent || { name: "none" }).name + "`", true)
+			.addField("Created At", channel.createdAt.toString().substring(0, 15), true)
+			.addField("Position", channel.position.toString(), true)
+			.addField("Type", channel.type.toString(), true);
+		if (channel.type === "text") {
+			infoEmbed.addField("Topic", channel.topic || "none", true)
+				.addField("NSFW", channel.nsfw.toString(), true);
+		} else if (channel.type === "voice") {
+			infoEmbed.addField("User Limit", channel.userLimit.toString(), true)
+				.addField("Users", channel.members.size.toString(), true)
+				.addField("Bitrate", channel.bitrate.toString(), true)
+				.addField("Full", channel.full.toString(), true);
+		} else if (channel.type === "category") {
+			infoEmbed.addField("Children", channel.children.size <= 0 ? "none" : "`" + channel.children.map((child) => child.name).join("`, `") + "`");
+		}
+		infoEmbed.setDefaultFooter(call.message.author);
+		call.safeSend(null, call.message, { embed: infoEmbed });
 	}
 };
