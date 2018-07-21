dfb4676eaaff7d491ce3eb6e291341ed3f68b6df
Yikey <gtc122004@gmail.com>
2018-07-21 19:24:11 -0400

    ban fix to use string not number

--- server/load/ban-update.js
+++ server/load/ban-update.js
@@ -8,7 +8,7 @@ module.exports = {
 				client.shard.broadcastEval(
 					"const BRO_TIME_GUILDS = ['330913265573953536', '453694109819994114', '463408396872187904', '398948242790023168'];" +
 					"for (let g of BRO_TIME_GUILDS) {" +
-						`if (this.guilds.has(g)) this.guilds.get(g).ban(${user.id}).catch((exc) => {` +
+						`if (this.guilds.has(g)) this.guilds.get(g).ban('${user.id}').catch((exc) => {` +
 							// eslint-disable-next-line no-useless-escape
 							"if (!exc.message.includes('Couldn\'t resolve the user ID to ban.')) console.warn(exc.stack);" +
 						"});" +
@@ -22,7 +22,7 @@ module.exports = {
 				client.shard.broadcastEval(
 					"const BRO_TIME_GUILDS = ['330913265573953536', '453694109819994114', '463408396872187904', '398948242790023168'];" +
 					"for (let g of BRO_TIME_GUILDS) {" +
-						`if (this.guilds.has(g)) this.guilds.get(g).unban(${user.id}).catch((exc) => {` +
+						`if (this.guilds.has(g)) this.guilds.get(g).unban('${user.id}').catch((exc) => {` +
 							// eslint-disable-next-line no-useless-escape
 							"if (!exc.message.includes('Couldn\'t resolve the user ID to ban.')) console.warn(exc.stack);" +
 						"});" +
