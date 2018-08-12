15daf4d872daa033cefa54a4ee799e7a6c9e5868
Yikey <gtc122004@gmail.com>
2018-08-12 18:38:20 -0400

    TYPESCRIPT BE LIKE

--- server/load/games.ts
+++ server/load/games.ts
@@ -1,18 +1,19 @@
 import games, { Input } from "@server/commands/Miscellaneous/games.ts";
+import { Message, MessageReaction, User, GuildMember, Snowflake, Client } from "discord.js";
 
 module.exports = {
 	id: "games",
-	exec: (client) => {
-		client.on("message", (message) => {
+	exec: (client: Client) => {
+		client.on("message", (message: Message) => {
 			games.dispatchInput(new Input("message", message.content, message.author, message.channel));
 		});
-		client.on("messageReactionAdd", (reaction, user) => {
+		client.on("messageReactionAdd", (reaction: MessageReaction, user: User) => {
 			games.dispatchInput(new Input("reaction", reaction, user, reaction.message.channel));
 		});
-		client.on("guildMemberAdd", (member) => {
+		client.on("guildMemberAdd", (member: GuildMember) => {
 			games.dispatchInput(new Input("member", member, member.user));
 		});
-		client.on("walletChange", (user, amount) => {
+		client.on("walletChange", (user: Snowflake, amount: number) => {
 			games.dispatchBalances(user, amount);
 		});
 	}
